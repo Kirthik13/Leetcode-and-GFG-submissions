6018f05658828c201ec18dc7006b7fb0c102bad9
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-01-06 20:33:36 +0530

    Time: 16 ms (43.16%), Space: 10.4 MB (29.36%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -2,23 +2,35 @@ class Solution {
 public:
     bool carPooling(vector<vector<int>>& v, int cap) {
      
-     int dp[1000]={0};
-    int f=0;
+   
+    vector<pair<int,int>>tp;
+
     for(auto i:v)
     {
-        for(int j=i[1];j<i[2];j++)
+      tp.push_back({i[1],i[0]});
+      tp.push_back({i[2],-i[0]});
+    }
+    sort(begin(tp),end(tp));
+
+    int filled{};
+
+    for(auto i:tp)
+    {
+        filled+=i.second;
+        if(filled>cap)
         {
-            dp[j]+=i[0];
-            if(cap<dp[j])
-            {
+            // cout<<"false"<<endl;
+         
+    
                 return 0;
                 // cout<<"false";
                 // f=1;
                 // break;
-            }
         }
+            }
+        
 
-    }
+    
     
     return 1;
     }
