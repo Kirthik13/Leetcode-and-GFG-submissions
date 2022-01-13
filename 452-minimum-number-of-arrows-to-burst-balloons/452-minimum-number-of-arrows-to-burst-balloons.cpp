77596dabfffe1818546e8690075de2089534afb8
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-01-13 20:50:13 +0530

    Time: 852 ms (9.10%), Space: 102.3 MB (11.99%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -2,56 +2,44 @@
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& v) {
-         // vector<vector<int>>v={{10,16},{2,8},{1,6},{7,12}};
-    // sort(all(v),[](auto &a,auto &b)
-    // {
+         
+    sort(all(v),[](auto &a,auto &b)
+    {
         // if(a[0]==b[0])
         // {
-            // return a[1]<b[1];
+            return a[1]<b[1];
         // }
         // return a[0]<b[0] ;
-    // }
-    // );
-        sort(all(v));
-    // for(auto i:v)
-    // {
-    //     for(auto j:i)
-    //     {
-    //         cout<<j<<" ";
-    //     }
-    //     cout<<nx;
-    // }
-    // cout<<sum;
+    }
+    );
+        // sort(all(v));
 
-    // vector<vector<int>>v2;
    int cnt=1;
     int end=v[0][1],st=v[0][0];
 
-    // for(auto i:v)
-    // {
-    //     if(i[0]<=end)
-    //     {
-    //         st=max(st,i[0]);
-    //         end=min(end,i[1]);
-    //     }
-    //     else{
-    //     cnt++;
-    //     end=i[1];
-    //         st=i[0];
-    //     }
-    // }
-        for(auto i:v)
-        {
-            if(i[0]<=end)
-            {
-                end=min(i[1],end);
-            }
-            else{
-                cnt++;
-                end=i[1];
-            }
+ for(auto i:v)
+ {
+     if(i[0]<=end)
+     {
+         continue;
+     }
+     else{
+         cnt++;
+         end=i[1];
+     }
+ }
+//         for(auto i:v)
+//         {
+//             if(i[0]<=end)
+//             {
+//                 end=min(i[1],end);
+//             }
+//             else{
+//                 cnt++;
+//                 end=i[1];
+//             }
             
-        }
+//         }
         return cnt;
     }
 };
