e92e06e7197a9486e14f2bcebbd75c84e9a293c5
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-01-14 09:03:17 +0530

    Time: 8 ms (15.20%), Space: 7 MB (52.53%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
     int myAtoi(string s) {
+        long long res{};
+     int i=s.find_first_not_of(' ');
+        if(i>=s.size())
+        {
+            return 0;
+        }
         
-        stringstream ss(s);
-        int g=0;
-        ss>>g;
-        return g;
+        int c=(s[i]=='-')?-1:1;
+        if(s[i]=='-' or s[i]=='+')
+        {
+            i++;
+        }
+        while(i<s.size() and isdigit(s[i]))
+        {
+            res=res*10+(s[i]-48);
+            if(res*c>=INT_MAX) return INT_MAX;
+                if(res*c<=INT_MIN) return INT_MIN;
+            // if(res)
+            i++;
+        }
+        return c*res;
     }
 };
