f59e7c32d4cecb4ce104eac73c85e8d1eb139dc0
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-01-16 12:22:19 +0530

    Time: 240 ms (5.24%), Space: 17.7 MB (13.89%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -1,48 +1,52 @@
 class Solution {
 public:
-    int fn(int &idx,int &n,vector<int>&v)
+    int fn(int &idx, int &n, vector<int> &v, unordered_map<int, vector<int>> &m)
 {
-    int c1=1,c2=1;
-    int i=idx-1;
-    int j=idx+1;
-
-    while(i>=0 or j<n)
+    int bf = INT_MAX, af = INT_MAX;
+  
+    for(auto i:m[1])
     {
-       if(j<n)
+        if(i>idx)
         {
-            if(v[j]==1)
-            {
-                break;
-            }
+            af=i;
+            break;
         }
-        if(i>=0)
-        {
-            if(v[i]==1)
-            {
-                break;
-            }
-        }
-        c1++;
-
-        i>=0?--i:i;
-        j<n?++j:j;
+        bf=i;
+    }
+    // deb2(bf, af);
+    if(bf==INT_MAX )
+    {
+        return af-idx;
+    }
+    else if(af==INT_MAX)
+    {
+        return idx-bf;
 
     }
-
-    return c1;
+    return min(af-idx, idx-bf);
+    
 }
-    int maxDistToClosest(vector<int>&v) {
-        int n=v.size(),ans=INT_MIN;
-int i=0;
-while(i<n)
-{
-    if(v[i]==0)
+    int maxDistToClosest(vector<int>& v) {
+        int n = v.size(), ans = INT_MIN;
+    unordered_map<int, vector<int>> m;
+    for (int i = 0; i < n; i++)
     {
-        // continue;
-    ans=max(ans,fn(i,n,v));
+        if (v[i] == 1)
+        {
+            m[v[i]].push_back(i);
+        }
     }
-    i++;
-}
+  
+    int i = 0;
+    while (i < n)
+    {
+        if (v[i] == 0)
+        {
+            
+            ans = max(ans, fn(i, n, v, m));
+        }
+        i++;
+    }
         return ans;
     }
 };
