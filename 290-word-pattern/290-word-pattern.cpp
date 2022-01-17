9a236864bc5487ea66cdcbe2807266d4da5032e2
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-01-17 10:20:47 +0530

    Time: 3 ms (20.00%), Space: 6.3 MB (96.25%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -1,60 +1,42 @@
 class Solution {
 public:
     bool wordPattern(string pat, string s) {
-        int n1=pat.size();
-s.push_back(' ');
-        int n2=s.size();
-
-int i{},j{};
+        stringstream st(s);
 string sam;
-unordered_map<char,string>m;
-while(i<n1 and j<n2)
+vector<string>v;
+while(st>>sam)
 {
-    // cout<<m.size()<<endl;
-    if(s[j]==' '){
-        // cout<<"sam : "<<sam<<endl;
-        if(m.find(pat[i])==m.end())
-        {
-            for(auto b:m)
-            {
-                if(b.second==sam)
-                {
-                    // r"false";
-                    return 0;
-                }
-            }
-        m.insert({pat[i],sam});
-        }
-        else{
-            // deb2(s[i],sam);
-            if(m[pat[i]]!=sam)
-            {
-                // cout<<"false";
-                return 0;
-            }
-            
-        }
-        sam.clear();
-        i++;
-
-    }
-    else if(isalpha(s[j]))
-    {
-        sam.push_back(s[j]);
-    }
-    j++;
-    // cout<<"j : "<<j<<endl;
-    //    cout<<"sam : "<<sam<<endl;
-
+    v.push_back(sam);
 }
-        // cout<<m.size();
-        // for(auto b:m)
-        // {
-        //     cout<<b.first<<" "<<b.second<<endl;
-        // }
-        if(i==n1 and j==n2){
-        return 1;
+if(v.size()!=pat.size())
+{
+    // cout<<"false";
+    return 0;
+}
+unordered_map<char,string>m;
+set<string>s1;
+for(int i=0;i<pat.size();i++)
+{
+    char c=pat[i];
+    if(m.find(c)==m.end())
+    {
+        if(s1.find(v[i])!=s1.end())
+        {
+            // cout<<"false";
+            return 0;
         }
-        return 0;
+        m[c]=v[i];
+        s1.insert(v[i]);
+    }
+    else{
+        if(m[c]!=v[i])
+        {
+            // cout<<"false";
+            return 0;
+        }
+    }
+}
+// cout<<"true";
+        return 1;
     }
 };
