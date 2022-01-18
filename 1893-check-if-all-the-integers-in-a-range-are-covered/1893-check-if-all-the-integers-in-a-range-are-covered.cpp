f5045fa276b7bc45d3b7b49ddff4251ce0d2376d
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-01-18 18:17:01 +0530

    Time: 12 ms (15.09%), Space: 8.9 MB (63.73%) - LeetHub

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
     bool isCovered(vector<vector<int>>& v, int l, int r) {
-        // vector<vector<int>>v={{1,10},{10,20}};
-unordered_map<int,int>m;
- // int l= 21;
- // int r=21;
- for(int i=l;i<=r;i++)
- {
-     m[i]++;
- }
+          int pre[52]={0};
 
-int num=l;
- for(auto &i:v)
- {
-   int l1=i[0];
-   int r1=i[1];
+    for(auto &i:v)
+    {
+        ++pre[i[0]];
+        --pre[i[1]+1];
+    }
 
-   for(int j=l1;j<=r1;j++)
-   {
-       if(m.find(j)!=m.end())
-       {
-           m[j]++;
-       }
-   }
- }
-
- for(int i=l;i<=r;i++)
- {
-     if(m[i]==1)
-     {
- // cout<<"fasle";
-         return 0;
-         // break;
-     }
- }
- // cout<<"true";
- return true;
+    for(int i=1;i<52;i++)
+    {
+        pre[i]+=pre[i-1];
+    }
+    for(int i=l;i<=r;i++)
+    {
+        if(!pre[i])
+        {
+            return 0;
+        }
+    }   
+    return 1;   
     }
 };
