f9b66a6eb87ddf1ff33bed51429408e7d2a3e81f
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-01-18 11:58:19 +0530

    Time: 46 ms (45.29%), Space: 23.2 MB (21.72%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    int numPairsDivisibleBy60(vector<int>&v) {
+    int numPairsDivisibleBy60(vector<int>& v) {
         unordered_map<int,int>m;
-        int n=v.size(),c{};
-        // for(int i=0;i<n;i++)
-        // {
-        //     int a=v[i]%60;
-        //     if(a==0)
-        //     {
-        //         c+=m[0];
-        //     }
-        //     else{
-        //         c+=m[60-a];
-        //     }
-        //     m[a]++;
-        // }
         
-        for(auto i:v)
+        int n=v.size();
+        int c{};
+        for(int i=0;i<n;i++)
         {
-            m[i%60]++;
+            int a=v[i]%60;
+            if(a==0)
+            {
+                c+=m[0];
+            }
+            else if(m.find(60-a)!=m.end())
+            {
+                c+=m[60-a];
+            }
+            m[a]++;
+            
+            
+                
         }
-        c+=(m[0]*(m[0]-1))/2;
-            c+=(m[30]*(m[30]-1))/2;
-    for(int i=1;i<=29;i++)
-    {
-        c+=m[i]*m[60-i];
-    }
         return c;
     }
 };
