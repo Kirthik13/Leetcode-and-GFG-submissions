dbff3d296a16341ca6b425c3a71227435f414167
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-01-19 09:25:05 +0530

    Time: 8 ms (83.32%), Space: 7.6 MB (73.74%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -17,45 +17,36 @@ public:
         ListNode* h=head;
                 ListNode* f=head;
         ListNode* s=head;
-        int cnt{};
-        unordered_map<ListNode*,int>m;
-        while(h)
-        {
-            if(m.find(h)==m.end()){
-            m[h]++;
-                
-            }
-            else{
-                m[h]++;
-                break;
-            }
-            h=h->next;
-            ++cnt;
-        }
-//     while(f and f->next)
-//     {
-//         s=s->next;
-//         f=f->next->next;
-//         if(f==s)
-//         {
-//             if(cnt&1){
-//             return f->next;
-//             }
-//             else{
-//                             return f;
+    
+    while(f and f->next)
+    {
+        f=f->next->next;
 
-//             }
-//         }
-//     }
-        while(f and f->next)
+        // ListNode* t=s;
+        s=s->next;
+        if(f==s)
         {
-            ListNode* k=f->next;
-            if(m[k]==2)
+           s=head;
+            while(s!=f)
             {
-                return k;
+                s=s->next;
+                f=f->next;
             }
-            f=f->next;
+            
+                            return f;
+
+            // }
         }
+    }
+        // while(f and f->next)
+        // {
+        //     ListNode* k=f->next;
+        //     if(m[k]==2)
+        //     {
+        //         return k;
+        //     }
+        //     f=f->next;
+        // }
         
         return NULL;
        
