7b97054bd656d55c8ba091413df05c120b8ecc8b
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-01-20 22:42:44 +0530

    Added solution - LeetHub

---
+++ Cyclically rotate an array by one - GFG/cyclically-rotate-an-array-by-one.c
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+#include <stdio.h>
+
+void rotate(int arr[], int n);
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        int a[n] , i;
+        for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        rotate(a, n);
+        for (i = 0; i < n; i++)
+            printf("%d ", a[i]);
+        printf("\n");
+    }
+        return 0;
+}
+// } Driver Code Ends
+
+
+void rotate(int v[], int n)
+{
+    int t=v[0];
+    int i=n-1;
+    
+ while(i>=1)
+ {
+     
+ 
+    //  t=v[i+1];
+     v[(i+1)%n]=v[i];
+     i--;
+ }
+ v[1]=t;
+}
