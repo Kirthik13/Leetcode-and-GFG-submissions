3533bdeb742b61d70b07662a1e9071411c79eaf7
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-01-22 15:55:15 +0530

    Time: 9 ms (16.30%), Space: 11.1 MB (75.14%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int search(vector<int>& v, int target) {
         int n=v.size();
+        // if(n==1)
+        // {
+        //     return v[0]==target?0:-1;
+        // }
         int l=0,r=n-1;
-        if(n==1)
-        {
-            return v[0]==target?0:-1;
-        }
         while(l<=r)
         {
             int mid=l+((r-l)/2);
@@ -14,19 +14,21 @@ public:
             {
                 return mid;
             }
-            if(v[l]<=v[mid])
+            else if(v[l]<=v[mid])
             {
                 if(v[l]<=target and v[mid]>=target)
                 {
-                    r=mid-1;
+                                        r=mid-1;
+
                 }
                 else{
-                    l=mid+1;
+                                        l=mid+1;
+
                 }
             }
             else
             {
-                if(v[r]>=target and v[mid+1]<=target)
+                 if(v[r]>=target and v[mid+1]<=target )
                 {
                     l=mid+1;
                 }
@@ -34,8 +36,8 @@ public:
                     r=mid-1;
                 }
             }
+            // cout<<"l : "<<l<<" r : "<<r<<" m : "<<mid<<endl;
         }
         return -1;
-        
     }
 };
