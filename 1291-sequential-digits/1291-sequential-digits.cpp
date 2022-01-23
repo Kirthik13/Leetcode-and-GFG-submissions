cdd378dc940214b426372e30ca5364ba7f8648f9
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-01-23 09:27:44 +0530

    Time: 4 ms (12.14%), Space: 6.2 MB (36.79%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        string s="123456789";
+        string hs=to_string(high);
+                string ls=to_string(low);
+    vector<int>res;
+        int n=s.size();
+        for(int i=ls.size();i<=hs.size();i++)
+        {
+            for(int j=0;j<=n-i;j++)
+            {
+                // if(s[j]-48>hs[0]-48){
+                //     continue;
+                // }
+                string g=s.substr(j,i);
+                int ans=stoi(g);
+                if(ans<=high and ans>=low)
+                {
+                    // break;
+                // }
+                res.emplace_back(ans);
+                }
+            }
+        }
+        return res;
+    }
+};
