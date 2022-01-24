5f509617ea718705d8b262cef5b3bffbdba388bb
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-01-24 17:46:17 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (57.66%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -2,22 +2,41 @@ class Solution {
 public:
     string addBinary(string a, string b) {
         string s;
-         int i=a.size()-1,j=b.size()-1,carry{};
+         // int i=a.size()-1,j=b.size()-1,carry{};
 
-    while(i>=0 or j>=0)
-    {
-        int x=i>=0?a[i--]-48:0;
-        int y=j>=0?b[j--]-48:0;
+//     while(i>=0 or j>=0)
+//     {
+//         int x=i>=0?a[i--]-48:0;
+//         int y=j>=0?b[j--]-48:0;
 
-        int sum=x+y+carry;
-        char c=(sum%2)+48;
-        s.insert(s.begin(),c);
-        carry=sum/2;
-    }
-    if(carry>0)
-    {
-        s.insert(s.begin(),'1');
-    }
+//         int sum=x+y+carry;
+//         char c=(sum%2)+48;
+//         s.insert(s.begin(),c);
+//         carry=sum/2;
+    // }
+//     if(carry>0)
+//     {
+//         s.insert(s.begin(),'1');
+//     }
+        
+        int i=a.size()-1,j=b.size()-1,carry{};
+        
+        while(i>=0 or j>=0)
+        {
+            int x=(i>=0)?a[i]-48:0;
+            int y=(j>=0)?b[j]-48:0;
+            int sum=x+y+carry;
+            char c=(sum%2)+48;
+            s.insert(s.begin(),c);
+            carry=sum/2;
+            i--;
+            j--;
+        }
+        if(carry>0)
+        {
+                s.insert(s.begin(),'1');
+
+        }
         return s;
     }
 };
