7f2c7550ad284dcdf0ceaeac9fe336178abafc83
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-01-31 08:39:01 +0530

    Time: 14 ms (8.24%), Space: 7.9 MB (49.93%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& v) {
-     int ans=INT_MIN;
-        for(auto &i:v)
-        {
-            int c{};
-            for(auto &j:i)
-            {
-                c+=j;
-            }
-            ans=max(ans,c);
-        }
-        return ans;
+     // int ans=INT_MIN;
+     //    for(auto &i:v)
+     //    {
+     //        int c{};
+     //        for(auto &j:i)
+     //        {
+     //            c+=j;
+     //        }
+     //        ans=max(ans,c);
+     //    }
+     //    return ans;
+        return accumulate(cbegin(v),cend(v),0,[](auto sum,const auto &money)
+                          {
+                              return max(sum,reduce(cbegin(money),cend(money),0));
+                          });
     }
 };
