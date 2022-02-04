97ae40732f8bd16c82702293c121ab89c620b99f
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-04 09:21:05 +0530

    Time: 177 ms (46.19%), Space: 83.8 MB (55.07%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& v) {
+        if(v.size()==1)
+        {
+            return 0;
+        }
         int c{};
         unordered_map<int,int>m;
-        m[0]=-1;
+        m[0]=0;
         int sum{};
         for(int i=0;i<v.size();i++)
         {
+            
            sum+=v[i]?1:-1;
-            if(m.find(sum)!=m.end())
+           // if(m.find(-1)!=m.end())
+           // {
+           //     cout<<"ASd"<<endl;
+           // }
+             if(m.find(sum)!=m.end())
             {
-                
+                 if(sum==0)
+                 {
+                     c=i+1;
+                 }
+                 else{
+                // cout<<sum<<" "<<m[sum]<<" a"<<endl;
                 c=max(i-m[sum],c);
+                 }
             }
             else{
+            // cout<<sum<<" "<<m[sum]<<" b"<<endl;
+
             m[sum]=i;
             }
         }
