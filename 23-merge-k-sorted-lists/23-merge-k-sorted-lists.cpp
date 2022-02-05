f259c63f73e50f7b0b5c38915380c560cedfe4b6
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-05 08:10:13 +0530

    Time: 20 ms (91.54%), Space: 14 MB (23.32%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -11,29 +11,40 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& v) {
-        map<int,int>m;
+        // map<int,int>m;
+        priority_queue<int,vector<int>,greater<int>>pq;
+
         for(auto &i:v)
         {
             ListNode* p=i;
             while(p){
-            m[p->val]++;
+            // m[p->val]++;
+                pq.push(p->val);
                 p=p->next;
             }
             
         }
         ListNode* dum=new ListNode(0);
         ListNode* h=dum;
-        for(auto &i:m)
-        {
-            int dat=i.first,occ=i.second;
-            while(occ--)
-            {
-                ListNode* node=new ListNode(dat);
-                h->next=node;
-                h=h->next;
+//         for(auto &i:m)
+//         {
+//             int dat=i.first,occ=i.second;
+//             while(occ--)
+//             {
+//                 ListNode* node=new ListNode(dat);
+//                 h->next=node;
+//                 h=h->next;
                 
-            }
+//             }
             
+//         }
+        while(!pq.empty())
+        {
+            int dat=pq.top();
+             pq.pop();
+             ListNode* node=new ListNode(dat);
+             h->next=node;
+             h=h->next;
         }
         h->next=NULL;
         return dum->next;
