eaefb85a1e4fbbf9da1882ce18a2cf1e3be454c1
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-05 22:53:59 +0530

    Time: 36 ms (76.88%), Space: 37.4 MB (5.01%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,22 +1,73 @@
+struct node{
+  node* links[26];
+  bool end=false;
+  int cw=-1;
+  bool contains(char ch)
+  {
+      return links[ch-'a']!=NULL;
+  }
+  void put(char &ch,node* h)
+  {
+      links[ch-'a']=h;
+  }
+  node* get(char &ch)
+  {
+      return links[ch-'a'];
+  }
+  
+  
+};
+class Trie{
+    public:
+        node* root;
+        
+        Trie()
+        {
+            root=new node();
+        }
+        int insert(string &s,int &idx)
+        {
+            node* h=root;
+            for(int i=0;i<s.size();i++)
+            {
+                if(!(h->contains(s[i])))
+                {
+                    h->put(s[i],new node());
+                    
+                }
+                h=h->get(s[i]);
+            }
+            h->end=true;
+            if(h->cw==-1) h->cw=++idx;
+            return h->cw;
+        
+        }
+      
+};
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& v) {
          vector<vector<string>>tot;
-        unordered_map<string,vector<string>>m;
-        // Trie h;
+        Trie h;
+        int idx=-1;
+      
         for(int i=0;i<v.size();i++)
         {
             string k=v[i];
             sort(k.begin(),k.end());
-            // h.insert(k);
-            m[k].emplace_back(v[i]);
+            int newid=h.insert(k,idx);
+            
+            if(newid==tot.size())
+            {
+                tot.push_back(vector<string>(1,v[i]));
+            }
+            else{
+                tot[newid].push_back(v[i]);
+            }
+            
             
             
         }
-        for(auto i:m)
-        {
-           tot.emplace_back(i.second);
-        }
-        return tot;
+            return tot;
     }
 };
