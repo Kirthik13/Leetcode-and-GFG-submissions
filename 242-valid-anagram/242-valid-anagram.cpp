c1c16f4533d49f6a46c261861d1f1ebf43723ae8
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-08 17:28:17 +0530

    Time: 18 ms (28.93%), Space: 7.4 MB (8.20%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int>m1,m2;
+        unordered_map<char,int>m1,m2;
         if(s.size()!=t.size()) return 0;
         for(auto &i:s)
         {
