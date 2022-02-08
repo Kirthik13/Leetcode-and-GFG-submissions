f93217bc6a6bfa19b8c91aa7fd4e309b48bafab6
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-08 17:23:12 +0530

    Time: 4 ms (93.89%), Space: 7 MB (75.22%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char>m1,m2;
+        // unordered_map<char,char>m1,m2;
+        int m1[128]={0},m2[128]={0};
         if(s.size()!=t.size()) return 0;
         for(int i=0;i<s.size();i++)
         {
-            if(m1.size()>0 and m2.size()>0)
-            {
-                if(m1.find(s[i])!=m1.end() or m2.find(t[i])!=m2.end())
-                {
-                    if(m1[s[i]]!=t[i]) return 0;
-                    if(m2[t[i]]!=s[i]) return 0;
-                }
-            }
-            m1[s[i]]=t[i];
-            m2[t[i]]=s[i];
+            // if(m1.size()>0 and m2.size()>0)
+            // {
+            //     if(m1.find(s[i])!=m1.end() or m2.find(t[i])!=m2.end())
+            //     {
+            //         if(m1[s[i]]!=t[i]) return 0;
+            //         if(m2[t[i]]!=s[i]) return 0;
+            //     }
+            // }
+            if(m1[s[i]]!=m2[t[i]]) return 0;
+            // m1[s[i]]=t[i];
+            // m2[t[i]]=s[i];
+            m1[s[i]]=i+1;
+            m2[t[i]]=i+1;
         }
         return 1;
     }
