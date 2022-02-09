32853a8067329d525839c22be1ac959c64a5a448
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-09 09:29:35 +0530

    Time: 23 ms (26.77%), Space: 7.7 MB (43.59%) - LeetHub

---
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    string sortString(string s) {
+        if(s.size()==1)
+        {
+            return s;
+        }
+        string t;
+    int n=s.size();
+       
+        vector<int>m(26,0);
+        for(auto &i:s)
+        {
+            m[i-'a']++;
+        }
+                vector<int>m2=m;
+
+       
+        int i=0;
+        int f=0;
+        while(t.size()<s.size())
+        {
+            int sum=accumulate(m.begin(),m.end(),0);
+            if(sum==0)
+            {
+                break;
+            }
+            if(m[i]>0)
+            {
+                m[i]--;
+                char ch=char(i)+'a';
+                t+=ch;
+                
+            }
+            
+            if(!f){
+            i++;
+            if(i==26)
+            {
+                i=25;
+                f=1;
+                // m=m2;
+            }
+            }
+            else if(f)
+            {
+                i--;
+            if(i==-1)
+            {
+                i=0;
+                f=0;
+                // m=m2;
+            }
+            }
+            
+            
+        }
+        return t;
+            
+    }
+};
