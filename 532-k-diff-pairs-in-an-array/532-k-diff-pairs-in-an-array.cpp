87f410c1f6a405ccf810b31b6ca075f4e2a4e94b
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-09 07:57:55 +0530

    Time: 22 ms (62.20%), Space: 13.3 MB (58.82%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -2,41 +2,18 @@ class Solution {
 public:
     int findPairs(vector<int>& v, int k) {
         unordered_map<int,int>m;
-        set<pair<int,int>>st;
+       
         int c{};
-        for(int i=0;i<v.size();i++)
+        for(auto  &i:v)
         {
-            int a,b;
-            
-                a=v[i]-k;
-                b=v[i]+k;
-            // }
-            // else{
-            //     a=v[i]-k;
-            // }
-            if(m[a]>0 )
+            m[i]++;
+        }
+        for(auto &i:m)
+        {
+            if((k>0 and m.find(i.first+k)!=m.end()) or (k==0 and m[i.first]>=2))
             {
-                if(st.find({a,v[i]})==st.end() and st.find({v[i],a})==st.end() ){
-                st.insert({a,v[i]});
-                // if(a!=v[i]){
-                        st.insert({v[i],a});
-                // }
                 c++;
-                }
-
             }
-     if(m[b]>0 )
-            {
-                if(st.find({b,v[i]})==st.end() and st.find({v[i],b})==st.end() ){
-                st.insert({b,v[i]});
-                // if(a!=v[i]){
-                        st.insert({v[i],b});
-                // }
-                c++;
-                }
-
-            }
-            m[v[i]]++;
         }
         return c;
         
