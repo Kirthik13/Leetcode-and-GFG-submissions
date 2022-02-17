b31f296623c21315fad65ec0385e7adb6942de9b
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-17 08:12:54 +0530

    Time: 8 ms (74.76%), Space: 10.9 MB (72.35%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void fn(vector<int>&v,int &k,vector<int> &sam,set<vector<int>>&tot,int idx,int sum)
+    void fn(vector<int>&v,int &k,vector<int> &sam,vector<vector<int>>&tot,int idx,int sum)
     {
         
         if(sum>k)
@@ -9,31 +9,22 @@ public:
             }
         if(sum==k)
             {
-                tot.insert(sam);
-            }
-        if(idx>=v.size())
-        {
-            // if(sum==k)
-            // {
-            //     tot.emplace_back(sam);
-            // }
+                tot.emplace_back(sam);
             return;
-        }
-        
-        sam.emplace_back(v[idx]);
-        fn(v,k,sam,tot,idx,sum+v[idx]);
+            }
+      
+        for(int j=idx;j<v.size();j++){
+            if(sum+v[j]<=k){
+        sam.emplace_back(v[j]);
+        fn(v,k,sam,tot,j,sum+v[j]);
         sam.pop_back();
-        fn(v,k,sam,tot,idx+1,sum);
+            }
+        }
     }
     vector<vector<int>> combinationSum(vector<int>& v, int k) {
-        set<vector<int>> tot;
+        vector<vector<int>> tot;
         vector<int>sam;
         fn(v,k,sam,tot,0,0);
-        vector<vector<int>>ans;
-        for(auto &i:tot)
-        {
-            ans.emplace_back(i);
-        }
-        return ans;
+        return tot;
     }
 };
