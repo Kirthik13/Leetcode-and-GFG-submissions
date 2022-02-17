a9d94e93df154e042b273bf9d4fc5b3a85ee42c5
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-17 08:49:09 +0530

    Time: 112 ms (5.06%), Space: 256.4 MB (7.94%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -16,6 +16,28 @@ public:
         if(s1.size()<s2.size()) swap(s1,s2);
         int n=s1.size(),m=s2.size();
         vector<vector<int>>dp(1001,vector<int>(1001,-1));
-       return fn(s1,s2,n,m,0,0,dp);
+        for(int i=0;i<=m;i++)
+        {
+            dp[n][i]=0;
+        }
+         for(int i=0;i<=n;i++)
+        {
+            dp[i][m]=0;
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=0;j--)
+            {
+                if(s1[i]==s2[j])
+                {
+                    dp[i][j]=1+dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        return dp[0][0];
+       // return fn(s1,s2,n,m,0,0,dp);
     }
 };
