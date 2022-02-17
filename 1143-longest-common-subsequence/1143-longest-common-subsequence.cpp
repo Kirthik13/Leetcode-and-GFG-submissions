65de855fd598fa4a62a904304279ac89cc0a28e7
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-17 08:49:26 +0530

    Time: 56 ms (14.31%), Space: 256.5 MB (7.94%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int fn(string &s1,string &s2,int n,int m,int i1,int i2,vector<vector<int>>&dp)
-    {
-        if(n==i1 or m==i2) return 0;
-        if(dp[i1][i2]!=-1)  return dp[i1][i2];
-        if(s1[i1]==s2[i2])
-        {
-            return dp[i1][i2]=1+fn(s1,s2,n,m,i1+1,i2+1,dp);
-        }
+//     int fn(string &s1,string &s2,int n,int m,int i1,int i2,vector<vector<int>>&dp)
+//     {
+//         if(n==i1 or m==i2) return 0;
+//         if(dp[i1][i2]!=-1)  return dp[i1][i2];
+//         if(s1[i1]==s2[i2])
+//         {
+//             return dp[i1][i2]=1+fn(s1,s2,n,m,i1+1,i2+1,dp);
+//         }
         
-           return dp[i1][i2]=max(fn(s1,s2,n,m,i1+1,i2,dp),fn(s1,s2,n,m,i1,i2+1,dp));
+//            return dp[i1][i2]=max(fn(s1,s2,n,m,i1+1,i2,dp),fn(s1,s2,n,m,i1,i2+1,dp));
         
-    }
+//     }
     int longestCommonSubsequence(string s1, string s2) {
         if(s1.size()<s2.size()) swap(s1,s2);
         int n=s1.size(),m=s2.size();
