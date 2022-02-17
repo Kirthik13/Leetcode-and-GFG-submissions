55c6d78da228add6d46f6dde872feb77bf3bdb4a
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-17 20:06:22 +0530

    Added solution - LeetHub

--- DFS of Graph - GFG/dfs-of-graph.cpp
+++ DFS of Graph - GFG/dfs-of-graph.cpp
@@ -6,28 +6,48 @@ using namespace std;
 class Solution {
   public:
     // Function to return a list containing the DFS traversal of the graph.
-    void dfs(vector<int>&tot,vector<int> adj[],int node,vector<int>&vis)
-    {
-        vis[node]=1;
-        tot.emplace_back(node);
-        for(auto i:adj[node])
-        {
-            if(!vis[i]){
-            dfs(tot,adj,i,vis);
-            }
-        }
+    // void dfs(vector<int>&tot,vector<int> adj[],int node,vector<int>&vis)
+    // {
+    //     vis[node]=1;
+    //     tot.emplace_back(node);
+    //     for(auto i:adj[node])
+    //     {
+    //         if(!vis[i]){
+    //         dfs(tot,adj,i,vis);
+    //         }
+    //     }
         
-    }
+    // }
     vector<int> dfsOfGraph(int V, vector<int> adj[]) {
         // Code here
         vector<int>tot;
         vector<int>vis(V,0);
-        for(int i=0;i<V;i++)
+        stack<int>st;
+        st.push(0);
+        vis[0]=1;
+            tot.emplace_back(0);
+        while(!st.empty())
         {
-            if(!vis[i]){
+            int node=st.top();
+           
+            int f=0;
+            if(!vis[node]){
+            tot.emplace_back(node);
+            }
+            vis[node]=1;
             
-                dfs(tot,adj,i,vis);
-            
+            for(auto &i:adj[node])
+            {
+                if(!vis[i])
+                {
+                    st.push(i);
+                    // vis[i]=1;
+                    f=1;
+                    break;
+                }
+            }
+            if(!f) {
+                st.pop();
             }
         }
         return tot;
