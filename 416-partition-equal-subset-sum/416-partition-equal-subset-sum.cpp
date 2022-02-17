081d1be8dc142385fb2ed79c1c4f6fcff4658151
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-17 17:23:01 +0530

    Time: 188 ms (55.64%), Space: 75.4 MB (20.03%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    bool fn2(vector<int>&v,int sum,int idx,int k,vector<int>&dp)
+    bool fn2(vector<int>&v,int sum,int idx,int k,vector<vector<int>>&dp)
     {
         if(sum==k) return 1;
         if(sum<k) return 0;
         if(idx>=v.size()) return 0;
-        if(dp[k]!=-1) return dp[k];
+        if(dp[idx][k]!=-1) return dp[idx][k];
         for(int i=idx;i<v.size();i++)
         {
             if(k+v[i]<=sum)
             {
                 if(fn2(v,sum,i+1,k+v[i],dp))
                 {
-                    return dp[k+v[i]]=1;
+                    return dp[i][k+v[i]]=1;
                 }
                 
             }
         }
-        return dp[k]=0;
+        return dp[idx][k]=0;
     }
     bool fn(vector<int>&v,int sum)
     {
-        vector<int>dp(sum+1,-1);
-       return fn2(v,sum,0,0,dp);
+     vector<vector<int>>dp(v.size(),vector<int>(sum+1,-1));
+        return fn2(v,sum,0,0,dp);
     }
     bool canPartition(vector<int>& v) {
         int sum=accumulate(begin(v),end(v),0);
