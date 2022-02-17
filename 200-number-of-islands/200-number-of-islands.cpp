8606582dec21ca4281d28515388a8f62d274d933
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-17 23:29:32 +0530

    Time: 55 ms (43.02%), Space: 18.2 MB (18.83%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -3,41 +3,65 @@ public:
     vector<int>dx={0,1,0,-1};
     vector<int>dy={1,0,-1,0};
 
-    void fn(vector<vector<char>>& v,int i,int j,  vector<vector<int>>&vis)
-    {
-        // if(i==v.size()) return;
-        // if(j==v[0].size()) return;
-        // if(i<0 or j<0) return;
-        if(v[i][j]=='0') return;
-        vis[i][j]=1;
-        if(v[i][j]=='1')
-        {
-            for(int s=0;s<4;s++)
-            {
-                int x=i+dx[s],y=j+dy[s];
+//     void fn(vector<vector<char>>& v,int i,int j,  vector<vector<int>>&vis)
+//     {
+//         // if(i==v.size()) return;
+//         // if(j==v[0].size()) return;
+//         // if(i<0 or j<0) return;
+//         if(v[i][j]=='0') return;
+//         vis[i][j]=1;
+//         if(v[i][j]=='1')
+//         {
+//             for(int s=0;s<4;s++)
+//             {
+//                 int x=i+dx[s],y=j+dy[s];
                 
-                if((x>=0 and x<=v.size()-1) and  (y>=0 and y<=v[0].size()-1))
-                {
-                    if(!vis[x][y] and v[x][y]=='1' )
-                    {
-                        fn(v,x,y,vis);
-                    }
-                }
-            }
-        }
-    }
+//                 if((x>=0 and x<=v.size()-1) and  (y>=0 and y<=v[0].size()-1))
+//                 {
+//                     if(!vis[x][y] and v[x][y]=='1' )
+//                     {
+//                         fn(v,x,y,vis);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+    
     int numIslands(vector<vector<char>>& v) {
         // fn(v,0,0);
         int ans{};
-        vector<vector<int>>vis(v.size(),vector<int>(v[0].size(),0));
+        // vector<vector<int>>vis(v.size(),vector<int>(v[0].size(),0));
         for(int i=0;i<v.size();i++)
         {
             for(int j=0;j<v[0].size();j++)
             {
-                if(v[i][j]=='1' and !vis[i][j])
+                if(v[i][j]=='1')
                 {
-                    fn(v,i,j,vis);
+                    // fn(v,i,j,vis);
+                    
                     ans++;
+                    queue<pair<int,int>>q;
+                    q.push({i,j});
+                    v[i][j]='0';
+                    while(!q.empty())
+                    {
+                        int i1=q.front().first;
+                        int j1=q.front().second;
+                        q.pop();
+                        for(int s=0;s<4;s++)
+                        {
+                            int x=i1+dx[s],y=j1+dy[s];
+
+                            if((x>=0 and x<=v.size()-1) and  (y>=0 and y<=v[0].size()-1) and v[x][y]=='1')
+                            {
+                                
+                                    q.push({x,y});
+                                    v[x][y]='0';
+
+                                
+                            }
+                        }
+                    }
                 }
             }
         }
