af64f2ebb03307da8a1be34c5d69e58c018b5c0f
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-23 12:46:41 +0530

    Time: 205 ms (72.79%), Space: 103.9 MB (80.27%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -11,70 +11,62 @@
  */
 class Solution {
 public:
-//      int rec(TreeNode *root,int &mx,int &mn,int &a)
+    int ans{};
+//     int fn(TreeNode* root,int &mn,int &mx)
 //     {
 //         if(!root) return 0;
         
-// 	// lmn,lmx will find the minimum and maximum node in the left subtree 
-// 	// similarly, rmn,rmx will find minimum and maximum node in the right subtree
-		
-//         int lmn,rmn,rmx,lmx;
+//         int lmn=INT_MAX,lmx=INT_MIN,rmn=INT_MAX,rmx=INT_MIN;
         
-//         lmn=rmn=INT_MAX;
-//         lmx=rmx=INT_MIN;
-		
-//         int l=rec(root->left,lmx,lmn,a);
-//         int r=rec(root->right,rmx,rmn,a);
+//         int l=fn(root->left,lmn,lmx);
+//         int r=fn(root->right,rmn,rmx);
         
 //         int val=l+r+root->val;
-    
-// 	// if the left maximum is less than and right minimum is greater than current node
-// 	// than the subtree rooted at current node is BST.
-		
-//         if((lmx<(root->val)) and (rmn>(root->val)))
+//         if(lmx<root->val and rmn>root->val)
 //         {
-//             a=max(a,val);
-//             mx=max(root->val,rmx);
+            
+//             // if(root->val==3)
+//             // {
+//             //     cout<<lmx<<" "<<rmn<<endl;
+//             //     cout<<"ASd";
+//             // }
+//             ans=max(ans,val);
+//            mx=max(root->val,rmx);
 //             mn=min(root->val,lmn);
 //         }
-//         else
-//         {
-//             mx=INT_MAX;
+//         else{
 //             mn=INT_MIN;
+//             mx=INT_MAX;
 //         }
-//         return val;        
+//         return val;
 //     }
-    
-//     int maxSumBST(TreeNode* root) {
-//         int a=0;
-//         int mx=INT_MIN,mn=INT_MAX;
-//         rec(root,mx,mn,a);
-//         return a;
-//     }
-    int ans{};
-    int fn(TreeNode* root,int &mn,int &mx)
-    {
-        if(!root) return 0;
-        
-        int lmn=INT_MAX,lmx=INT_MIN,rmn=INT_MAX,rmx=INT_MIN;
-        
-        int l=fn(root->left,lmn,lmx);
-        int r=fn(root->right,rmn,rmx);
-        
-        int val=l+r+root->val;
-        if(lmx<root->val and rmn>root->val)
+    // int maxSumBST(TreeNode* root) {
+    //     int mx=INT_MIN,mn=INT_MAX;
+    //     fn(root,mn,mx);
+    //     return ans;
+    // }
+        int fn(TreeNode* root,int &mn,int &mx)
         {
-            ans=max(ans,val);
-           mx=max(root->val,rmx);
-            mn=min(root->val,lmn);
+            if(!root) return 0;
+            int lmn,rmn,lmx,rmx;
+            lmn=rmn=INT_MAX;
+            lmx=rmx=INT_MIN;
+            int l=fn(root->left,lmn,lmx);
+            int r=fn(root->right,rmn,rmx);
+            int val=l+r+root->val;
+            if(lmx<root->val and rmn>root->val)
+            {
+                ans=max(ans,val);
+                mx=max(root->val,rmx);
+                mn=min(root->val,lmn);
+            }
+            else{
+                mn=INT_MIN;
+                mx=INT_MAX;
+            }
+          return val;
         }
-        else{
-            mn=INT_MIN;
-            mx=INT_MAX;
-        }
-        return val;
-    }
-    int maxSumBST(TreeNode* root) {
+      int maxSumBST(TreeNode* root) {
         int mx=INT_MIN,mn=INT_MAX;
         fn(root,mn,mx);
         return ans;
