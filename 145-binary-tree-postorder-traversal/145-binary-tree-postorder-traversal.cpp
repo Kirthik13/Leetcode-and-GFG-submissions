689ad84c3f18ae82ce15d1e3ccf10dee16d3ce7b
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-02-25 21:26:16 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (65.70%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -14,26 +14,55 @@ public:
     vector<int> postorderTraversal(TreeNode* root) {
         if(!root) return {};
         vector<int>v;
-        stack<TreeNode*>st1,st2;
-        st1.push(root);
-        while(!st1.empty())
+        // stack<TreeNode*>st1,st2;
+        // st1.push(root);
+        // while(!st1.empty())
+        // {
+        //     TreeNode* node=st1.top();
+        //     st1.pop();
+        //     st2.push(node);
+        //     if(node->left)
+        //     {
+        //         st1.push(node->left);
+        //     }
+        //     if(node->right)
+        //     {
+        //         st1.push(node->right);
+        //     }
+        // }
+        // while(!st2.empty())
+        // {
+        //     v.push_back(st2.top()->val);
+        //     st2.pop();
+        // }
+        stack<TreeNode*>st;
+        TreeNode* cur=root;
+        while(cur or !st.empty())
         {
-            TreeNode* node=st1.top();
-            st1.pop();
-            st2.push(node);
-            if(node->left)
+            if(cur)
             {
-                st1.push(node->left);
+                st.push(cur);
+                cur=cur->left;
             }
-            if(node->right)
-            {
-                st1.push(node->right);
+            else{
+                TreeNode* temp=st.top()->right;
+                if(!temp)
+                {
+                    temp=st.top();
+                    v.push_back(temp->val);
+                    st.pop();
+                    while(!st.empty() and temp==st.top()->right)
+                    {
+                        temp=st.top();
+                        st.pop();
+                        v.push_back(temp->val);
+                    }
+                        
+                }
+                else{
+                    cur=temp;
+                }
             }
-        }
-        while(!st2.empty())
-        {
-            v.push_back(st2.top()->val);
-            st2.pop();
         }
         return v;
     }
