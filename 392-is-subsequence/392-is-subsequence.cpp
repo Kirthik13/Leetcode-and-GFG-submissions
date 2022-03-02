6a2dc0904c5f61c8c134f2122e01d1bc27907132
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-03-02 19:12:10 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (19.62%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
-    int dp[101][10001];
-    bool fn(string s,string t,int idx1,int idx2)
-    {
-        if(idx1==s.size())
-        {
-            return 1;
-        }
-        if(idx2==t.size())
-        {
-            if(idx1==s.size()) return 1;
-            return 0;
-        }
-        if(dp[idx1][idx2]!=-1)
-        {
-            return dp[idx1][idx2];
-        }
-                bool g=0;
-
-        if(s[idx1]==t[idx2])
-        {
-            return dp[idx1][idx2]=fn(s,t,idx1+1,idx2+1);
-        }
-        else{
-        g=fn(s,t,idx1,idx2+1);
-        }
-        return dp[idx1][idx2]=g;
-    }
     bool isSubsequence(string s, string t) {
-    if(s.size()==0)
-    {
-        // if(t.size()==0) return 1;
-        return 1;
-    }
-    if(t.size()==0)
-    {
-        if(s.size()==0) return 1;
-        return 0;
-    }
-        memset(dp,-1,sizeof(dp));
-       return fn(s,t,0,0);
+        unordered_map<char,vector<int>>hash;
+        for(int i=0;i<t.size();i++)
+        {
+            hash[t[i]].emplace_back(i);
+        }
+        int pre=-1;
+        
+        for(auto i:s)
+        {
+            auto it=hash.find(i);
+            if(it==hash.end()) return false;
+            auto vec=it->second;
+            auto pos=upper_bound(begin(vec),end(vec),pre);
+            if(pos==vec.end()) return false;
+            pre=*pos;
+            
+        }
+        return true;
     }
 };
