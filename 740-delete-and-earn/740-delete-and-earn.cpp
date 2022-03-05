cc141737760a294b032160ccf0bf19db0db9e396
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-03-05 18:47:07 +0530

    Time: 4 ms (94.57%), Space: 9.2 MB (90.77%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& v) {
-        int freq[10002]={0},dp[10002]={0};
+        int freq[10001]={0},dp[10001]={0};
         for(auto &i:v)
         {
             freq[i]++;
         }
         dp[0]=0;
         dp[1]=freq[1];
-        for(int i=2;i<10002;i++)
+        for(int i=2;i<10001;i++)
         {
             dp[i]=max(dp[i-1],freq[i]*i+dp[i-2]);
         }
