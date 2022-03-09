f2ef267c62089b481f5a8ab93ea125364924e403
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-03-09 09:16:42 +0530

    Time: 7 ms (83.62%), Space: 11.1 MB (59.49%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -12,15 +12,16 @@ class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if(!head or !head->next) return head;
-        ListNode* h=head;
-        ListNode* p=head;
-        ListNode* ans=head;
-        unordered_map<int,int>m;
-        while(p)
-        {
-            m[p->val]++;
-            p=p->next;
-        }
+        // ListNode* h=head;
+        // ListNode* p=head;
+        // ListNode* ans=head;
+        // unordered_map<int,int>m;
+        // while(p)
+        // {
+        //     m[p->val]++;
+        //     p=p->next;
+        // }
+        
         // ListNode* dum=new ListNode();
         // ListNode* t=dum;
         // while(h)
@@ -37,28 +38,51 @@ public:
         
         
         
-         p=head;
-        int f=1;
-        while(h)
-        {
-            if(m[h->val]==1)
-            {
-                if(f)
-                {
-                    ans=h;
-                    f=0;
-                }
-                if(p!=h){
+//          p=head;
+//         int f=1;
+//         while(h)
+//         {
+//             if(m[h->val]==1)
+//             {
+//                 if(f)
+//                 {
+//                     ans=h;
+//                     f=0;
+//                 }
+//                 if(p!=h){
             
-                p->next=h;
-                p=p->next;
-                }
-            }
-            h=h->next;
-        }
-        p->next=NULL;
-        return f==1?NULL:ans;
+//                 p->next=h;
+//                 p=p->next;
+//                 }
+//             }
+//             h=h->next;
+//         }
+//         p->next=NULL;
+//         return f==1?NULL:ans;
         
+        ListNode* h=head;
+        ListNode* dum=new ListNode(0 == head->val ? 1 : 0);
+        ListNode* p=dum;
+        ListNode* first=dum;
+        while(h and h->next)
+        {
+            if(p->val!=h->val and h->val !=h->next->val)
+            {
+                first->next=h;
+                first=first->next;
+            }
+            p=h;
+            h=h->next;
+            
+            
+        }
+        if(p->val!=h->val)
+        {
+            first->next=h;
+                first=first->next;
+        }
+        first->next=NULL;
+        return dum->next;
         
     }
 };
