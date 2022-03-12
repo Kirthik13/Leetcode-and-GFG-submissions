fea2ab5dc76854afbc5173e175c85683ca93eeb8
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-03-12 18:05:09 +0530

    Time: 10 ms (35.62%), Space: 9.7 MB (85.31%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -3,13 +3,13 @@ public:
     vector<int> shuffle(vector<int>& v, int n) {
        for(int i=n;i<v.size();i++)
        {
-           v[i]=(v[i]*1024)+v[i-n];
+           v[i]=(v[i]*1001)+v[i-n];
        }
        int idx=0;
         for(int i=n;i<v.size();i++)
         {
-            v[idx]=v[i]%1024;
-            v[idx+1]=v[i]/1024;
+            v[idx]=v[i]%1001;
+            v[idx+1]=v[i]/1001;
             
             idx+=2;
         }
