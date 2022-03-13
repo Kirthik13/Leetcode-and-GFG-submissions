deb1bbeab5a7a8781a824423adb9543ae4a5c834
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-03-13 07:36:10 +0530

    Time: 5 ms (18.36%), Space: 6.4 MB (51.50%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,50 +1,48 @@
-int ord(char ch)
-{
-    if(ch=='(' or ch==')') return 1;
-    if(ch=='{' or ch=='}') return 2;
-    return 3;
-    
-
-}
-bool isopen(char ch)
-{
-    return ch=='(' or ch=='{' or ch=='[';
-}
-bool isclose(char ch)
-{
-    return ch==')' or ch=='}' or ch==']';
-}
 class Solution {
 public:
+    bool isopen(char c)
+    {
+        if(c=='(' or c=='[' or c=='{') return 1;
+        return 0;
+    }
+    int type(char c)
+    {
+        if(c=='(' or c==')') return 1;
+             if(c=='[' or c==']') return 2;
+        return 3;
+    }
     bool isValid(string s) {
         if(s.size()==1) return 0;
             
         stack<char>st;
-        for(auto &i:s)
-            
+        for(int i=0;i<s.size();i++)
         {
-            
             if(st.empty())
             {
-               
-                if(isclose(i)) return 0;
-                st.push(i);
+                if(isopen(s[i])) st.push(s[i]);
+                else{
+                return false;
+                }
             }
-            else{
-                if((ord(st.top())==ord(i)) and (isopen(st.top()) and isclose(i)))
+            else if(!st.empty())
+            {
+                if(!isopen(s[i]))
                 {
-                    
-                        // cout<<st.top()<<" "<<i<<endl;
+                    if(isopen(st.top()) and type(s[i])==type(st.top()))
+                    {
                         st.pop();
-                    // }
+                        // cout<<"ae";
+                    }
+                    else{
+                    return false;
+                    }
+                    
                 }
                 else{
-                    st.push(i);
-                }
+                        st.push(s[i]);
+                    }
             }
         }
-        // cout<<st.size();
         return st.empty();
-        
     }
 };
