7161b592f02b9fd33ce047f2f0f050c71b08d2ed
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-03-20 11:42:19 +0530

    Time: 248 ms (12.50%), Space: 18.9 MB (62.50%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -2,15 +2,41 @@ class Solution {
 public:
     int countCollisions(string s) {
         int c{};
-        int n=s.size(),i=0;
-        int carfromright{};
-        while(i<n and s[i]=='L') i++;
-        for(;i<n;i++)
+        int n=s.size();
+        
+        // int i{};
+        // int carfromright{};
+        // while(i<n and s[i]=='L') i++;
+        // for(;i<n;i++)
+        // {
+        //     if(s[i]=='R') ++carfromright;
+        //     else{
+        //         c+=(s[i]=='S')?carfromright:carfromright+1;
+        //         carfromright=0;
+        //     }
+        // }
+        
+        stack<char>st;
+        for(int i=0;i<n;i++)
         {
-            if(s[i]=='R') ++carfromright;
+            if(st.empty()) st.push(s[i]);
+            else if(st.top()=='S' and s[i]=='L') c++;
+            else if(st.top()=='R' and s[i]=='L')
+            {
+                s[i]='S';
+                c+=2;
+                st.pop();
+                i--;
+            }
+            else if(st.top()=='R' and s[i]=='S')
+            {
+                s[i]='S';
+                c++;
+                st.pop();
+                i--;
+            }
             else{
-                c+=(s[i]=='S')?carfromright:carfromright+1;
-                carfromright=0;
+                st.push(s[i]);
             }
         }
         return c;
