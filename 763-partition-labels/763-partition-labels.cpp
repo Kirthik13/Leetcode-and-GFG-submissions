4af02d7094b6655e1c61044ca69559df0c07d427
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-03-21 17:09:26 +0530

    Time: 8 ms (32.00%), Space: 6.7 MB (64.08%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -10,26 +10,38 @@ public:
         {
             m[s[i]]=i;
         }
-        int i=0;
-        while(i<n)
-        {
-            int st=i;
-            int en=m[s[i]];
-            int j=st+1;
-            while(j<en)
-            {
-                if(m[s[j]]<=en)
-                {
-                    j++;
-                }
-                else{
-                    en++;
-                }
-            }
+//         int i=0;
+//         while(i<n)
+//         {
+//             int st=i;
+//             int en=m[s[i]];
+//             int j=st+1;
+//             while(j<en)
+//             {
+//                 if(m[s[j]]<=en)
+//                 {
+//                     j++;
+//                 }
+//                 else{
+//                     en++;
+//                 }
+//             }
             
-           tot.push_back(en-st+1);
-            i=en+1;
+//            tot.push_back(en-st+1);
+//             i=en+1;
+//         }
+        int idx=INT_MIN;
+        int last_idx=0;
+        for(int i=0;i<n;i++)
+        {
+            idx=max(idx,m[s[i]]);
+            if(i==idx)
+            {
+                tot.push_back(i-last_idx+1);
+                last_idx=i+1;
+            }
         }
+            
         return tot;
     }
 };
