10564b963d91912c854aba4c706bf00f9b5a75c4
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-03-27 09:10:35 +0530

    Time: 7 ms (98.92%), Space: 10.6 MB (42.57%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,12 +1,33 @@
 class Solution {
 public:
+    int calsoliders(vector<int>&v)
+    {
+        int l=0,r=v.size()-1,ans=INT_MIN;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            
+            if(v[mid]==1)
+            {
+                l=mid+1;
+                ans=max(ans,mid+1);
+            }
+            else{
+                r=mid-1;
+            }
+                
+        }
+        return ans==INT_MIN?0:ans;
+    }
     vector<int> kWeakestRows(vector<vector<int>>& v, int k) {
         priority_queue<pair<int,int>>pq;
         int r=0;
         for(auto i=0;i<v.size();i++)
         {
             
-            auto s=accumulate(begin(v[i]),end(v[i]),0);
+            // auto s=accumulate(begin(v[i]),end(v[i]),0);
+            int s=calsoliders(v[i]);
+            // cout<<"s : "<<s<<endl;
             pq.push({s,i});
             if(pq.size()>k) pq.pop();
             // r++;
