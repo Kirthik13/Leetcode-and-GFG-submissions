dbdc205e844947e4a99110722d34640aa1d45135
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-04-01 07:15:33 +0530

    Time: 24 ms (76.05%), Space: 23.3 MB (41.72%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     void reverseString(vector<char>& v) {
-        std::reverse(begin(v),end(v));
+        int l=0,r=v.size()-1;
+        while(l<r)
+        {
+            swap(v[l++],v[r--]);
+        }
+        // return v;
+            
     }
 };
