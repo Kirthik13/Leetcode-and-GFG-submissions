f82382964a4e16031cbf98279b25f9b9cc122246
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-04-02 18:59:15 +0530

    Time: 180 ms (59.78%), Space: 65.6 MB (8.33%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& v) {
+        int ans=INT_MIN,st{};
+        map<int,int>m;
+        for(int i=0;i<v.size();i++)
+        {
+            
+            m[v[i]]++;
+            if(m.size()>2)
+            {
+                if(m[v[st]]==1)
+                {
+                    m.erase(v[st]);
+                }
+                else{
+                    m[v[st]]--;
+                }
+                st++;
+            }
+            
+            // cout<<st<<" "<<i-st+1<<endl;
+            ans=max(ans,i-st+1);
+        }
+                    // ans=max(ans,(int)v.size()-st+1);
+
+        return ans;
+    }
+};
