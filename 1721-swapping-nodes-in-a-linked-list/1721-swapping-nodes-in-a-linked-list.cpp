3a5b3aa6b8049f405a3bd6229eb7f278adae07ad
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-04-04 16:40:11 +0530

    Time: 1233 ms (8.93%), Space: 180.1 MB (57.49%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -13,38 +13,65 @@ public:
     ListNode* swapNodes(ListNode* head, int k) {
         if(!head->next) return head;
         
-        ListNode* h1=head;
-        int k1=0,len{};
-                ListNode* ans1=head;
-                ListNode* ans2=head;
+//         ListNode* h1=head;
+//         int k1=0,len{};
+//                 ListNode* ans1=head;
+//                 ListNode* ans2=head;
 
+//         while(h1)
+//         {
+//             k1++;
+//             if(k1==k)
+//             {
+//                 ans1=h1;
+//             }
+//             h1=h1->next;
+            
+//             len++;    
+//         }
+//         k=len-k+1;
+//         k1=0;
+//         h1=head;
+//          while(h1)
+//         {
+//             k1++;
+//             if(k1==k)
+//             {
+//                 ans2=h1;
+//             }
+//             h1=h1->next;
+            
+//             // len++;    
+//         }
+//         // cout<<ans1->val<<" "<<ans2->val<<endl;
+//         swap(ans1->val,ans2->val);
+//         return head;
+        
+        
+        
+        ListNode* ans1=head;
+            ListNode* ans2=head;
+        ListNode* h1=head;
+        int f=0;
         while(h1)
         {
-            k1++;
-            if(k1==k)
+               // ans2 = ans2 == nullptr ? nullptr :ans2->next;
+            if(--k==0)
             {
                 ans1=h1;
+                ans2=head;
+                f=1;
             }
-            h1=h1->next;
             
-            len++;    
-        }
-        k=len-k+1;
-        k1=0;
-        h1=head;
-         while(h1)
-        {
-            k1++;
-            if(k1==k)
-            {
-                ans2=h1;
+            h1=h1->next;
+            if(!f){
+            ans2=ans2->next;
             }
-            h1=h1->next;
+            f=0;
             
-            // len++;    
         }
-        // cout<<ans1->val<<" "<<ans2->val<<endl;
         swap(ans1->val,ans2->val);
         return head;
+        
     }
 };
