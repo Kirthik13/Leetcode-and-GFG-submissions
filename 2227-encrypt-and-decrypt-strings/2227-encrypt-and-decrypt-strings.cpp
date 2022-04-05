65989a271f287c8765fbb5e271d523d0a8e556b3
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-04-05 17:59:37 +0530

    Time: 313 ms (59.81%), Space: 91.8 MB (52.49%) - LeetHub

---
+++ 2227-encrypt-and-decrypt-strings/2227-encrypt-and-decrypt-strings.cpp
@@ -0,0 +1,90 @@
+class Encrypter {
+public:
+    map<char,string>m1;
+    // map<string,vector<char>>m2;
+    unordered_map<string,int>dic;
+
+//     void fn(string &s2,string sam,int i,int &ans,unordered_set<string>&vis)
+//     {
+//         if(i>=s2.size())
+//         {
+//             if(dic.find(sam)!=dic.end()) 
+//             {
+//                 if(vis.find(sam)==vis.end())
+//                 {
+//                     // cout<<"sam : "<<sam<<endl;
+//                     ++ans;
+//                     vis.insert(sam);
+//                 }
+//             }
+//             return;
+//         }
+    
+//             string dum=s2.substr(i,2);
+//             // fn(dum);
+// //             for(int j=0;j<m2[dum].size();j++)
+// //             {
+                
+// //             }
+//         for(auto j:m2[dum])
+//         {
+//             sam.push_back(j);
+//             fn(s2,sam,i+2,ans,vis);
+//             sam.pop_back();
+//         }
+        
+//     }
+    
+    Encrypter(vector<char>& keys, vector<string>& values, vector<string>& dictionary) {
+        for(int i=0;i<keys.size();i++)
+        {
+            m1[keys[i]]=values[i];
+            // m2[values[i]].push_back(keys[i]);
+        }
+        
+        for(auto &i:dictionary)
+        {
+            string sam;
+            int f=1;
+            for(auto &j:i){
+                // dic.insert(i);
+                if(m1.count(j)==0)
+                {
+                    f=0;
+                    break;
+                }
+                sam+=(m1[j]);
+            }
+            if(f){
+            dic[sam]++;
+            }
+            // cout<<"Sam : "<<sam<<endl;
+        }
+    }
+    
+    string encrypt(string s1) {
+        string ans;
+        for(auto &i:s1)
+        {
+            ans+=(m1[i]);
+        }
+        return ans;
+    }
+    
+    
+    int decrypt(string s2) {
+        // int ans{};
+        // unordered_set<string>vis;
+        //  fn(s2,"",0,ans,vis);
+        // return ans;
+        // cout<<"s2 : "<<s2<<endl;
+        return dic[s2];
+    }
+};
+
+/**
+ * Your Encrypter object will be instantiated and called as such:
+ * Encrypter* obj = new Encrypter(keys, values, dictionary);
+ * string param_1 = obj->encrypt(word1);
+ * int param_2 = obj->decrypt(word2);
+ */
