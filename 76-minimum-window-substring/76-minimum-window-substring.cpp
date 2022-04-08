e29a20dc9261b75abc9b08c709b6ab99fc0c3625
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-04-08 12:04:12 +0530

    Time: 31 ms (44.98%), Space: 7.8 MB (66.77%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -8,38 +8,68 @@ public:
             m1[i]++;
         }
         
-        int count=t.size();
+//         int count=t.size();
         
-        int l{},r{},minlen=INT_MAX,head{};
-        string ans;
+//         int l{},r{},minlen=INT_MAX,head{};
+//         string ans;
+        
+//         while(r<s.size())
+//         {
+//             if(m1.count(s[r]) and m1[s[r]]>0)
+//             {
+//                 count--;
+//             }
+//             m1[s[r]]--;
+
+//             while(count==0 )
+//             {
+//                 if(minlen>r-l+1)
+//                 {
+//                     head=l;
+//                     minlen=r-l+1;
+//                 }
+//                 m1[s[l]]++;
+
+//                 if(m1[s[l]]>0) count++;
+
+//                 l++;
+//             }
+//                         r++;
+
+            
+            
+//         }
+//         return minlen==INT_MAX?"":s.substr(head,minlen);
+        
+        
+        
+    
+        int win=t.size();
+        int l{},r{},minlen=INT_MAX,head{},count=win;
         
         while(r<s.size())
         {
-            if(m1.count(s[r]) and m1[s[r]]>0)
+            if(m1[s[r]]>0)
             {
                 count--;
             }
             m1[s[r]]--;
-
-            while(count==0 )
+            
+            while(count==0)
             {
                 if(minlen>r-l+1)
                 {
                     head=l;
                     minlen=r-l+1;
                 }
+                                if(m1[s[l]]>=0) count++;
                 m1[s[l]]++;
 
-                if(m1[s[l]]>0) count++;
-
                 l++;
+                
             }
-                        r++;
-
-            
-            
+            r++;
         }
         return minlen==INT_MAX?"":s.substr(head,minlen);
-        
     }
 };
