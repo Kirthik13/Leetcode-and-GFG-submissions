23eb76c33a3b8e7a6f09063964f9687f776baf8e
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-04-16 13:05:23 +0530

    Time: 83 ms (59.68%), Space: 28.4 MB (81.22%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void rand_shuf(vector<int>&v)
+    {
+        int n=v.size();
+        for(int i=n-1;i>0;i--)
+        {
+            int j=rand()%i;
+            swap(v[i],v[j]);
+        }
+        
+    }
+    int partition(vector<int>&v,int l,int r)
+    {
+        int pivot=v[r];
+        int pidx=l;
+        for(int i=l;i<r;i++)
+        {
+            if(v[i]<=pivot)
+            {
+                swap(v[i],v[pidx]);
+                pidx++;
+            }
+        }
+        swap(v[pidx],v[r]);
+        return pidx;
+    }
+    void quicksort(vector<int>&v,int l,int r)
+    {
+        if(l<r)
+        {
+            int pidx=partition(v,l,r);
+            quicksort(v,l,pidx-1);
+            quicksort(v,pidx+1,r);
+
+        }
+    }
+    vector<int> sortArray(vector<int>& v) {
+        rand_shuf(v);
+        quicksort(v,0,v.size()-1);
+        return v;
+    }
+};
