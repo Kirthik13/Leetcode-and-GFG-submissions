850287e2d3a2c1ee4046bc334d2a66f782b2852b
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-04-16 16:34:46 +0530

    Time: 107 ms (64.40%), Space: 20 MB (67.43%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -5,27 +5,44 @@ public:
         
         sort(begin(v),end(v));
         
-        for(int i=0;i<v.size();i++)
+        for(unsigned int i=0;i<v.size();i++)
         {
             if(i>0 and v[i-1]==v[i]) continue;
             int l=i+1,r=v.size()-1;
             while(l<r)
             {
-                if(v[l]+v[i]+v[r]==0)
-                {
-                    tot.push_back({v[i],v[l],v[r]});
-                    // break;
-                    int l1=v[l],r1=v[r];
-                    while(l1==v[l] and l<r) l++;
-                    while(r1==v[r] and r>i) r--;
-                }
-                else if(v[l]+v[r]+v[i]>0)
+                int s=v[i]+v[l]+v[r];
+                if(s>0)
                 {
                     r--;
                 }
-                else{
+                else if(s<0)
+                {
                     l++;
                 }
+                else{
+                    tot.push_back(vector<int>{v[i],v[l],v[r]});
+                    while(v[l]==v[l+1] and l+1<r) l++;
+                    while(v[r]==v[r-1] and l<r-1) r--;
+                    l++;
+                    r--;
+
+                }
+                // if(v[l]+v[i]+v[r]==0)
+                // {
+                //     tot.push_back({v[i],v[l],v[r]});
+                //     // break;
+                //     int l1=v[l],r1=v[r];
+                //     while(l1==v[l] and l<r) l++;
+                //     while(r1==v[r] and r>i) r--;
+                // }
+                // else if(v[l]+v[r]+v[i]>0)
+                // {
+                //     r--;
+                // }
+                // else{
+                //     l++;
+                // }
             }
             
         }
