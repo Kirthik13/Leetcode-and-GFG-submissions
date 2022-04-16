b82325a7259306fb52f9dbe9f937b8167f83def0
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-04-16 13:07:00 +0530

    Time: 78 ms (64.05%), Space: 28.5 MB (81.22%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     void rand_shuf(vector<int>&v)
     {
+        srand(time(NULL));
         int n=v.size();
         for(int i=n-1;i>0;i--)
         {
-            int j=rand()%(i+1);
+            int j=rand()%(i);
             swap(v[i],v[j]);
         }
         
