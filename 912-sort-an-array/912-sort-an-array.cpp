1a3f98660df77aac5bbd9a9ecb8ee0a640e81ca4
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-04-16 13:05:43 +0530

    Time: 74 ms (68.32%), Space: 28.6 MB (53.82%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -5,7 +5,7 @@ public:
         int n=v.size();
         for(int i=n-1;i>0;i--)
         {
-            int j=rand()%i;
+            int j=rand()%(i+1);
             swap(v[i],v[j]);
         }
         
