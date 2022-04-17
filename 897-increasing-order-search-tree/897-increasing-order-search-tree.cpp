6df6ec76a0b6b39e818b5e67acc48618214ecdb2
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-04-17 09:53:55 +0530

    Time: 3 ms (65.44%), Space: 8 MB (19.70%) - LeetHub

---
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>v;
+    void fn(TreeNode* root)
+    {
+        if(!root) return;
+        
+        fn(root->left);
+        // TreeNode* g=root;
+//         TreeNode* l=root->left;
+//         TreeNode* r=root->right;
+
+//         g->left=NULL;
+//         g->right=NULL;
+
+        v.push_back(root->val);
+        // g->left=l;
+        // g->right=r;
+        fn(root->right);
+            
+    }
+    TreeNode* increasingBST(TreeNode* root) {
+        if(!root ) return root;
+        
+        fn(root);
+        vector<TreeNode*>v2;
+       
+        for(int i=0;i<v.size();i++)
+        {
+     
+            TreeNode* g=new TreeNode(v[i]);
+            v2.push_back(g);
+            
+        }
+        for(int i=0;i<v2.size()-1;i++)
+        {
+            if(i==0) root=v2[i];
+            v2[i]->right=v2[i+1];
+        }
+       
+       return root;
+    }
+};
