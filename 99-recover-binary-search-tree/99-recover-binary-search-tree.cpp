a201af1ddcfade41c176ce93ef9d273856cdd52a
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-04-19 17:56:52 +0530

    Time: 71 ms (12.43%), Space: 57.9 MB (47.12%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -11,30 +11,57 @@
  */
 class Solution {
 public:
-    void fn1(TreeNode* root,multiset<int>&v)
-    {
-        if(!root) return;
+//     void fn1(TreeNode* root,multiset<int>&v)
+//     {
+//         if(!root) return;
         
-        fn1(root->left,v);
-        v.insert(root->val);
-        fn1(root->right,v);
+//         fn1(root->left,v);
+//         v.insert(root->val);
+//         fn1(root->right,v);
 
             
-    }
-    void fn2(TreeNode* &root,multiset<int>&v)
+//     }
+//     void fn2(TreeNode* &root,multiset<int>&v)
+//     {
+//         if(!root) return;
+        
+//         fn2(root->left,v);
+        
+//         root->val=*v.begin();
+//         v.erase(v.begin());
+//         fn2(root->right,v);
+//     }
+    
+    void fn(TreeNode* root,TreeNode* &first,TreeNode* &end,TreeNode* &prev)
     {
         if(!root) return;
         
-        fn2(root->left,v);
+        fn(root->left,first,end,prev);
         
-        root->val=*v.begin();
-        v.erase(v.begin());
-        fn2(root->right,v);
+        if(prev)
+        {
+            if(prev->val>root->val)
+            {
+                if(!first)
+                {
+                    first=prev;
+                    
+                }
+                end=root;
+            }
+        }
+        prev=root;
+        
+        fn(root->right,first,end,prev);
     }
     void recoverTree(TreeNode* root) {
-        multiset<int>v;
-        fn1(root,v);
-        fn2(root,v);
+        // multiset<int>v;
+        // fn1(root,v);
+        // fn2(root,v);
+        TreeNode* first=NULL;TreeNode* end=NULL;TreeNode* prev=NULL;
+        fn(root,first,end,prev);
+        swap(first->val, end->val);
+       
 
     }
 };
