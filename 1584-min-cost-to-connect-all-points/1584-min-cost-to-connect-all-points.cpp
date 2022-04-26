1745c83d8bbf568e873d4c721d01c93c36441812
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-04-26 18:52:38 +0530

    Time: 616 ms (51.31%), Space: 192.9 MB (8.48%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& graph) {
+        vector<vector<pair<int,int>>>dist(graph.size());
+        for(int i=0;i<graph.size();i++)
+        {
+            for(int j=i+1;j<graph.size();j++)
+            {
+                int len=abs(abs(graph[i][0]-graph[j][0])+abs(graph[i][1]-graph[j][1]));
+                dist[i].push_back({j,len});
+                dist[j].push_back({i,len});
+            }
+        }
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        vector<int>vis(graph.size(),0);
+        pq.push({0,{0,0}});
+        int cost{};
+        while(!pq.empty())
+        {
+                int curr_node=pq.top().second.second;
+                int g=accumulate(begin(vis),end(vis),0);
+            if(g==vis.size()) break;
+                if(vis[curr_node]==0)
+                {
+                    cost+=pq.top().first;
+                    // cout<<pq.top().first<<" "<<pq.top().second.first<<" " <<pq.top().second.second<<endl;
+                    vis[curr_node]=1;
+                    pq.pop();
+
+                    for(auto &i:dist[curr_node])
+                    {
+                        pq.push({i.second,{curr_node,i.first}});
+                        // cout<<curr_node<<" "<<i.first<<" "<<i.second<<endl;
+                    }
+                    
+                
+                    
+                    
+                }
+            else{
+                pq.pop();
+            }
+        }
+        return cost;
+    }
+};
