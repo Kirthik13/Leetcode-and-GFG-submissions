a13aafac7dbc3423ef661338870edc724a232c9a
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-04-29 08:47:25 +0530

    Time: 45 ms (21.74%), Space: 14.4 MB (17.74%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph, vector<int>&vis,int prev,int node)
+    bool bfs(vector<vector<int>>& graph, vector<int>&vis,int node)
     {
-        vis[node]=prev;
-        for(auto &i:graph[node])
+        // vis[node]=prev;
+        // for(auto &i:graph[node])
+        // {
+        //     if(!vis[i])
+        //     {
+        //         if(!dfs(graph,vis,1-prev,i)) return 0;
+        //     }
+        //     else{
+        //         if(prev==vis[i]) return 0;
+        //     }
+        // }
+        // return 1;
+        
+        queue<int>q;
+        q.push(node);
+        vis[node]=0;
+        while(!q.empty())
         {
-            if(!vis[i])
+            int el=q.front();
+            q.pop();
+            for(auto &i:graph[el])
             {
-                if(!dfs(graph,vis,1-prev,i)) return 0;
-            }
-            else{
-                if(prev==vis[i]) return 0;
+                if(!vis[i])
+                {
+                    vis[i]=1-vis[el];
+                    q.push(i);
+                }
+                else{
+                    if(vis[el]==vis[i]) return 0;
+                }
             }
         }
-        return 1;
+        return  1;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         
@@ -24,7 +45,7 @@ public:
         {
             if(!vis[i])
             {
-                if(!dfs(graph,vis,0,i)) return 0;
+                if(!bfs(graph,vis,i)) return 0;
             }
         }
         return 1;
