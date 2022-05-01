191c825367126fbbeddc43557ff71f439a930b53
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-05-01 10:51:00 +0530

    Time: 2 ms (47.87%), Space: 6.3 MB (39.15%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,37 +1,51 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char>s1,s2;
+//         stack<char>s1,s2;
         
-        for(auto &i:s)
-        {
-            if(isalpha(i)) s1.push(i);
-            else
-            {
-                if(!s1.empty()) s1.pop();
+//         for(auto &i:s)
+//         {
+//             if(isalpha(i)) s1.push(i);
+//             else
+//             {
+//                 if(!s1.empty()) s1.pop();
+//             }
+//         }
+//         for(auto &i:t)
+//         {
+//             if(isalpha(i)) s2.push(i);
+//             else
+//             {
+//                 if(!s2.empty()) s2.pop();
+//             }
+//         }
+      
+//         if(s1==s2) return 1;
+//         return 0;
+        
+        int i=s.size()-1,j=t.size()-1;
+      while(1)
+      {
+           int back = 0;
+            while (i >= 0 && (back > 0 || s[i] == '#')) {
+                back += s[i] == '#' ? 1 : -1;
+                i--;
             }
-        }
-        for(auto &i:t)
-        {
-            if(isalpha(i)) s2.push(i);
-            else
-            {
-                if(!s2.empty()) s2.pop();
+            back = 0;
+            while (j >= 0 && (back > 0 || t[j] == '#')) {
+                back += t[j] == '#' ? 1 : -1;
+                j--;
             }
-        }
-        // while(!s1.empty())
-        // {
-        //     cout<<s1.top()<<" ";
-        //     s1.pop();
-        // }
-        // cout<<endl;
-        //  while(!s2.empty())
-        // {
-        //     cout<<s2.top()<<" ";
-        //     s2.pop();
-        // }
-        // cout<<"next : "<<endl;
-        if(s1==s2) return 1;
-        return 0;
+             if (i >= 0 && j >= 0 && s[i] == t[j]) {
+                i--;
+                j--;
+            } else {
+                break;
+            }
+          
+      }
+        
+        return i==-1 and j==-1;
+
     }
 };
