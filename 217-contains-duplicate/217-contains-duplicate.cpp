314fa457e20204e32a1086749560f5d188b48861
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-05-07 00:11:38 +0530

    Time: 109 ms (70.06%), Space: 46.6 MB (79.89%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& v) {
-        unordered_map<int,int>m;
-        for(auto &i:v)
+        // unordered_map<int,int>m;
+        // for(auto &i:v)
+        // {
+        //     m[i]++;
+        //     if(m[i]>=2) return 1;
+        // }
+        // return 0;
+        
+        sort(begin(v),end(v));
+        for(int i=1;i<v.size();i++)
         {
-            m[i]++;
-            if(m[i]>=2) return 1;
+            if(v[i-1]==v[i]) return 1;
         }
         return 0;
     }
