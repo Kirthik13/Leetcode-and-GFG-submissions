7e0f3c75ee1e681df04c99d866533934811c8249
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-05-06 19:55:25 +0530

    Time: 22 ms (74.52%), Space: 10.9 MB (36.83%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -2,40 +2,46 @@ class Solution {
 public:
     string removeDuplicates(string s, int k) {
      string ans;
-        stack<pair<int,char>>st;
-        for(auto i:s)
-        {
-            if(st.empty())
-            {
-                st.push({1,i});
-            }
-            else{
-                if(st.top().second==i)
-                {
-                    st.top().first++;
-                    if(st.top().first==k) 
-                    {
-                        st.pop();
-                    }
-                }
-                else{
-                    st.push({1,i});
+//         stack<pair<int,char>>st;
+//         for(auto i:s)
+//         {
+//             if(st.empty())
+//             {
+//                 st.push({1,i});
+//             }
+//             else{
+//                 if(st.top().second==i)
+//                 {
+//                     st.top().first++;
+//                     if(st.top().first==k) 
+//                     {
+//                         st.pop();
+//                     }
+//                 }
+//                 else{
+//                     st.push({1,i});
 
-                }
+//                 }
                 
-            }
-        }
-        while(!st.empty())
+//             }
+//         }
+//         while(!st.empty())
+//         {
+//             char ch=st.top().second;
+//             int nos=st.top().first;
+//                ans.append(p.first, p.second);
+//             st.pop();
+//         }
+        vector<int>cnt(s.size());
+        int i{},j{};
+        while(j<s.size())
         {
-            char ch=st.top().second;
-            int nos=st.top().first;
-            // while(nos--)
-            // {
-                ans.insert(ans.begin(),nos,ch);
-            // }
-            st.pop();
+            s[i]=s[j];
+            cnt[i]=(i>0 and s[i-1]==s[j])?cnt[i-1]+1:1;
+            if(cnt[i]==k) i-=k;
+            i++;
+            j++;
         }
-        // std::reverse(begin(ans),end(ans));
-        return ans;
+        return s.substr(0,i);
     }
 };
