c7fae0c27e81ba50f67bee7499fabc8953725706
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-05-12 00:34:40 +0530

    Time: 58 ms (77.29%), Space: 15.2 MB (72.50%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.cpp
+++ 2266-count-number-of-texts/2266-count-number-of-texts.cpp
@@ -1,30 +1,47 @@
 #define mod 1000000007
 class Solution {
 public:
-    unordered_map<int,int>dp;
-    int fn(int idx,string &s,vector<int>&keys)
-    {
-        if(idx==s.size())
-        {
-            return 1;
-        }
+//     unordered_map<int,int>dp;
+//     int fn(int idx,string &s,vector<int>&keys)
+//     {
+//         if(idx==s.size())
+//         {
+//             return 1;
+//         }
         
-        if(dp.find(idx)!=dp.end()) return dp[idx];
+//         if(dp.find(idx)!=dp.end()) return dp[idx];
         
-        int num=int(s[idx])-'0';
-        int but=keys[num];
-        int ans{};
+//         int num=int(s[idx])-'0';
+//         int but=keys[num];
+//         int ans{};
       
-        for(int j=idx;j<idx+but and j<s.size() and s[j]==s[idx];j++)
-        {
-            ans+=fn(j+1,s,keys);
-            ans%=mod;
-        }
+//         for(int j=idx;j<idx+but and j<s.size() and s[j]==s[idx];j++)
+//         {
+//             ans+=fn(j+1,s,keys);
+//             ans%=mod;
+//         }
         
-        return dp[idx]=ans;
-    }
+//         return dp[idx]=ans;
+//     }
     int countTexts(string s) {
-        vector<int>keys={ 0, 0, 3, 3, 3, 3, 3, 4, 3, 4 };
-        return fn(0,s,keys);
+        // vector<int>keys={ 0, 0, 3, 3, 3, 3, 3, 4, 3, 4 };
+        // return fn(0,s,keys);
+        int n=s.size();
+        vector<int>dp(n+1);
+        dp[0]=1;
+        for(int i=1;i<=n;i++)
+        {
+            dp[i]=dp[i-1];
+            char ch=s[i-1];
+            if(i>=2 and s[i-2]==ch) dp[i]=(dp[i]+dp[i-2])%mod;
+            else continue;
+            if(i>=3 and s[i-3]==ch) dp[i]=(dp[i]+dp[i-3])%mod;
+            else continue;
+    
+            if(i>=4 and s[i-4]==ch and (ch=='7' or ch=='9')) dp[i]=(dp[i]+dp[i-4])%mod;
+     
+
+        }
+        return dp[n]%mod;
     }
 };
