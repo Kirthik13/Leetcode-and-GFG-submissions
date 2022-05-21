636471fcab77c8fe137e2c42db58e3fb48579b37
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-05-21 07:51:43 +0530

    Time: 106 ms (60.47%), Space: 10.7 MB (79.56%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    int dp[10001][13];
-    int fn(vector<int>& v, int amt,int n)
-    {
-        if(n==0 or amt==0)
-        {
-            if(amt==0) return 0;
-            return INT_MAX;
-        }
-        if(dp[amt][n]!=-1) return dp[amt][n];
-        
-        if(v[n-1]>amt)
-        {
-            return dp[amt][n]=fn(v,amt,n-1);
-        }
-        int l=fn(v,amt-v[n-1],n);
-        if(l==INT_MAX)
-        {
-         return dp[amt][n]=  fn(v,amt,n-1);
-        }
-        return dp[amt][n]=min(1+l,fn(v,amt,n-1));
-        
-        
-        
-    }
-    int coinChange(vector<int>& v, int amt) {
-            memset(dp,-1,sizeof(dp));
+    // int ans=INT_MIN;
+    // map<pair<int,int>,int>dp;
     
-        int ans=fn(v,amt,v.size());
-       return ans==INT_MAX?-1:ans;
+    int dp[10001][13];
+   int fn(vector<int>& v, int amt,int n)
+   {
+       if(n==-1 or amt==0)
+       {
+           if(amt==0) return 0;
+           return INT_MAX;
+       }
+       if(dp[amt][n]!=-1) return dp[amt][n];
+       if(v[n]>amt)
+       {
+            return dp[amt][n]=fn(v,amt,n-1);
+
+       }
+       int l=fn(v,amt-v[n],n);
+       if(l==INT_MAX)
+       {
+            return dp[amt][n]=fn(v,amt,n-1);
+       }
+       
+       return dp[amt][n]=min(1+l,fn(v,amt,n-1));
+   }
+    int coinChange(vector<int>& v, int amt) {
+        memset(dp,-1,sizeof(dp));
+        int g=fn(v,amt,v.size()-1);
+        return g==INT_MAX?-1:g;
     }
 };
