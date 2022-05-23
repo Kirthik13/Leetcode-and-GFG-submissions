55c19b7ee441529b9f2c120661a96fdcb2d86d4e
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-05-23 14:49:52 +0530

    Time: 706 ms (17.49%), Space: 36.3 MB (37.85%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    int ans=INT_MIN;
   int dp[601][101][101];
     int fn(vector<string>& v, int &max_0, int &max_1, unordered_map<int,pair<int,int>>&m,int i,int curr_z,int curr_one,int st)
     {
@@ -8,28 +7,23 @@ public:
         
         if(i>=v.size())
         {
-               if(curr_z<=max_0 and curr_one<=max_1)
-               {
-                   int siz=st;
-                   ans=max(ans,siz);
-               }
+            
             return 0;
         }
-        // if(dp.find({i,{curr_z,curr_one}})!=dp.end()) return   dp[{i,{curr_z,curr_one}}];
+       
         if(dp[i][curr_z][curr_one]!=-1) return dp[i][curr_z][curr_one];
-        // st++;
-        int l=1+fn(v,max_0,max_1,m,i+1,curr_z+m[i].first,curr_one+m[i].second,st);
-        // if(st>0){
-        // st--;
-        // }
+        
+        int l=1+fn(v,max_0,max_1,m,i+1,curr_z+m[i].first,curr_one+m[i].second,st);      
         int r=fn(v,max_0,max_1,m,i+1,curr_z,curr_one,st);
         
-       return dp[i][curr_z][curr_one]=max(l,r);
+        return dp[i][curr_z][curr_one]=max(l,r);
 
         
     }
     int findMaxForm(vector<string>& v, int max_0, int max_1) {
+        
         unordered_map<int,pair<int,int>>m;
+        
         for(int i=0;i<v.size();i++)
         {
             int z{},o{};
@@ -42,10 +36,11 @@ public:
             }
             m[i]={z,o};
         }
-        // vector<string>st;
-        int st{};
+        
         memset(dp,-1,sizeof(dp));
-        int g=fn(v,max_0,max_1,m,0,0,0,st);
+        
+        int g=fn(v,max_0,max_1,m,0,0,0,0);
+        
         return g;
     }
 };
