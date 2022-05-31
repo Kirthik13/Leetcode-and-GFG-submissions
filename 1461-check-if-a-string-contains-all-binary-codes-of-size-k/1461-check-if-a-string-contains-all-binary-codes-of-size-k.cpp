c3f910c82505abe79b0fff74b872b0592ccb33c7
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-05-31 08:43:02 +0530

    Time: 1491 ms (6.28%), Space: 115.5 MB (5.66%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool fn(set<string>&st2,int &win,string &t)
+    bool fn(set<string>&st2,int &win,string t)
     {
         if(t.size()==win)
         {
@@ -10,19 +10,19 @@ public:
         }
         // for(int i=0;i<win;i++)
         // {
-            t.push_back('0');
-            if(!(fn(st2,win,t))) return 0;
-            if(t.size()>0)
-            {
-                t.pop_back();
-            }
+            // t.push_back('0');
+            if(!(fn(st2,win,t+'0'))) return 0;
+            // if(t.size()>0)
+            // {
+            //     t.pop_back();
+            // }
             
-            t.push_back('1');
-             if(!(fn(st2,win,t))) return 0;
-            if(t.size()>0)
-            {
-                t.pop_back();
-            }
+            // t.push_back('1');
+             if(!(fn(st2,win,t+'1'))) return 0;
+            // if(t.size()>0)
+            // {
+            //     t.pop_back();
+            // }
         // }
         return 1;
     }
