77f63849e13c38fe8015fe6b80757abb4e17d437
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-06-11 09:20:44 +0530

    Time: 3 ms (97.08%), Space: 8.6 MB (72.29%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -22,46 +22,30 @@ public:
 class Solution {
 public:
     unordered_map<int,Node*>m;
-    //dfs
-    
-    // Node* cre(Node* node)
-    // {
-    //     if(m.find(node->val)!=m.end())
-    //     {
-    //         return m[node->val];
-    //     }
-    //     Node* newnode=new Node(node->val);
-    //     m[node->val]=newnode;
-    //     for(auto it:node->neighbors)
-    //     {
-    //         newnode->neighbors.push_back(cre(it));
-    //     }
-    //     return newnode;
-    // }
-    
     Node* cloneGraph(Node* node) {
-        if(!node) return NULL;
+        if(!node) return node;
         queue<Node*>q;
-        q.push(node);
-        m[node->val]= new Node(node->val);
+       q.push(node);
+                    m[node->val]=new Node(node->val);
+
         while(!q.empty())
         {
-            Node* curr=q.front();
+            Node* el=q.front();
             q.pop();
-            for(auto it:curr->neighbors)
+
+            
+            for(auto &it:el->neighbors)
             {
                 if(m.find(it->val)==m.end())
                 {
-                    Node* newnode=new Node(it->val);
-                    m[newnode->val] = newnode;
+                    m[it->val]=new Node(it->val);
+                                    q.push(it);
 
-                    q.push(it);
                 }
-                m[curr->val]->neighbors.push_back(m[it->val]);
+                m[el->val]->neighbors.push_back(m[it->val]);
             }
         }
-        //dfs
+
         return m[node->val];
-        // return cre(node);
     }
 };
