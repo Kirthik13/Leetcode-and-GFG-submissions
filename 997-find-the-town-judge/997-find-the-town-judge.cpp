a173260ae122a7aef8de1fb7ada3b295edbc4d25
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-06-11 09:39:52 +0530

    Time: 356 ms (12.03%), Space: 66.7 MB (42.08%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,55 +1,23 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& v) {
-//         vector<int>town(n+1,0);
-//                 vector<int>F(n+1,0);
-//         int ans=0;
-//         int c=0;
-//         for(auto i:v)
-//         {
-//             F[i[0]]=1;
-//             town[i[1]]++;
-//         }
-        
-//         for(int i=1;i<=n;i++)
-//         {
-//             if(!F[i])
-//             {
-//                 c++;
-//                 ans=i;
-//             }
-//         }
-        
-//         if(c!=1 or town[ans]!=n-1)
-//         {
-//             return -1;
-            
-//         }
-        
-        
-        vector<int>town(n+1);
-            vector<int>people(n+1);
+        map<int,vector<int>>m;
         for(auto &i:v)
         {
-            ++people[i[0]];
-            ++town[i[1]];
-
+            m[i[0]].push_back(i[1]);
         }
-        int ans=-1,c{};
-        for(int i=1;i<=n;i++)
+        vector<int>ans(n+1,0);
+        for(auto &i:m)
         {
-            if(people[i]==0)
+            for(auto &j:i.second)
             {
-                ans=i;
-                c++;
+                ans[j]++;
             }
         }
-        
-        if(ans==-1 or town[ans]!=n-1)
+        for(auto i=1;i<ans.size();i++)
         {
-            return -1;
+            if(ans[i]==n-1 and m.find(i)==m.end()) return i;
         }
-        return ans;
-
+        return -1;
     }
 };
