3dcec97f45ed2558ce8f3497c80f84d9c482955c
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-06-14 15:11:36 +0530

    Time: 429 ms (34.48%), Space: 105.4 MB (20.69%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -12,7 +12,7 @@ public:
 
         q.push({0,0});
         
-        // vector<int>vis(n);
+        vector<int>vis(n);
         int ans=INT_MAX;
         
         while(!q.empty())
@@ -20,7 +20,16 @@ public:
             auto it=q.front();
             q.pop();
             int elidx=it.first;
-            // vis[elidx]=1;
+             if(vis[elidx]==1)
+            {
+                // cout<<"v : "<<v[x][y]<<endl;
+                 // if(vis[{dx,dy}]==1){
+                    // cout<<"vis : "<<vis[{dx,dy}]<<endl;
+                    // cout<<"x : "<<x<<" y  : "<<y<<endl;
+                    // }
+                continue;
+            }
+            vis[elidx]=1;
             int steps=it.second;
             
             if(elidx==n-1)
@@ -30,16 +39,16 @@ public:
             }
             else
             {
-                if(elidx+1<n and m.find(v[elidx+1])!=m.end())
+                if(elidx+1<n and m.find(v[elidx+1])!=m.end() and !vis[elidx+1])
                 {
                 q.push({elidx+1,steps+1});
                 }
-                if(elidx-1>=0 and m.find(v[elidx-1])!=m.end()){
+                if(elidx-1>=0 and m.find(v[elidx-1])!=m.end() and !vis[elidx-1]){
                 q.push({elidx-1,steps+1});
                 }
                 for(auto j:m[v[elidx]])
                 {
-                    if( j!=elidx)
+                    if( j!=elidx  and !vis[j])
                     {
                         q.push({j,steps+1});
                     }
