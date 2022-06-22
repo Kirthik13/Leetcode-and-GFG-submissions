3de765f444ac1e4e60c7f150a307214cc9cade17
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-06-22 21:21:50 +0530

    Time: 19 ms (13.99%), Space: 11.8 MB (55.03%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -10,43 +10,39 @@
  */
 class Solution {
 public:
+    int getsize(ListNode* head)
+    {
+        int c=0;
+        while(head)
+        {
+            c++;
+            head=head->next;
+        }
+        return c;
+    }
     ListNode* rotateRight(ListNode* head, int k) {
         if(!head or !head->next) return head;
-        
-        ListNode* h=head;
-        int l{},c{};
-        while(h)
-        {
-            l++;
-            h=h->next;
-        }
-        c=k%l;
-        if(c==0)
-        {
-            return head;
-        }
-        int g=l-c;
-        
-        int f=0;
-        // cout<<"c : "<<c<<" l : "<<l<<" g : "<<g<<endl;
-        h=head;
-        ListNode* p=NULL;
-        while(f!=g)
-        {
-            p=h;
+        int n=getsize(head);
+        k=k%n;
+        if(k==0) return head;
+        int reqnode=n-k+1;
+        ListNode* h1=head;
 
-            h=h->next;
-            f++;
-        }
-        // cout<<" p ->val : "<<p->val<<endl;
-        p->next=NULL;
-        ListNode* tem=h;
-        while(tem->next)
+        ListNode* prev;
+        while(--reqnode and h1)
         {
-            tem=tem->next;
+            prev=h1;
+            h1=h1->next;
         }
-        tem->next=head;
-        return h;
+        prev->next=NULL;
+        
+        ListNode* h2=h1;
+        while(h2->next)
+        {
+            h2=h2->next;
+        }
+        h2->next=head;
+        return h1;
         
     }
 };
