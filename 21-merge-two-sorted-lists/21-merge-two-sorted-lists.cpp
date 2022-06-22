7dd933740eabb4f12cf0bd8a873791b7080e6ff5
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-06-22 12:07:11 +0530

    Time: 10 ms (64.30%), Space: 15.4 MB (8.22%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,41 +10,80 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* l1=list1;
-        ListNode* l2=list2;
+    ListNode* midnode(ListNode* head)
+    {
+        ListNode *slow = head;
+    ListNode *fast = head->next;
+    
+    while(slow->next != NULL && (fast!=NULL && fast->next!=NULL)) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+    }
+    ListNode* sort(ListNode* l1,ListNode* l2)
+    {
+        ListNode* c1=l1;
+        ListNode* c2=l2;
         ListNode* dum=new ListNode(-1);
-        ListNode* curr=dum;    
-        while(l1 and l2)
+        ListNode *curr=dum;
+        
+        while(c1 and c2)
         {
-            if(l1->val<=l2->val)
+            if(c1->val<=c2->val)
             {
-                curr->next=l1;
-                l1=l1->next;
+                curr->next=c1;
+                c1=c1->next;
             }
             else
             {
-                curr->next=l2;
-                l2=l2->next;
+                curr->next=c2;
+                c2=c2->next;
             }
+            
             curr=curr->next;
         }
-        while(l1)
+        while(c1)
         {
-            curr->next=l1;
-                        curr=curr->next;
-
-                l1=l1->next;
+            curr->next=c1;
+            c1=c1->next;
+            curr=curr->next;
+       
         }
-        while(l2)
+        while(c2)
         {
-            curr->next=l2;
-                        curr=curr->next;
-
-                l2=l2->next;
+            curr->next=c2;
+            c2=c2->next;
+            curr=curr->next;
+       
         }
         curr->next=NULL;
-        
         return dum->next;
+    }
+    ListNode* merge(ListNode* l1)
+    {
+        if(!l1->next) return l1;
+        ListNode* mid=midnode(l1);
+        ListNode* l2=mid->next;
+        mid->next=NULL;
+        ListNode* newnode= sort(merge(l1),merge(l2));
+        return newnode;
+        
+    }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(!list1 or !list2)
+        {
+            return !list1?list2:list1;
+        }
+        ListNode* l1=list1;
+        ListNode* l2=list2;
+        
+        while(l1->next)
+        {
+            l1=l1->next;
+        }
+        l1->next=l2;
+        
+        return merge(list1);
     }
 };
