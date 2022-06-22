a36265f1b62dd344b0fecd7c1090e47ed10a78b6
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-06-22 18:14:17 +0530

    Time: 274 ms (76.48%), Space: 121.2 MB (34.44%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,43 +10,28 @@
  */
 class Solution {
 public:
-    ListNode* midnode(ListNode* head)
+    ListNode* tem;
+    bool fn(ListNode* h)
     {
-        ListNode* s=head;
-        ListNode* f=head->next;
-        
-        while(f and f->next)
+        if(!h->next)
         {
-            f=f->next->next;
-            s=s->next;
+            if(tem->val==h->val)
+            {
+                tem=tem->next;
+                return 1;
+            }
+            return 0;
         }
-        return s;
-            
-    }
-    ListNode* reverse(ListNode* root)
-    {
-        if(!root->next) return root;
-        
-        ListNode* head=reverse(root->next);
-        root->next->next=root;
-        root->next=NULL;
-        return head;
+        if(!fn(h->next)) return 0;
+        if(tem->val==h->val)
+        {
+            tem=tem->next;
+            return 1;
+        }
+        return 0;
     }
     bool isPalindrome(ListNode* head) {
-        if(!head->next) return 1;
-        ListNode* mid=midnode(head);
-        ListNode* newnode=mid->next;
-        mid->next=NULL;
-        ListNode* l1=head;
-        ListNode* l2=reverse(newnode);
-        while(l1 and l2)
-        {
-            if(l1->val!=l2->val) return 0;
-            l1=l1->next;
-            l2=l2->next;
-      
-        }
-        // if(l1 or l2) return 0;
-        return 1;
+        tem=head;
+        return fn(head);
     }
 };
