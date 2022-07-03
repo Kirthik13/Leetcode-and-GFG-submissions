b073a778341729c39bbe2d9004e5b0b16525314d
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-03 09:04:11 +0530

    Time: 6 ms (43.60%), Space: 7.1 MB (43.94%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -3,16 +3,16 @@ public:
     int dp[1001][2];
     int fn(vector<int>&v,int idx,int positive)
     {
-        if(idx==v.size()-1) return 1;
+        if(idx==v.size()) return 1;
         if(dp[idx][positive]!=-1) return dp[idx][positive];
         
         int take=INT_MIN;
             
-        if(positive and v[idx+1]-v[idx]>0)
+        if(positive and v[idx]-v[idx-1]>0)
         {
            take=1+fn(v,idx+1,0);
         }
-         if(!positive and v[idx+1]-v[idx]<0)
+         if(!positive and v[idx]-v[idx-1]<0)
         {
            take=1+fn(v,idx+1,1);
         }
@@ -23,6 +23,6 @@ public:
     int wiggleMaxLength(vector<int>& v) {
         int n=v.size();
         memset(dp,-1,sizeof(dp));
-        return max(fn(v,0,1),fn(v,0,0));
+        return max(fn(v,1,1),fn(v,1,0));
     }
 };
