0edb94ec0358fe12364430f07a1aef07a022db99
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-04 21:52:35 +0530

    Time: 3 ms (35.32%), Space: 5.9 MB (83.67%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,29 +1,13 @@
 class Solution {
 public:
-    int fn(int idx,int n, vector<int>&dp)
-    {
-        if(idx==n) return 1;
-        if(idx>n) return 0;
-        
-        if(dp[idx]!=-1) return dp[idx];
-        
-        return dp[idx]=fn(idx+1,n,dp)+fn(idx+2,n,dp);
-    }
-        
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-         dp[n]=1;
-        for(int i=n-1;i>=0;i--)
+        int prev=1,nextprev=1;
+        for(int i=n-2;i>=0;i--)
         {
-            int f{},s=0;
-            if(i+2<=n)
-            {
-                s=dp[i+2];
-            }
-            f=dp[i+1];
-            dp[i]=f+s;
+            int curr=prev+nextprev;
+            nextprev=prev;
+            prev=curr;
         }
-        // return fn(0,n,dp);   
-        return dp[0];
+        return prev;
     }
 };
