66b882cba4196f3adf1d6bc8e84e4a41d49b5360
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-04 21:15:12 +0530

    Time: 3 ms (60.82%), Space: 6.2 MB (9.69%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    int fib1(int n,  vector<int>&dp) {
-        if(n==1) return 1;
-        if(n==0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=fib1(n-1,dp)+fib1(n-2,dp);
-    }
+    // int fib1(int n,  vector<int>&dp) {
+    //     if(n==1) return 1;
+    //     if(n==0) return 0;
+    //     if(dp[n]!=-1) return dp[n];
+    //     return dp[n]=fib1(n-1,dp)+fib1(n-2,dp);
+    // }
     int fib(int n) {
         vector<int>dp(n+1,-1);
-        return fib1(n,dp);
+        dp[0]=0;
+        if(n>=1){
+        dp[1]=1;
+            for(int i=2;i<=n;i++)
+            {
+                // cout<<"asd";
+                dp[i]=dp[i-1]+dp[i-2];
+            }
+        }
+        // return fib1(n,dp);
+        return dp[n];
     }
 
 };
