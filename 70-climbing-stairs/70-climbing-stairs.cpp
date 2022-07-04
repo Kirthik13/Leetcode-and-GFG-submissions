5a3a302ffaf0b52f87b84cd5af3f7bfcbe5434c8
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-04 21:37:04 +0530

    Time: 4 ms (22.48%), Space: 6.3 MB (14.93%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -12,6 +12,18 @@ public:
         
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return fn(0,n,dp);   
+         dp[n]=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            int f{},s=0;
+            if(i+2<=n)
+            {
+                s=dp[i+2];
+            }
+            f=dp[i+1];
+            dp[i]=f+s;
+        }
+        // return fn(0,n,dp);   
+        return dp[0];
     }
 };
