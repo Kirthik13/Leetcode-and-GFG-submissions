c15f545b64584c328abae378f1d54d21aa7c612e
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-07 09:08:28 +0530

    Time: 92 ms (12.73%), Space: 15.9 MB (31.63%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -27,72 +27,31 @@ public:
 //     }
     bool isInterleave(string s1, string s2, string s3) {
       
-//          if(s1.size() + s2.size() != s3.size()) return false;
-//         if(s1.size() == 0) return s2 == s3;
-//         if(s2.size() == 0) return s1 == s3;
-//         int m=s1.size(),n=s2.size(),p=s3.size();
-// //         if(m==0 and n==0 and p==0) return 1;
-// //         if(p==0) return 0;
-// //         if(m==0 or n==0 or p==0)
-// //         {
-// //             if(m+n!=p) return 0;
-// //             if(m>0)
-// //             {
-// //                 if(s1!=s3) return 0;
-// //             }
-// //             if(n>0)
-// //             {
-// //                 if(s2!=s3) return 0;
-// //             }
-           
-// //         }
-// //             if(m+n!=p) return 0;
+         if(s1.size() + s2.size() != s3.size()) return false;
+        if(s1.size() == 0) return s2 == s3;
+        if(s2.size() == 0) return s1 == s3;
+        int m=s1.size(),n=s2.size(),p=s3.size();
+
 
        
             
-//         vector<vector<vector<int>>>dp(m+1,vector<vector<int>>(n+1,vector<int>(p+1,-1)));
+        vector<vector<vector<bool>>>dp(m+1,vector<vector<bool>>(n+1,vector<bool>(p+1,false)));
              
+        dp[0][0][0]=1;
 
     
-//        for(int i = 1;i<=m;i++)
-//             if(s1[i-1] == s3[i-1]) dp[i][0][i] = dp[i-1][0][i-1];
-//         for(int j = 1;j<=n;j++)
-//             if(s2[j-1] == s3[j-1]) dp[0][j][j] = dp[0][j-1][j-1];
-        
-//         dp[0][0][0]=1;
-
-//         for(int i=1;i<=m;i++)
-//         {
-//             for(int j=1;j<=n;j++)
-//             {
-//                 for(int k=1;k<=p;k++)
-//                 {
-//                     if(i+j == k){
-//                         dp[i][j][k] = (dp[i-1][j][k-1] and s1[i-1] == s3[k-1]) or 
-//                             (dp[i][j-1][k-1] and s2[j-1] == s3[k-1]);
-//                     }
-//                 }
-//             }
-//         }
-//         return dp[m][n][p];
-        
-//         // return fn(0,0,0,s1,s2,s3);
-        
-        
-//     }
-          if(s1.size() + s2.size() != s3.size()) return false;
-        if(s1.size() == 0) return s2 == s3;
-        if(s2.size() == 0) return s1 == s3;
-        int n1 = s1.size(), n2 = s2.size(), n3 = s3.size();
-        vector<vector<vector<bool>>> dp(n1+1,vector<vector<bool>>(n2+1,vector<bool>(n3+1,false)));
-        dp[0][0][0] = true;
-        for(int i = 1;i<=n1;i++)
+       for(int i = 1;i<=m;i++)
             if(s1[i-1] == s3[i-1]) dp[i][0][i] = dp[i-1][0][i-1];
-        for(int j = 1;j<=n2;j++)
+        for(int j = 1;j<=n;j++)
             if(s2[j-1] == s3[j-1]) dp[0][j][j] = dp[0][j-1][j-1];
-        for(int i = 1;i<=n1;i++){
-            for(int j = 1;j<=n2;j++){
-                for(int k = 1;k<=n3;k++){
+        
+
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                for(int k=1;k<=p;k++)
+                {
                     if(i+j == k){
                         dp[i][j][k] = (dp[i-1][j][k-1] and s1[i-1] == s3[k-1]) or 
                             (dp[i][j-1][k-1] and s2[j-1] == s3[k-1]);
@@ -100,6 +59,10 @@ public:
                 }
             }
         }
-        return dp[n1][n2][n3];
+        return dp[m][n][p];
+        
+        // return fn(0,0,0,s1,s2,s3);
+        
+        
     }
 };
