a19a504548b8194f0087c4d89842de6f0bb03e66
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-08 22:42:54 +0530

    Time: 90 ms (79.79%), Space: 10.7 MB (92.29%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.cpp
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -1,50 +1,47 @@
 class Solution {
 public:
-    //declare dp of size m*m*n (the max possible)
     int dp[100][100][21];
-    
     const int mx = 1e7+1;
-    int f(vector<int>& houses, vector<vector<int>>& cost, int target, int idx, int count, int prev){
-        //base cases
-        if(idx == houses.size()) return count==target ? 0 : mx;
-        if(idx > houses.size()) return mx;
+    int fn(vector<int>& v, vector<vector<int>>& cost, int idx, int grp,int prevcolor, int target)
+    {
+        if(idx==v.size())
+        {
+            return (target==grp)?0:mx;
+        }
+        if(idx > v.size()) return mx;
         
-        //memoization
-        if(dp[idx][count][prev]!=-1) return dp[idx][count][prev];
+        if(dp[idx][grp][prevcolor]!=-1) return dp[idx][grp][prevcolor];
         
-        int minCost = mx;
-        //If already painted
-        if(houses[idx]){
-            
-            //if prev house is of different color, increase the count by 1
-            if(houses[idx]!=prev) minCost = f(houses,cost,target,idx+1,count+1,houses[idx]);
-            
-            else minCost = f(houses,cost,target,idx+1,count,houses[idx]);
-            
-        }else{
-            //we have to try all the colors
-            for(int j=0;j<cost[0].size();j++){
+        int mincost=mx;
+        
+        if(v[idx])
+        {
+            if(prevcolor!=v[idx])
+            {
+                mincost=fn(v,cost,idx+1,grp+1,v[idx],target);
+            }
+            else  mincost=fn(v,cost,idx+1,grp,v[idx],target);
+        }
+        else{
+            for(int j=0;j<cost[0].size();j++)
+            {
+                
                 int tmp;  //store the current cost
                 
-                //if prev house is of different color, increase the count by 1
-                if((j+1)!=prev) tmp = cost[idx][j] + f(houses,cost,target,idx+1,count+1,j+1);
+                if((j+1)!=prevcolor) tmp = cost[idx][j] +  fn(v,cost,idx+1,grp+1,j+1,target);
                 
-                else tmp = cost[idx][j] + f(houses,cost,target,idx+1,count,j+1);
+                else tmp = cost[idx][j] + fn(v,cost,idx+1,grp,j+1,target);
+                mincost=min(mincost,tmp);
                 
-                minCost = min(minCost,tmp);
             }
         }
-        //store before returning
-        return dp[idx][count][prev] = minCost;
+        return dp[idx][grp][prevcolor]=mincost;
     }
-    int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
+    int minCost(vector<int>& v, vector<vector<int>>& cost, int m, int n, int target) {
         memset(dp,-1,sizeof(dp));
-        
-        // 0 0 0 represents the current index(idx) , neighbourhood count(count), color of prev house(prev)
-        int ans = f(houses,cost,target,0,0,0);
-        
-        //if no solution exist
+        int ans= fn(v,cost,0,0,0,target);
         if(ans==mx) return -1;
         else return ans;
+        
     }
 };
