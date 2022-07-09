97f97bdff46d359c72515e8dd4b1729d2e031b09
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-09 08:42:51 +0530

    Time: 280 ms (60.53%), Space: 78.3 MB (87.89%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,43 +1,53 @@
 class Solution {
 public:
-//     int fn(vector<int>&v,int idx,int &k,vector<int>&dp)
-//     {
-//         if(idx>=v.size()-1) return -1e5;
-//         if(idx==v.size()-1) return v[idx];
-        
-//         if(dp[idx]!=-1) return dp[idx];
-        
-//         int ans=INT_MIN;
-//         int dest=idx+k;
-//         for(int i=idx+1;i<=dest;i++)
-//         {
-//             int g=fn(v,i,k,dp);
-//             g+=v[idx];
-            
-//             ans=max({ans,g});
-//         }
-//         return dp[idx]=ans;
-//     }
     int maxResult(vector<int>& v, int k) {
-        vector<int>dp(v.size(),INT_MIN);
-        int n=v.size();
+//         vector<int>dp(v.size(),INT_MIN);
+//         int n=v.size();
         
-        multiset<int>st;
-        dp[n-1]=v[n-1];
-        st.insert(v[n-1]);
-        for(int idx=n-2;idx>=0;idx--)
+//         multiset<int>st;
+//         dp[n-1]=v[n-1];
+//         st.insert(v[n-1]);
+//         for(int idx=n-2;idx>=0;idx--)
+//         {
+//           if(st.size()>k)
+//           {
+//               auto it=st.find(dp[idx+k+1]);
+//               st.erase(it);
+//           }
+//           dp[idx]=max(*st.rbegin()+v[idx],dp[idx]);
+//           st.insert(dp[idx]);
+            
+//         }
+        
+//         return dp[0];
+        
+        deque<int>dq;
+        int n=v.size();
+        int curr=0;
+        for(int i=n-1;i>=0;i--)
         {
-          if(st.size()>k)
-          {
-              auto it=st.find(dp[idx+k+1]);
-              st.erase(it);
-          }
-          dp[idx]=max(*st.rbegin()+v[idx],dp[idx]);
-          st.insert(dp[idx]);
+           curr=v[i]+(dq.empty()?0:v[dq.front()]);
+           
+            while(!dq.empty() and curr>v[dq.back()])
+            {
+                dq.pop_back();
+                
+            }
+            dq.push_back(i);
+            
+            if(dq.front()>=i+k)
+            {
+                dq.pop_front();
+                
+            }
+            
+            v[i]=curr;
             
         }
+        // return 
+        return curr;
         
-        return dp[0];
-        // return fn(v,0,k,dp);
+      
+        
     }
 };
