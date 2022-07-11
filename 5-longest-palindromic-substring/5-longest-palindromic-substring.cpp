3526eaca66bd787dc75cbb9a17e099e0756f798c
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-11 21:48:19 +0530

    Time: 1305 ms (7.60%), Space: 125.2 MB (23.56%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-          int n=s.size();
-      
-       bool dp[n][n];
-        string ans;
-        for(int i=n-1;i>=0;i--)
+    bool ispalin(string &s)
+    {
+        int l=0,r=s.size()-1;
+        while(l<r)
         {
-            int maxj=i;
-            for(int j=i;j<n;j++)
-            {
-               dp[i][j]=(s[i]==s[j] and (j-i<3 or dp[i+1][j-1] ));
-               if(dp[i][j] )
-               {
-                   maxj=j;
-               }
-              
-            }
-            if(ans.size()==0 or ans.size()<maxj-i+1)
-            {
-                    
-                    ans=s.substr(i,maxj-i+1);
-            }
-            
-           
+            if(s[l]!=s[r]) return 0;
+            l++;r--;
         }
+        return 1;
+    }
+    string longestPalindrome(string s) {
+        string t=s;
+        std::reverse(begin(t),end(t));
+        
+        int m=s.size(),n=t.size(),res=INT_MIN;
+        string ans;
+        vector<int>dp(n+1,0);
+    
+        for(int idx1=m-1;idx1>=0;idx1--)
+        {
+            int prev{};
+            for(int idx2=n-1;idx2>=0;idx2--)
+            {
+                int curr=dp[idx2];
+                if(s[idx1]==t[idx2])
+                {
+                    dp[idx2]=1+prev;//prev
+                }
+                else{
+                    dp[idx2]=0;
+                }
+                if(dp[idx2]>res)
+                {
+                 
+                     string temp=s.substr(idx1,dp[idx2]);
+                        if(ispalin(temp)){    //check if common substring is palindromic or not
+                             ans=temp;
+                        res=dp[idx2];
+                        }
+                }
+                prev=curr;
+            }
+
+        }
+
         return ans;
     }
 };
