27cc3610ea8841cb3dec2a4d13e950c55331ee7b
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-11 22:47:23 +0530

    Time: 49 ms (29.28%), Space: 12.3 MB (35.99%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -13,16 +13,46 @@ public:
             return fn(s,t,idx1+1,idx2+1,m,n,dp);
         }
         int call1=INT_MAX,call2=INT_MAX;
-        call1=fn(s,t,idx1+1,idx2,m,n,dp);
-        call2=fn(s,t,idx1,idx2+1,m,n,dp);
+        call1=1+fn(s,t,idx1+1,idx2,m,n,dp);
+        call2=1+fn(s,t,idx1,idx2+1,m,n,dp);
         
-        return dp[idx1][idx2]=1+min(call1,call2);
+        return dp[idx1][idx2]=min(call1,call2);
 
     }
     int minDistance(string s, string t) {
           int m=s.size(),n=t.size();
-            vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-            return fn(s,t,0,0,m,n,dp);
+            vector<vector<int>>dp(m+1,vector<int>(n+1,1e5));
+            
+                    dp[m][n]=0;
+
+            for(int idx2=n-1;idx2>=0;idx2--)
+            {
+                dp[m][idx2]=n-idx2;
+            }
+             for(int idx1=m-1;idx1>=0;idx1--)
+            {
+                dp[idx1][n]=m-idx1;
+            }
+            for(int idx1=m-1;idx1>=0;idx1--)
+            {
+                for(int idx2=n-1;idx2>=0;idx2--)
+                {
+                      
+                    if(s[idx1]==t[idx2])
+                    {
+                        dp[idx1][idx2]=dp[idx1+1][idx2+1];
+                        continue;
+                    }
+                    int call1=INT_MAX,call2=INT_MAX;
+                    call1=1+dp[idx1+1][idx2];
+                    call2=1+dp[idx1][idx2+1];
+
+                    dp[idx1][idx2]=min(call1,call2);
+                }
+
+            }
+            // return fn(s,t,0,0,m,n,dp);
+        return dp[0][0];
             
             
     }
