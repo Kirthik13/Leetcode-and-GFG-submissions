ee5b71c76eaf27e32fe59af16ec0cd9682d5b9f2
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-11 21:07:57 +0530

    Time: 615 ms (5.03%), Space: 6.5 MB (98.54%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -5,25 +5,29 @@ public:
         std::reverse(begin(t),end(t));
         int m=s.size();
         int n=t.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        // vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        vector<int>dp(n+1,0);
         for(int idx1=m-1;idx1>=0;idx1--)
         {
+            int prev{};
             for(int idx2=n-1;idx2>=0;idx2--)
             {
+                int curr=dp[idx2];
                 int call1{},call2{},call3{};
                 if(s[idx1]==t[idx2])
                 {
-                    call1=1+dp[idx1+1][idx2+1];
+                    call1=1+prev;//prev
                 }
                 else{
-                    call2=dp[idx1+1][idx2];
-                    call3=dp[idx1][idx2+1];
+                    call2=dp[idx2];
+                    call3=dp[idx2+1];
 
                 }
-                dp[idx1][idx2]=max({call1,call2,call3});
+                dp[idx2]=max({call1,call2,call3});
+                prev=curr;
             }
         }
-        return dp[0][0];
+        return dp[0];
         
     }
 };
