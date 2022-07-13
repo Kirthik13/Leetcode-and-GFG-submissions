27c79593fb09657a1d8205fcf257a21fd021340e
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-13 10:23:54 +0530

    Time: 4 ms (91.57%), Space: 15.8 MB (10.62%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
     int dp[30001][2];
-    int fn(vector<int>&v,int idx,int n,int flag)
+    int fn(vector<int>& v,int idx,int n,int curr_money,bool bought)
     {
-        if(idx>=n) return 0;
+                // cout<<"idx : "<<idx<<" cur_mon : "<<curr_money<<" cur_prof : "<<curr_profit<<endl;
+
+//        if(idx==n-1)
+//        {
         
-        if(dp[idx][flag]!=-1) return dp[idx][flag];
+//            if(bought) return curr_profit+v[idx]-curr_money;
+//            return curr_profit;
+//        }
+         if(idx>=n)
+       {
         
+           // if(bought) return curr_profit+v[idx]-curr_money;
+           return 0;
+       }
+        if(dp[idx][bought]!=-1) return dp[idx][bought];
         int curr_res{};
-        if(flag==0)
-        {
-            int buy=fn(v,idx+1,n,1)-v[idx];
-            int notbuy=fn(v,idx+1,n,0);
-            curr_res=max(buy,notbuy);
-        }
-        else{
-             int sell=fn(v,idx+1,n,0)+v[idx];
-            int notsell=fn(v,idx+1,n,1);
-            curr_res=max(sell,notsell);
-        }
-        return dp[idx][flag]=curr_res;
+       if(bought)
+       {
+           int profit=v[idx];
+           int sell=fn(v,idx+1,n,curr_money,false)+profit;
+           int notsell=fn(v,idx+1,n,curr_money,bought);
+           curr_res= max(sell,notsell);
+           
+           
+       }
+       else{
+           int buy=fn(v,idx+1,n,v[idx],true)-v[idx];
+           int notbuy=fn(v,idx+1,n,curr_money,bought);
+           curr_res=max(buy,notbuy);
+
+       } 
+        return dp[idx][bought]=curr_res;
     }
     int maxProfit(vector<int>& v) {
+        int n=v.size();
         memset(dp,-1,sizeof(dp));
-        return fn(v,0,v.size(),0);
+        int g=fn(v,0,n,0,false);
+        return g;
     }
 };
