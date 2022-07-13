dc174f96e6810dbb5da05709e857412f3290f954
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-13 22:43:48 +0530

    Time: 538 ms (26.34%), Space: 10.6 MB (44.40%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -19,26 +19,35 @@ public:
     int lengthOfLIS(vector<int>& v) {
         int n=v.size();
         // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        vector<int>dp(n+1,0);
-        vector<int>curr(n+1,0);
+        vector<int>dp(n+1,1);
 
-        for(int idx=n-1;idx>=0;idx--)
+//         for(int idx=n-1;idx>=0;idx--)
+//         {
+//             for(int previdx=idx-1;previdx>=-1;previdx--)
+//             {
+//                  int call1{};
+//                 if(previdx==-1 or v[idx]>v[previdx])
+//                 {
+//                     call1=dp[idx+1]+1;
+//                 }
+//                 call1=max(call1,dp[previdx+1]);
+
+//                 dp[previdx+1]=call1;
+//             }
+//             // dp=curr;
+//         }
+        for(int idx=1;idx<n;idx++)
         {
-            for(int previdx=idx-1;previdx>=-1;previdx--)
+            for(int prev=idx-1;prev>=0;prev--)
             {
-                 int call1{};
-                if(previdx==-1 or v[idx]>v[previdx])
+                if(v[idx]>v[prev])
                 {
-                    call1=dp[idx+1]+1;
+                    dp[idx]=max(dp[idx],dp[prev]+1);
                 }
-                call1=max(call1,dp[previdx+1]);
-
-                dp[previdx+1]=call1;
             }
-            // dp=curr;
         }
         
-        return dp[0];
+        return *max_element(begin(dp),end(dp));
         // return fn(v,0,-1,n,dp);
     }
 };
