765a0d99429a59d568dd1fcf98445327700dfaad
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-13 23:02:13 +0530

    Time: 9 ms (93.17%), Space: 10.4 MB (87.63%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,72 +1,21 @@
 class Solution {
 public:
-    int fn(vector<int>& v,int idx,int previdx,int n,  vector<vector<int>>&dp)
-    {
-        if(idx>=n) return 0;
-        
-        if(dp[idx][previdx+1]!=-1) return dp[idx][previdx+1];
-        
-        int call1{};
-        if(previdx==-1 or v[idx]>v[previdx])
-        {
-            call1=fn(v,idx+1,idx,n,dp)+1;
-        }
-        call1=max(call1,fn(v,idx+1,previdx,n,dp));
-        
-        return dp[idx][previdx+1]=call1;
-
-    }
     int lengthOfLIS(vector<int>& v) {
-        int n=v.size();
-        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        vector<int>dp(n+1,1);
-
-//         for(int idx=n-1;idx>=0;idx--)
-//         {
-//             for(int previdx=idx-1;previdx>=-1;previdx--)
-//             {
-//                  int call1{};
-//                 if(previdx==-1 or v[idx]>v[previdx])
-//                 {
-//                     call1=dp[idx+1]+1;
-//                 }
-//                 call1=max(call1,dp[previdx+1]);
-
-//                 dp[previdx+1]=call1;
-//             }
-//             // dp=curr;
-//         }
-        vector<int>hash(n+1,0);
-        for(int i=0;i<n;i++)
-        {
-            hash[i]=i;
-        }
         
-        
-        int maxidx=0,maxinos=1;
-        for(int idx=1;idx<n;idx++)
+        vector<int>temp;
+        temp.push_back(v[0]);
+        for(int i=1;i<v.size();i++)
         {
-            for(int prev=idx-1;prev>=0;prev--)
+            if(v[i]>temp.back())
             {
-                if(v[idx]>v[prev])
-                {
-                    if(dp[idx]<dp[prev]+1)
-                    {
-                        hash[idx]=prev;
-                        dp[idx]=dp[prev]+1;
-                    }
-                }
+                temp.push_back(v[i]);
+            }
+            else{
+                auto idx=lower_bound(begin(temp),end(temp),v[i])-begin(temp);
                 
-                if(maxinos<dp[idx])
-                {
-                    maxidx=idx;
-                    maxinos=dp[idx];
-                }
+                temp[idx]=v[i];
             }
         }
-        
-        // return *max_element(begin(dp),end(dp));
-        return maxinos;
-        // return fn(v,0,-1,n,dp);
+        return temp.size();
     }
 };
