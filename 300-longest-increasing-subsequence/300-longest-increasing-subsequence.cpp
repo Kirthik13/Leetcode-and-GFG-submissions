94518430c4234da1cf298117c1ffb4aef3278056
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-13 21:40:16 +0530

    Time: 2648 ms (5.00%), Space: 292.6 MB (6.79%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-//     int fn(vector<int>&v,int ind,int n,int prev_ind, vector<vector<int>>&dp)
-//     {
-//        if(ind ==n)return 0;
-//         if(dp[ind][prev_ind+1]!=-1)return dp[ind][prev_ind+1];
-//         int len = 0+fn(v,ind+1,n,prev_ind,dp);
-//         if(prev_ind == -1 || v[ind]>v[prev_ind]){
-//              len=max(len,1+fn(v,ind+1,n,ind,dp));
-//         }
-//         return dp[ind][prev_ind+1]=len;
 
-       
-//     }
     
  int fn(vector<int>&v,int idx,int n,int previdx, vector<vector<int>>&dp)
     {
         if(idx>=n) return 0;
         
-        if(dp[idx][previdx+1]!=-1) return dp[idx][previdx+1];
+        if(dp[idx][previdx]!=-1) return dp[idx][previdx];
         
         int call1{};
-        if(previdx==-1 or v[idx]>v[previdx])
+     
+        if(idx==previdx)
+        {
+            call1=1+fn(v,idx+1,n,idx,dp);
+            call1=max(call1,fn(v,idx+1,n,idx+1,dp));
+        }
+     else{
+        if( v[idx]>v[previdx])
         {
             call1=1+fn(v,idx+1,n,idx,dp);
         }
         call1=max(call1,fn(v,idx+1,n,previdx,dp));
+     }
 
-        return dp[idx][previdx+1]=call1;
-         // return max({call1,call2});
+        return dp[idx][previdx]=call1;
 
        
     }
@@ -35,29 +31,29 @@ public:
     int lengthOfLIS(vector<int>& v) {
         // v.insert(v.begin(),INT_MIN);
                 int n=v.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
 //         vector<int>dp(n+1,0);
 //         // vector<int>curr(n+1,0);
 
         
-//         for(int idx=n-1;idx>=1;idx--)
+//         for(int idx=n-1;idx>=0;idx--)
 //         {
 
-//             for(int previdx=n-1;previdx>=0;previdx--)
+//             for(int previdx=n-1;previdx>=-1;previdx--)
 //             {
-//                  int call1{},call2{};
-//                 if((idx==1 and v[idx]>=v[previdx]) or (idx>1 and v[idx]>v[previdx]))
-//                 {
-//                     call1=1+dp[idx];
-//                 }
-//                 call2=dp[previdx];
+//                     int call1{};
+//                     if(previdx==-1 or v[idx]>v[previdx])
+//                     {
+//                         call1=1+dp[idx+1][idx];
+//                     }
+//                     call1=max(call1,dp[idx+1][previdx]);
 
-//                 dp[previdx]=max({call1,call2});
+//                     return dp[idx][previdx+1]=call1;
 //             }
 //             // dp=curr;
 //         }
         
-//         return dp[0];
-        return fn(v,0,n,-1,dp);
+        // return dp[0][0];
+        return fn(v,0,n,0,dp);
     }
 };
