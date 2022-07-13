c24f236887d87fd8ae532e08746bed2dcfc770b0
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-13 10:42:06 +0530

    Time: 11 ms (55.70%), Space: 15.7 MB (10.96%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
     int dp[30001][2];
-    int fn(vector<int>& v,int idx,int n,int curr_money,bool bought)
+    int fn(vector<int>& v,int idx,int n,bool bought)
     {
                 // cout<<"idx : "<<idx<<" cur_mon : "<<curr_money<<" cur_prof : "<<curr_profit<<endl;
 
-//        if(idx==n-1)
-//        {
-        
-//            if(bought) return curr_profit+v[idx]-curr_money;
-//            return curr_profit;
-//        }
-         if(idx>=n)
+       if(idx>=n)
        {
         
-           // if(bought) return curr_profit+v[idx]-curr_money;
-           return 0;
+          return 0;
        }
+        
         if(dp[idx][bought]!=-1) return dp[idx][bought];
-        int curr_res{};
+        
        if(bought)
        {
-           int profit=v[idx];
-           int sell=fn(v,idx+1,n,curr_money,false)+profit;
-           int notsell=fn(v,idx+1,n,curr_money,bought);
-           curr_res= max(sell,notsell);
+           int sell=fn(v,idx+1,n,false)+v[idx];
+           int notsell=fn(v,idx+1,n,bought);
+           return dp[idx][bought] =max(sell,notsell);
            
            
        }
        else{
-           int buy=fn(v,idx+1,n,v[idx],true)-v[idx];
-           int notbuy=fn(v,idx+1,n,curr_money,bought);
-           curr_res=max(buy,notbuy);
+           int buy=fn(v,idx+1,n,true)-v[idx];
+           int notbuy=fn(v,idx+1,n,bought);
+           return dp[idx][bought]=max(buy,notbuy);
 
        } 
-        return dp[idx][bought]=curr_res;
     }
     int maxProfit(vector<int>& v) {
         int n=v.size();
         memset(dp,-1,sizeof(dp));
-        int g=fn(v,0,n,0,false);
+        int g=fn(v,0,n,false);
         return g;
     }
 };
