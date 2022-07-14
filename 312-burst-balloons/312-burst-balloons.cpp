a84f0acce8855517d5dee5506e8f4bb6434fdb50
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-14 20:53:13 +0530

    Time: 1186 ms (5.01%), Space: 10.4 MB (52.77%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int fn(int i,int j,vector<int>& v,vector<vector<int>>dp)
+    int fn(int i,int j,vector<int>& v,vector<vector<int>>&dp)
 {
     if(i>j) return 0;
    
@@ -20,25 +20,25 @@ public:
          int n=v.size();
     v.insert(v.begin(),1);
     v.push_back(1);
-    vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+    vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
         
-        for(int i=n;i>=1;i--)
-        {
-            for(int j=i;j<=n;j++)
-            {
-                  int ans{};
-                for(int idx=i;idx<=j;idx++)
-                {
-                    int gain=v[i-1]*v[idx]*v[j+1];
-                    int call1=gain+dp[i][idx-1]+dp[idx+1][j];
-                    ans=max(ans,call1);
-                }
+//         for(int i=n;i>=1;i--)
+//         {
+//             for(int j=i;j<=n;j++)
+//             {
+//                   int ans{};
+//                 for(int idx=i;idx<=j;idx++)
+//                 {
+//                     int gain=v[i-1]*v[idx]*v[j+1];
+//                     int call1=gain+dp[i][idx-1]+dp[idx+1][j];
+//                     ans=max(ans,call1);
+//                 }
 
-                dp[i][j]=ans;
-            }
-        }
+//                 dp[i][j]=ans;
+//             }
+//         }
         
-        return dp[1][n];
-    // return fn(1,n,v,dp);
+        // return dp[1][n];
+    return fn(1,n,v,dp);
     }
 };
