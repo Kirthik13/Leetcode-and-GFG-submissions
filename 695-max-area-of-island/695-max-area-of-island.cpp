541048bf148d080c94b8614b4dd2891585d0ca30
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-15 09:52:45 +0530

    Time: 31 ms (51.00%), Space: 23.9 MB (29.56%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
+    vector<pair<int,int>>dir={{-1,0},{0,1},{1,0},{0,-1}};
     int dfs(vector<vector<int>>& v,int i,int j,int m,int n)
     {
-        if(i<0 or j<0 or i>=m or j>=n or  v[i][j]==0) return 0;
           v[i][j] = 0;
-  
-        return 1+dfs(v,i-1,j,m,n)+dfs(v,i+1,j,m,n)+dfs(v,i,j-1,m,n)+dfs(v,i,j+1,m,n);
+        int ans=0;
+        for(auto &d:dir)
+        {
+            int new_x=i+d.first;
+            int new_y=j+d.second;
+            
+            if(new_x>=0 and new_x<m and new_y>=0 and new_y<n and v[new_x][new_y]==1)
+            {            
+                // cout<<"newx: "<<new_x<<" newy : "<<new_y<<endl;
+                ans+=dfs(v,new_x,new_y,m,n);
+            }
+
+        }
+        // cout<<"ans : "<<ans<<endl;
+        return 1+ans;
         
     }
     int maxAreaOfIsland(vector<vector<int>>& v) {
@@ -15,7 +28,7 @@ public:
         {
             for(int j=0;j<n;j++)
             {
-                if(v[i][j]==1)
+                if( v[i][j]==1)
                 {
                     int call1=dfs(v,i,j,m,n);
                     // cout<<"cal 1 : "<<call1<<endl;
