a4db96ab1147aa98ce8b6d2b0b73bfbe770d7099
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-16 09:21:26 +0530

    Time: 8 ms (84.71%), Space: 6.7 MB (71.79%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -27,7 +27,32 @@ public:
         return dp[r][c][k]=ans%mod;
     }
     int findPaths(int m, int n, int maxMove, int sr, int sc) {
-        vector<vector<vector<int>>>dp(m+1,vector<vector<int>>(n+1,vector<int>(maxMove+1,-1)));
-        return fn(sr,sc,m,n,maxMove,dp);
+             int dp[maxMove+1][m][n];
+        memset(dp,0,sizeof dp);
+        for(int i=0;i<maxMove;i++){
+            for(int j=0;j<m;j++){
+                for(int k=0;k<n;k++){
+                    if(i==0){
+                        dp[i][j][k]+=(j-1<0 ? 1 : 0);
+                        dp[i][j][k]+=(k-1<0 ? 1 : 0);
+                        dp[i][j][k]+=(j+1 >= m ? 1 :0);
+                        dp[i][j][k]+=(k+1>= n ? 1 : 0);
+                    }
+                    else{
+                        long a=(j-1<0 ? 1 : dp[i-1][j-1][k]);
+                        long b=(k-1<0 ? 1 : dp[i-1][j][k-1]);
+                        long c=(j+1 >= m ? 1 : dp[i-1][j+1][k]);
+                        long d=(k+1>= n ? 1 : dp[i-1][j][k+1]);
+                        
+                        dp[i][j][k] = ((a+b+c+d)*1L)%mod;
+                    }
+                }
+            }
+        }
+        if(maxMove==0){
+            return dp[0][sr][sc];
+        }
+        return dp[maxMove-1][sr][sc];
+        // return fn(sr,sc,m,n,maxMove,dp);
     }
 };
