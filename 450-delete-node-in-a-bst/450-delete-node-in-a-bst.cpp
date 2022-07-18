c31d70151369c5201edbb3a5a891a64fddf2d108
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-18 11:33:05 +0530

    Time: 61 ms (34.79%), Space: 32.6 MB (78.28%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -11,43 +11,35 @@
  */
 class Solution {
 public:
-    TreeNode* fn(TreeNode* root,int key)
+    TreeNode* fn(TreeNode* root, int k)
     {
         if(!root) return NULL;
-        if(root->val==key)
+        if(root->val==k)
         {
-               if(!root->left)
-                {
-                    return root->right;
-                }
-                else if(!root->right)
-                {
-                    return root->left;
-                }
-                else{
-                    TreeNode* nextnode=root->right;
-                    
-                    while(nextnode->left!=NULL)
-                    {
-                        nextnode=nextnode->left;
-                    }
-                    nextnode->left=root->left;
-                    return root->right;
-                }
+            if(!root->left) return root->right;
+            if(!root->right) return root->left;
+            
+            TreeNode* rn=root->right;
+            while(rn->left)
+            {
+                rn=rn->left;
+            }
+            rn->left=root->left;
+            return root->right;
+            
         }
-        if(root->val>key)
+        else if(root->val < k)
         {
-            root->left=fn(root->left,key);
+            root->right=fn(root->right,k);
         }
-        if(root->val<key)
+        else if(root->val > k)
         {
-           root->right= fn(root->right,key);
-
+            root->left=fn(root->left,k);
+            
         }
-        
         return root;
     }
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        return fn(root,key);
+    TreeNode* deleteNode(TreeNode* root, int k) {
+     return fn(root,k);   
     }
 };
