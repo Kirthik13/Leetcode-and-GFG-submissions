fc9480b1a32f98e1f423d80170a410b72473e2f3
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-22 23:08:51 +0530

    Time: 7 ms (78.64%), Space: 8.8 MB (70.18%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& v1, vector<int>& v2) {
-                int n=v2.size();
-
-        vector<int>tot(v1.size());
+        
+        int n=v2.size();
+        vector<int>ng(n,-1);
         stack<int>st;
         st.push(v2[n-1]);
-        vector<int>ans(n,-1);
-        
         for(int i=n-2;i>=0;i--)
         {
-           
-            while(st.size()>0 and st.top()<v2[i])
+            if(!st.empty())
             {
-                st.pop();
+                while(!st.empty() and st.top()<v2[i]){
+                    st.pop();
+                }
+                if(st.size()>0)
+                {
+                    ng[i]=st.top();
+                }
             }
-            if(st.size()>0) ans[i]=st.top();
-            st.push(v2[i]);
+                st.push(v2[i]);
+            
+            
         }
-        map<int,int>m;
-        for(int i=0;i<v2.size();i++)
+        unordered_map<int,int>m;
+        for(int i=0;i<n;i++)
         {
             m[v2[i]]=i;
         }
         
+        vector<int>ans;
         for(int i=0;i<v1.size();i++)
         {
-            int idx=m[v1[i]];
-            tot[i]=ans[idx];
-        }
-        return tot;
+            if(m.find(v1[i])!=m.end())
+            {
+                ans.push_back(ng[m[v1[i]]]);
+            }
+            else{
+             ans.push_back(-1);
+            }    
             
-        
+        }
+        return ans;
     }
 };
