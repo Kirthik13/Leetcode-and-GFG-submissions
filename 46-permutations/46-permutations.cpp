c7b48648ad321dd3bfa61e484bc69b88ea3c9d6e
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-22 14:06:24 +0530

    Time: 39 ms (5.26%), Space: 8 MB (37.41%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    set<vector<int>>totans;
+    
+    void fn(vector<int>& v,int idx,int n)
+    {
+        if(idx>=n)
+        {
+            totans.insert(v);
+            return;
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            // if(i==idx) continue;
+            swap(v[idx],v[i]);
+            fn(v,idx+1,n);
+            swap(v[idx],v[i]);
+            
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& v) {
+        int n=v.size();
+        fn(v,0,n);
+        vector<vector<int>>temp(begin(totans),end(totans));
+        
+        return temp;
+    }
+};
