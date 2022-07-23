e9248c0edacf6d45d0104119a60ef99afb0341de
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-23 16:34:56 +0530

    Time: 7 ms (7.53%), Space: 7.1 MB (56.08%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -8,38 +8,36 @@ public:
     }
     
     void push(int x) {
-        st1.push(x);
-    }
-    
-    int pop() {
-        while(!st1.empty())
+        if(st1.empty())
+        {
+            st1.push(x);
+            return;
+        }
+         while(!st1.empty())
         {
             st2.push(st1.top());
             st1.pop();
         }
-        int ans=st2.top();
-        st2.pop();
-        // swap(st1,st2);
-        while(!st2.empty())
+        st1.push(x);
+         while(!st2.empty())
         {
             st1.push(st2.top());
             st2.pop();
-        }        
+        }   
+    }
+    
+    int pop() {
+       
+        int ans=st1.top();
+        st1.pop();
+            
         return ans;
     }
     
     int peek() {
-        while(!st1.empty())
-        {
-            st2.push(st1.top());
-            st1.pop();
-        }
-        int ans=st2.top();
-        while(!st2.empty())
-        {
-            st1.push(st2.top());
-            st2.pop();
-        }
+       
+        int ans=st1.top();
+      
         return ans;
     }
     
