62471f622c126b71a427dc2c9ab44b410754a990
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-24 10:09:26 +0530

    Time: 799 ms (33.33%), Space: 166.1 MB (33.33%) - LeetHub

---
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -0,0 +1,47 @@
+struct cmp{
+    bool operator()
+        (pair<int,string>a,pair<int,string>b) const{
+        if(a.first==b.first) return a.second>b.second;
+        return a.first<b.first;
+    }
+};
+class FoodRatings {
+public:
+    map<string,string>fmap;
+    map<string,int>rmap;
+    map<string,set<pair<int,string>,cmp>>m2;
+    
+    FoodRatings(vector<string>& f, vector<string>& c, vector<int>& r) {
+        for(int i=0;i<f.size();i++)
+        {
+            fmap[f[i]]=c[i];
+            rmap[f[i]]=r[i];
+            
+            m2[c[i]].insert({r[i],f[i]});
+            
+        }
+    }
+    
+    void changeRating(string food, int newr) {
+        string c=fmap[food];
+        pair<int,string>p={rmap[food],food};
+        // auto it=m2[c].find(p);
+        m2[c].erase(p);
+        m2[c].insert({newr,food});
+        
+        rmap[food]=newr;
+        
+    }
+    
+    string highestRated(string c) {
+        auto it=m2[c].rbegin();
+        return it->second;
+    }
+};
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
