f75feb73570a87aaa71804ba382a50b954eeeb95
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-26 11:53:01 +0530

    Time: 7 ms (12.25%), Space: 7.9 MB (5.92%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -15,7 +15,18 @@ public:
     }
     int rob(vector<int>& v) {
         int n=v.size();
-        vector<int>dp(n+1,-1);
-        return fn(v,0,n,dp);
+        vector<int>dp(n+2,0);
+        
+        for(int idx=n-1;idx>=0;idx--)
+        {
+            int call1=v[idx]+dp[idx+2];
+            int call2=dp[idx+1];
+            
+            int ans=max(call1,call2);
+            
+            dp[idx]=ans;
+        }
+        return dp[0];
+        // return fn(v,0,n,dp);
     }
 };
