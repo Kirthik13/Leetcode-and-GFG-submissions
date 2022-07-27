7b52e367575ce44f104b7f18db9499a5c67477e3
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-27 17:42:07 +0530

    Time: 160 ms (45.73%), Space: 20 MB (85.94%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,51 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& v) {
-        vector<vector<int>>tot;
-        
+     vector<vector<int>>totans;   
         sort(begin(v),end(v));
-        
-        for(unsigned int i=0;i<v.size();i++)
+        int n=v.size();
+        for(int i=0;i<=n-3;i++)
         {
             if(i>0 and v[i-1]==v[i]) continue;
-            int l=i+1,r=v.size()-1;
+            int l=i+1,r=n-1;
+            int el=v[i];
             while(l<r)
             {
-                int s=v[i]+v[l]+v[r];
-                if(s>0)
+                
+                if(el+v[l]+v[r]==0)
                 {
-                    r--;
-                }
-                else if(s<0)
-                {
-                    l++;
-                }
-                else{
-                    tot.push_back(vector<int>{v[i],v[l],v[r]});
+                    totans.push_back({el,v[l],v[r]});
+                    
+                    
                     while(v[l]==v[l+1] and l+1<r) l++;
                     while(v[r]==v[r-1] and l<r-1) r--;
                     l++;
                     r--;
-
+                   
+                    
                 }
-                // if(v[l]+v[i]+v[r]==0)
-                // {
-                //     tot.push_back({v[i],v[l],v[r]});
-                //     // break;
-                //     int l1=v[l],r1=v[r];
-                //     while(l1==v[l] and l<r) l++;
-                //     while(r1==v[r] and r>i) r--;
-                // }
-                // else if(v[l]+v[r]+v[i]>0)
-                // {
-                //     r--;
-                // }
-                // else{
-                //     l++;
-                // }
+                else if(el+v[l]+v[r]<0)
+                {
+                    l++;
+                }
+                else{
+                    r--;
+                }
             }
             
         }
-        return tot;
+        return totans;
     }
 };
