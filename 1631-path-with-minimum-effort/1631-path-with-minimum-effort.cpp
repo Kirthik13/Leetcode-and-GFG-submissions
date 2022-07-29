48844c306631202bbe7f5d6f12f2f9590dd26116
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-29 19:01:22 +0530

    Time: 536 ms (32.88%), Space: 41.9 MB (32.63%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,57 +1,56 @@
 class Solution {
-private:
-    vector<int>dirx={-1,0,1,0};
-    vector<int>diry={0,1,0,-1};
-
-    int m,n;
 public:
-  
-    bool isvalid(int x,int y)
+    vector<vector<int>>dir={{-1,0},{1,0},{0,1},{0,-1}};
+    
+    bool isvalid(vector<vector<int>>&v,int x,int y,int m,int n,int max_diff,int prev,vector<vector<int>>&vis)
     {
-        return x>=0 and x<m and y>=0 and y<n;
-        
+        return x<0 or y<0 or x>=m or y>=n or abs(v[x][y]-prev)>max_diff or vis[x][y]==1;
     }
-    bool dfs( vector<vector<int>>&v,int x,int y,int mid,vector<vector<int>>&vis)
+    bool dfs(vector<vector<int>>&v,int x,int y,int m,int n,int max_diff,vector<vector<int>>&vis)
     {
-
-        if(x==m-1 and y==n-1) return 1;
-        vis[x][y]=1;
-
-        for(int i=0;i<4;i++)
+        if(x==m-1 and y==n-1)
         {
-            int dx=x+dirx[i],dy=y+diry[i];
-            if(isvalid(dx,dy) and vis[dx][dy]==0 and abs(v[x][y]-v[dx][dy])<=mid)
+            return true;
+        }
+        
+        vis[x][y]=1;
+        for(auto &i:dir)
+        {
+            int newx=x+i[0];
+            int newy=y+i[1];
+            int prev=v[x][y];
+            if(!isvalid(v,newx,newy,m,n,max_diff,prev,vis))
             {
-                if(dfs(v,dx,dy,mid,vis)) return 1;
-                    
+                if(dfs(v,newx,newy,m,n,max_diff,vis)) return 1;
             }
+            
         }
         return 0;
     }
-   
+    bool cando(vector<vector<int>>&v,int max_diff,int m,int n)
+    {
+        int dfsmin=INT_MAX;
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        return dfs(v,0,0,m,n,max_diff,vis);
+    }
     int minimumEffortPath(vector<vector<int>>& v) {
-        int l=0,r=(1e6);
-        int ans=INT_MAX;
-        m=v.size();
-        n=v[0].size();
-              vector<vector<int>>vis;
-
-        while(l<=r)
+        
+        int l=0,r=1e6,m=v.size(),n=v[0].size();
+        
+        while(l<r)
         {
             int mid=l+((r-l)/2);
-            vis.assign(m,vector<int>(n,0));
-
-            if(dfs(v,0,0,mid,vis))
+            
+            if(cando(v,mid,m,n))
             {
-                r=mid-1;
-                ans=min(ans,mid);
+                r=mid;
+                
             }
             else{
                 l=mid+1;
             }
-            
         }
-        return ans==INT_MAX?0:ans;
-        // return l;
+        return l;
+        
     }
 };
