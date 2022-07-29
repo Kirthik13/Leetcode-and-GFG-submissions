82de219929d9d644601c846c429dd0694b9833c4
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-29 10:40:08 +0530

    Time: 116 ms (21.56%), Space: 26 MB (67.26%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool cando(vector<int>&v,int max_cap,int days)
+    {
+        int curr_wt=0;
+        for(int i=0;i<v.size();i++)
+        {
+            if(max_cap<v[i]) return 0;
+            
+            curr_wt+=v[i];
+            
+            if(curr_wt>max_cap)
+            {
+                days--;
+                curr_wt=v[i];
+            }
+            if(days==0) return 0;
+        
+        }
+        return 1;
+        
+    }
+    int shipWithinDays(vector<int>& v, int days) {
+        
+        int l=0,r=accumulate(begin(v),end(v),0);
+        
+        while(l<r)
+        {
+            int mid=l+((r-l)/2);
+            
+            if(cando(v,mid,days))
+            {
+                r=mid;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return l;
+    }
+};
