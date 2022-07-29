d6a0f4415c6bd926b9b522af2b2a3b4a86d11f7e
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-29 14:23:21 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (36.08%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,39 +1,45 @@
+#define ll long long 
 class Solution {
 public:
-    bool cando(vector<int>&v,int &k,int &mid)
+    
+    bool cando(vector<int>&v,ll int max_sum,int m)
     {
-        int curr{},nos=1;
+        ll int curr_sum=0;
+        int k=1;
+        
         for(int i=0;i<v.size();i++)
         {
-            curr+=v[i];
-            if(v[i]>mid) return 0;
-            if(curr>mid)
+            if(v[i]>max_sum) return 0;
+            if(curr_sum+v[i]<=max_sum)
             {
-               curr=v[i];
-                nos++;
-                if(nos>k) return 0;
+                curr_sum+=v[i];
+                    
+                
+            }
+            else{
+                k++;
+                curr_sum=v[i];
             }
         }
-    return 1;
+        return k<=m;
     }
-    int splitArray(vector<int>& v, int k) {
-        int l=0,r=accumulate(begin(v),end(v),0);
-        int ans=INT_MAX;
-        while(l<=r)
+    
+    int splitArray(vector<int>& v, int m) {
+        int n=v.size();
+        ll int l=0,r=n*1e6;
+        
+        while(l<r)
         {
-            int mid=(r+l)/2;
-            // cout<<"mid : "<<mid<<endl;
-            if(cando(v,k,mid))
+            ll int mid=l+((r-l)/2);
+            
+            if(cando(v,mid,m))
             {
-                ans=min(ans,mid);
-                // cout<<"ans : "<<ans<<endl;
-                r=mid-1;
+                r=mid;
             }
             else{
                 l=mid+1;
             }
-            
         }
-        return ans;
+        return l;
     }
 };
