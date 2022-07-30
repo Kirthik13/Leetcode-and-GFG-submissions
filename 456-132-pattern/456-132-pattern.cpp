bafc9f7203df138b433ddced22714b0a51a3b109
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-30 13:26:12 +0530

    Time: 103 ms (67.12%), Space: 38.3 MB (55.41%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& v) {
+        if(v.size()<3) return 0;
         stack<int>st;
-        int n=v.size();
         int f=INT_MIN;
-        for(int i=n-1;i>=0;i--)
+        for(int i=v.size()-1;i>=0;i--)
         {
-            if(v[i]<f) return 1;
+            if(f>v[i]) return 1;
+            
             while(!st.empty() and st.top()<v[i])
             {
                 f=st.top();
                 st.pop();
             }
             st.push(v[i]);
+           
         }
         return 0;
     }
