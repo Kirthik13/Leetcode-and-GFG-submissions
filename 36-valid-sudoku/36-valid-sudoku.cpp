7ac3d43d1854986ac6d7def3cde80461825c23c7
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-30 20:25:24 +0530

    Time: 32 ms (65.96%), Space: 18.9 MB (54.60%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -0,0 +1,97 @@
+class Solution {
+public:
+    bool rowsfn(vector<vector<char>>& v,int r,int c,int n)
+    {
+        vector<int>hash(10,0);
+        for(int i=0;i<9;i++)
+        {
+            int g=int(v[i][c])-48;
+
+            if(v[i][c]!='.' )
+            {
+                if(hash[g]>0) return false;
+            }
+            if(v[i][c]!='.' )
+            {
+                hash[g]++;
+            }
+        }
+        return 1;
+    }
+        
+     bool colfn(vector<vector<char>>& v,int r,int c,int n)
+    {
+        vector<int>hash(10,0);
+        for(int i=0;i<9;i++)
+        {
+                int g=int(v[r][i])-48;
+            
+            if(v[r][i]!='.' )
+            {
+                if(hash[g]>0) return false;
+            }
+            if(v[r][i]!='.' )
+            {
+                hash[g]++;
+            }
+        }
+         return 1;
+    }
+    bool indfn(vector<vector<char>>& v,int r,int c,int n)
+    {
+         vector<int>hash(10,0);
+        for(int i=r;i<r+3;i++)
+        {
+            for(int j=c;j<c+3;j++)
+            {
+                int g=int(v[i][j])-48;
+                
+                if(v[i][j]!='.' )
+                {
+                    if(hash[g]>0) return false;
+                }
+                if(v[i][j]!='.' )
+                {
+                    // int g=int(v[i][j])-48;
+                    hash[g]++;
+                }
+            }
+        }
+        return 1;
+    }
+    bool isValidSudoku(vector<vector<char>>& v) {
+        int n=v.size();
+        
+        for(int i=0;i<9;i++)
+        {
+            
+            if(!rowsfn(v,0,i,n))
+            {
+                return false;
+            }
+        }
+        for(int i=0;i<9;i++)
+        {
+            
+            if(!colfn(v,i,0,n))
+            {
+
+                return false;
+            }
+        }
+        
+        vector<pair<int,int>> dir={{0,0},{0,3},{0,6},{3,0},{3,3},{3,6},{6,0},{6,3},{6,6}};
+        
+        for(auto &i:dir)
+        {
+            if(!indfn(v,i.first,i.second,n))
+            {
+
+                return 0;
+            }
+        }
+        return 1;
+        
+        
+    }
+};
