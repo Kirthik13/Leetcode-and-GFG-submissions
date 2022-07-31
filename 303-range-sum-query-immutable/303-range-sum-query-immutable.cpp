2130ad36ab9aea0a41750df80b8daa89cec2db6d
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-31 16:59:53 +0530

    Time: 78 ms (22.61%), Space: 29.1 MB (7.61%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
@@ -1,21 +1,72 @@
+class segmentnode
+{
+  public:
+    int start,end,val;
+    segmentnode* left;
+    segmentnode* right;
+    
+    segmentnode(int l,int r)
+    {
+        this->start=l;
+        this->end=r;
+        this->val=0;
+        left=NULL;
+        right=NULL;
+        
+        
+        
+    }
+        
+    
+};
 class NumArray {
 public:
-    vector<int>ans;
-    NumArray(vector<int>& v) {
-        ans.push_back(v[0]);
-        for(int i=1;i<v.size();i++)
+    segmentnode* root;
+    
+    segmentnode* buildtree(vector<int>&v,int l,int r)
+    {
+        if(l>r) return NULL;
+        
+        segmentnode* node=new segmentnode(l,r);
+        if(l==r)
         {
-            int k=ans[i-1]+v[i];
-            ans.push_back(k);
+            node->val=v[l];
+            return node;
         }
+        
+        int mid=(l+r)/2;
+        node->left=buildtree(v,l,mid);
+        node->right=buildtree(v,mid+1,r);
+        
+        node->val=node->left->val+node->right->val;
+        
+        return node;
+        
+    }
+    
+    int sumtree(segmentnode* root,int l,int r)
+    {
+        if(root==NULL) return 0;
+        if(root->start==l and root->end==r)
+        {
+            return root->val;
+        }
+        
+        int mid=(root->start+root->end)/2;
+        if(l>mid) return sumtree(root->right,l,r);
+        if(r<=mid) return sumtree(root->left,l,r);
+        
+        return sumtree(root->left,l,mid)+sumtree(root->right,mid+1,r);
+        
+    }
+    
+    NumArray(vector<int>& v) {
+        int n=v.size();
+        root=buildtree(v,0,n-1);
     }
     
     int sumRange(int left, int right) {
-        if(left==0)
-        {
-            return ans[right];
-        }
-        return ans[right]-ans[left-1];
+        return sumtree(root,left,right);
     }
 };
 
