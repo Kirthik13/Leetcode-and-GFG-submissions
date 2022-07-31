bddd50cfb821feb44a469d68726016082cbb1756
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-07-31 16:46:49 +0530

    Time: 1174 ms (5.01%), Space: 186.3 MB (6.60%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -1,67 +1,114 @@
-struct SegmentTreeNode {
-    int start, end, sum;
-    SegmentTreeNode* left;
-    SegmentTreeNode* right;
-    SegmentTreeNode(int a, int b):start(a),end(b),sum(0),left(nullptr),right(nullptr){}
+class segmentnode
+{
+    public:
+    segmentnode* left=NULL;
+    segmentnode* right=NULL;
+    int val, start, end;
+    
+    segmentnode(int st,int en)
+    {
+        this->start=st;
+        this->end=en;
+        val=0;
+        left=NULL;
+        right=NULL;        
+        
+        
+    }
+    
+        
+    
 };
 class NumArray {
-    SegmentTreeNode* root;
 public:
-    NumArray(vector<int> &nums) {
-        int n = nums.size();
-        root = buildTree(nums,0,n-1);
-    }
-   
-    void update(int i, int val) {
-        modifyTree(i,val,root);
-    }
-
-    int sumRange(int i, int j) {
-        return queryTree(i, j, root);
-    }
-    SegmentTreeNode* buildTree(vector<int> &nums, int start, int end) {
-        if(start > end) return nullptr;
-        SegmentTreeNode* root = new SegmentTreeNode(start,end);
-        if(start == end) {
-            root->sum = nums[start];
-            return root;
+    segmentnode* root;
+    
+    segmentnode* buildtree(vector<int>&v,int l,int r)
+    {
+        if(l>r) return NULL;
+        segmentnode* node=new segmentnode(l,r);
+        if(l==r)
+        {
+            node->val=v[l];
+            return node; 
         }
-        int mid = start + (end - start) / 2;
-        root->left = buildTree(nums,start,mid);
-        root->right = buildTree(nums,mid+1,end);
-        root->sum = root->left->sum + root->right->sum;
-        return root;
+        
+        int mid=l+((r-l)/2);
+        node->left=buildtree(v,l,mid);
+        node->right=buildtree(v,mid+1,r);
+        
+        node->val=node->left->val+node->right->val;
+        
+        return node;
+        
+        
     }
-    int modifyTree(int i, int val, SegmentTreeNode* root) {
-        if(root == nullptr) return 0;
+    
+    int modifytree(int idx,int data,segmentnode* root)
+    {
+        if(root==NULL)
+        {
+            return 0;
+        }
         int diff;
-        if(root->start == i && root->end == i) {
-            diff = val - root->sum;
-            root->sum = val;
+        if(root->start==idx and root->end==idx)
+        {
+            diff=data-root->val;
+            root->val=data;
             return diff;
         }
-        int mid = (root->start + root->end) / 2;
-        if(i > mid) {
-            diff = modifyTree(i,val,root->right);
-        } else {
-            diff = modifyTree(i,val,root->left);
+        
+        
+        int mid=root->start+((root->end-root->start)/2);
+        
+        if(idx>mid)
+        {
+            diff=modifytree(idx,data,root->right);
         }
-        root->sum = root->sum + diff;
+        else{
+            diff=modifytree(idx,data,root->left);
+
+        }
+        
+        
+        root->val = root->val + diff;
         return diff;
     }
-    int queryTree(int i, int j, SegmentTreeNode* root) {
-        if(root == nullptr) return 0;
-        if(root->start == i && root->end == j) return root->sum;
-        int mid = (root->start + root->end) / 2;
-        if(i > mid) return queryTree(i,j,root->right);
-        if(j <= mid) return queryTree(i,j,root->left);
-        return queryTree(i,mid,root->left) + queryTree(mid+1,j,root->right);
+  
+    
+    int sumtree(segmentnode* root,int l,int r)
+    {
+        if(root==NULL) return 0;
+        
+        if(root->start==l and root->end==r) return root->val;
+        
+        int mid=root->start+((root->end-root->start)/2);
+        
+        if(l>mid) return sumtree(root->right,l,r);
+        if(r<=mid) return sumtree(root->left,l,r);
+        
+        return sumtree(root->left,l,mid)+sumtree(root->right,mid+1,r);
+        
+
+    }
+    NumArray(vector<int>& v) {
+        int n=v.size();
+        root=buildtree(v,0,n-1);
+    }
+    
+    void update(int index, int data) {
+        
+        int g=modifytree(index,data,root);
+    }
+    
+    int sumRange(int left, int right) {
+        return sumtree(root,left,right);
     }
 };
 
-
-// Your NumArray object will be instantiated and called as such:
-// NumArray numArray(nums);
-// numArray.sumRange(0, 1);
-// numArray.update(1, 10);
-// numArray.sumRange(1, 2);
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
