61141fa1034e53877d5bb127e9ee3cae75722bbf
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-01 08:08:03 +0530

    Time: 3 ms (44.81%), Space: 5.9 MB (75.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -4,16 +4,22 @@ public:
         int N=(m-1)+(n-1);
         int r=min(m-1,n-1);
         
-        double ans=1;
-        int h=N-r+1;
+        long long ans=1;
+//         int h=N-r+1;
+//         for(int i=1;i<=r;i++)
+//         {
+//             ans=ans*h/i;
+//             h++;
+            
+            
+//         }
+        long long int numerator=N-r+1;
         for(int i=1;i<=r;i++)
         {
-            ans=ans*h/i;
-            h++;
-            
-            
+            ans=ans*numerator/i;
+            numerator++;
         }
         
-        return (int)ans;
+        return ans;
     }
 };
