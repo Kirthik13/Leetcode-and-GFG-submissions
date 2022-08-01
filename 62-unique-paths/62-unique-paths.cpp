0773e903bde4b4316c9c209db471330034f0c8d3
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-01 08:22:56 +0530

    Time: 3 ms (44.81%), Space: 6.3 MB (53.20%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -16,6 +16,19 @@ public:
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+   
+        
+        
+//         dp[m-1][n-1]=1;
+//         for(int r=m-2;r>=0;r--)
+//         {
+//             for(int c=n-2;c>=0;c--)
+//             {
+//                 dp[r][c]=dp[r][c+1]+dp[r+1][c];
+//             }
+//         }
+        
+        // return dp[0][0];
         return fn(0,0,m,n,dp);
     }
 };
