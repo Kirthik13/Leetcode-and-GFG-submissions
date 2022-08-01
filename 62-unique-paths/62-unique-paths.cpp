58b7ce4296d3570e65a35ca62b39746e82ca30d8
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-01 08:08:37 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,17 +2,10 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         int N=(m-1)+(n-1);
-        int r=min(m-1,n-1);
+        int r=n-1;
         
         long long ans=1;
-//         int h=N-r+1;
-//         for(int i=1;i<=r;i++)
-//         {
-//             ans=ans*h/i;
-//             h++;
-            
-            
-//         }
+
         long long int numerator=N-r+1;
         for(int i=1;i<=r;i++)
         {
