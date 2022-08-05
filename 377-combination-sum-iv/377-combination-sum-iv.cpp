e1ed385b037d776a78239d95f396fc6850122472
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-05 08:50:00 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -17,7 +17,24 @@ public:
         return dp[sum]=c;
     }
     int combinationSum4(vector<int>& v, int target) {
-        vector<int>dp(target+1,-1);
-        return fn(v,0,target,dp);
+        vector<int>dp(target+1,0);
+        
+        dp[target]=1;
+        
+        for(int sum=target-1;sum>=0;sum--)
+        {
+            long long int c{};
+            for(auto &i:v)
+            {
+                if(i+sum<=target)
+                {
+                    c+=dp[i+sum];
+                }
+            }
+            dp[sum]=c;
+        }
+        
+        return dp[0];
+        // return fn(v,0,target,dp);
     }
 };
