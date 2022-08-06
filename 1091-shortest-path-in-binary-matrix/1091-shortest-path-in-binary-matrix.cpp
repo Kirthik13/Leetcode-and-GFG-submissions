ff25c9fb9b6da642d786d3e2bba8a293e63be80d
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-06 19:22:31 +0530

    Time: 62 ms (96.71%), Space: 19.4 MB (71.15%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -11,65 +11,47 @@ public:
         int ans=INT_MAX,m=grid.size(),n=grid[0].size();
         if(grid[0][0]==1 or grid[m-1][n-1]==1) return -1;
         
-         queue<pair<int, int>> queue;
-        queue.push(make_pair(0,0));
-        vector<vector<int>> directions = {{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
+         // queue<pair<int, int>> queue;
+        // queue.push(make_pair(0,0));
+        // vector<vector<int>> directions = {{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
         grid[0][0] = 1;
-        int row=m,col=n;
-        while(!queue.empty()){
-            auto curr = queue.front();
-                        queue.pop();
-
-            int x = curr.first, y = curr.second;
-            if( x == row -1 && y == col -1) return grid[x][y];
-
-            for(auto direction : directions){
-                int nx = x + direction[0];
-                int ny = y + direction[1];
-                if(!isvalid(grid,nx,ny,m,n)){
-                    queue.push(make_pair(nx,ny));
-                    grid[nx][ny] = grid[x][y] + 1;
-                }
+      
+        
+        
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        
+        
+        while(!q.empty())
+        {
+            auto node=q.front();
+            q.pop();
+            
+            auto x=node.first;
+            auto y=node.second;
+            auto wt=grid[x][y];
+            
+            if(x==m-1 and y==n-1)
+            {
+                        return wt;
             }
-        }
-        return -1;
-        
-        
-        
-        // queue<pair<int,int>>q;
-//         q.push({0,0});
-//         grid[0][0]=1;
-        
-        
-//         while(!q.empty())
-//         {
-//             auto node=q.front();
-//             auto x=node.first;
-//             auto y=node.second;
-//             auto wt=grid[x][y];
-            
-//             if(x==m-1 and y==n-1)
-//             {
-//                         return wt;
-//             }
-//             for(auto &it:dir)
-//             {
-//                 int newx=it[0]+x;
-//                 int newy=it[1]+y;
+            for(auto &it:dir)
+            {
+                int newx=it[0]+x;
+                int newy=it[1]+y;
                 
-//                 if(!isvalid(grid,newx,newy,m,n))
-//                 {
+                if(!isvalid(grid,newx,newy,m,n))
+                {
                    
-//                     q.push({newx,newy});
-//                     grid[newx][newy]=1+grid[x][y];
-//                 }
+                    q.push({newx,newy});
+                    grid[newx][newy]=1+grid[x][y];
+                }
                 
-//             }
-//             q.pop();
+            }
             
-//         }
+        }
         
-//         return ans;
+        return -1;
         
             
     }
