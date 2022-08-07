02028561053bfd2883fc181990bcfd309a086e95
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-07 17:19:31 +0530

    Time: 579 ms (18.98%), Space: 168.9 MB (5.12%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -2,60 +2,57 @@ class Solution {
 public:
     int minJumps(vector<int>& v) {
         int n=v.size();
-        unordered_map<int,vector<int>>m;
+        map<int,vector<int>>m;
         for(int i=0;i<v.size();i++)
         {
             m[v[i]].push_back(i);
         }
         
         queue<pair<int,int>>q;
-
         q.push({0,0});
-        
-        vector<int>vis(n);
+        vector<int>vis(n,0);
         int ans=INT_MAX;
         
         while(!q.empty())
         {
-            auto it=q.front();
+            auto idx=q.front().first;
+            auto dis=q.front().second;
+            vis[idx]=1;
             q.pop();
-            int elidx=it.first;
-             if(vis[elidx]==1)
+            
+            if(idx==n-1)
             {
-                // cout<<"v : "<<v[x][y]<<endl;
-                 // if(vis[{dx,dy}]==1){
-                    // cout<<"vis : "<<vis[{dx,dy}]<<endl;
-                    // cout<<"x : "<<x<<" y  : "<<y<<endl;
-                    // }
+                ans=min(dis,ans);
+               
                 continue;
             }
-            vis[elidx]=1;
-            int steps=it.second;
-            
-            if(elidx==n-1)
-            {
-                ans=min(ans,steps);
-                
+            if(idx+1<n and vis[idx+1]==0){
+                q.push({idx+1,dis+1});
             }
-            else
+            
+            if(idx-1>=0 and vis[idx-1]==0)
             {
-                if(elidx+1<n and m.find(v[elidx+1])!=m.end() and !vis[elidx+1])
+                            q.push({idx-1,dis+1});
+
+            }
+            
+             for(auto j:m[v[idx]])
                 {
-                q.push({elidx+1,steps+1});
-                }
-                if(elidx-1>=0 and m.find(v[elidx-1])!=m.end() and !vis[elidx-1]){
-                q.push({elidx-1,steps+1});
-                }
-                for(auto j:m[v[elidx]])
-                {
-                    if( j!=elidx  and !vis[j])
+                    if(!vis[j] and j!=idx)
                     {
-                        q.push({j,steps+1});
+                        q.push({j,dis+1});
                     }
                 }
-            }
-            m.erase(v[elidx]);
+            
+            m.erase(v[idx]);
+            
+           
+            
+            
         }
+        
         return ans;
+        
+        
     }
 };
