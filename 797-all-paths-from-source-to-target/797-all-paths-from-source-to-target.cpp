33d7736b6272743d90108258fe60b2f44add58f1
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-07 17:30:37 +0530

    Time: 40 ms (28.51%), Space: 19.2 MB (11.81%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -2,40 +2,64 @@ class Solution {
 public:
     vector<vector<int>>totans;
     
-    void fn(vector<vector<int>>& v,int node,vector<int>&vis,vector<int>&temp,int n)
-    {
-        if(node==n-1)
-        {
-            temp.push_back(node);
-            totans.push_back(temp);
-            temp.pop_back();
+//     void fn(vector<vector<int>>& v,int node,vector<int>&vis,vector<int>&temp,int n)
+//     {
+//         if(node==n-1)
+//         {
+//             temp.push_back(node);
+//             totans.push_back(temp);
+//             temp.pop_back();
             
-            return;
-        }
+//             return;
+//         }
         
         
-        vis[node]=1;
-        temp.push_back(node);
-        for(auto &it:v[node])
-        {
-            if(!vis[it])
-            {
+//         vis[node]=1;
+//         temp.push_back(node);
+//         for(auto &it:v[node])
+//         {
+//             if(!vis[it])
+//             {
                 
-                fn(v,it,vis,temp,n);
-            }
-        }
-        if(temp.size()>0) temp.pop_back();
-        vis[node]=0;
+//                 fn(v,it,vis,temp,n);
+//             }
+//         }
+//         if(temp.size()>0) temp.pop_back();
+//         vis[node]=0;
         
-    }
+//     }
     
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& v) {
         int n=v.size();
         
         vector<int>vis(n,0);
-        vector<int>temp;
+       
         
-        fn(v,0,vis,temp,n);
+        // fn(v,0,vis,temp,n);
+        
+        queue<pair<int,vector<int>>>q;
+        q.push({0,{}});
+        
+        while(!q.empty())
+        {
+            auto node=q.front().first;
+            auto temp=q.front().second;
+            
+            q.pop();
+            
+            if(node==n-1)
+            {
+                temp.push_back(node);
+                 totans.push_back(temp);
+                continue;
+            }
+            
+            temp.push_back(node);
+            for(auto &it:v[node])
+            {
+                q.push({it,temp});
+            }
+        }
         
         return totans;
     }
