218c9eb11d2731a14f499b2b851c08ba6a6bfca3
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-08 07:40:18 +0530

    Time: 2617 ms (5.00%), Space: 292.8 MB (8.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    long long dp[2501][2502];
-    int fn(vector<int>&v ,int idx,int n,int prev)
+    // long long dp[2501][2502];
+    int fn(vector<int>&v ,int idx,int n,int prev,vector<vector<int>>&dp)
     {
         if(idx>=n)
         {
@@ -13,16 +13,17 @@ public:
         int call1{},call2{};
         if(prev==-1 or v[prev]<v[idx])
         {
-            call1=1+fn(v,idx+1,n,idx);
+            call1=1+fn(v,idx+1,n,idx,dp);
         }
-        call2=fn(v,idx+1,n,prev);
+        call2=fn(v,idx+1,n,prev,dp);
         
         return dp[idx][prev+1]=max({call1,call2});
         
     }
     int lengthOfLIS(vector<int>&v ) {
         int n=v.size();
-        memset(dp,-1,sizeof(dp));
-        return fn(v,0,n,-1);
+        // memset(dp,-1,sizeof(dp));
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return fn(v,0,n,-1,dp);
     }
 };
