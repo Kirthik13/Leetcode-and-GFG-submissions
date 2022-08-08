6b8577dd9a34f0d46fe8b5845c0eec31074011cf
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-08 07:49:30 +0530

    Time: 1728 ms (7.66%), Space: 10.5 MB (47.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -22,7 +22,7 @@ public:
     }
     int lengthOfLIS(vector<int>&v ) {
         int n=v.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        vector<int>dp(n+1,0);
         
         for(int idx=n-1;idx>=0;idx--)
         {
@@ -31,18 +31,18 @@ public:
                 int call1{},call2{};
                 if(prev==-1 or v[prev]<v[idx])
                 {
-                    call1=1+dp[idx+1][idx+1];
+                    call1=1+dp[idx+1];
                 }
                 
                 
-                call2=dp[idx+1][prev+1];
+                call2=dp[prev+1];
         
-                dp[idx][prev+1]=max({call1,call2});
+                dp[prev+1]=max({call1,call2});
         
             }
         }
         
-        return dp[0][0];
+        return dp[0];
         // return fn(v,0,n,-1,dp);
     }
 };
