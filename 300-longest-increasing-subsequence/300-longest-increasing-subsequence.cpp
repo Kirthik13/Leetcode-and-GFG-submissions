9f147639f6083cf99846f2000b04ce7c20cc87c4
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-08 07:39:15 +0530

    Time: 2565 ms (5.00%), Space: 66.1 MB (15.41%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& v) {
-        
-        vector<int>temp;
-        temp.push_back(v[0]);
-        for(int i=1;i<v.size();i++)
+    long long dp[2501][2502];
+    int fn(vector<int>&v ,int idx,int n,int prev)
+    {
+        if(idx>=n)
         {
-            if(v[i]>temp.back())
-            {
-                temp.push_back(v[i]);
-            }
-            else{
-                auto idx=lower_bound(begin(temp),end(temp),v[i])-begin(temp);
-                
-                temp[idx]=v[i];
-            }
+            return 0;
         }
-        return temp.size();
+        
+        if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
+        
+        int call1{},call2{};
+        if(prev==-1 or v[prev]<v[idx])
+        {
+            call1=1+fn(v,idx+1,n,idx);
+        }
+        call2=fn(v,idx+1,n,prev);
+        
+        return dp[idx][prev+1]=max({call1,call2});
+        
+    }
+    int lengthOfLIS(vector<int>&v ) {
+        int n=v.size();
+        memset(dp,-1,sizeof(dp));
+        return fn(v,0,n,-1);
     }
 };
