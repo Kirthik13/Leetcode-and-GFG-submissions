5b36aa455a207745eaf83edff77a317de524f773
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-12 08:30:39 +0530

    Time: 974 ms (6.98%), Space: 172.7 MB (8.61%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -1,105 +1,40 @@
-class segmentnode
-{
-  public:
-    int start,end,val;
-    segmentnode* left;
-    segmentnode* right;
-    
-    segmentnode(int l,int r)
-    {
-        this->start=l;
-        this->end=r;
-        this->val=0;
-        left=NULL;
-        right=NULL;
-        
-        
-        
-    }
-        
-    
-};
 class NumArray {
 public:
-    segmentnode* root;
+    // int bit[100001];
+     vector<int> bit;
+     vector<int> arr;
     
-    
-    int updatetree(segmentnode* root,int idx,int data)
+    void update1(int i, int x)
     {
-        if(root==NULL) return 0;
-        int diff;
-        if(root->start==idx and root->end==idx)
-        {
-            diff=data-root->val;
-            root->val=data;
-            return diff;
-                
-        }
-        
-        int mid=(root->start+root->end)/2;
-        
-        if(idx>mid)
-        {
-            diff=updatetree(root->right,idx,data);
-        }
-        else{
-            diff=updatetree(root->left,idx,data);
-            
-        }
-        
-        root->val=root->val+diff;
-        return diff;
-        
+        int n=bit.size();
+        for(; i < n; i += (i&-i))
+            bit[i] += x;
     }
-    
-    segmentnode* buildtree(vector<int>&v,int l,int r)
-    {
-        if(l>r) return NULL;
-        
-        segmentnode* node=new segmentnode(l,r);
-        if(l==r)
-        {
-            node->val=v[l];
-            return node;
-        }
-        
-        int mid=(l+r)/2;
-        node->left=buildtree(v,l,mid);
-        node->right=buildtree(v,mid+1,r);
-        
-        node->val=node->left->val+node->right->val;
-        
-        return node;
-        
-    }
-    
-    int sumtree(segmentnode* root,int l,int r)
-    {
-        if(root==NULL) return 0;
-        if(root->start==l and root->end==r)
-        {
-            return root->val;
-        }
-        
-        int mid=(root->start+root->end)/2;
-        if(l>mid) return sumtree(root->right,l,r);
-        if(r<=mid) return sumtree(root->left,l,r);
-        
-        return sumtree(root->left,l,mid)+sumtree(root->right,mid+1,r);
-        
+
+    int sum(int i){
+        long long int ans = 0;
+        for(; i > 0; i -= (i&-i))
+            ans += bit[i];
+        return ans;
     }
     NumArray(vector<int>& v) {
-         int n=v.size();
-        root=buildtree(v,0,n-1);
+              bit = vector<int>(v.size()+1,0);
+              arr = v;
+
+        for(int i=0;i<v.size();i++)
+        {
+            update1(i+1,v[i]);
+        }
     }
     
     void update(int index, int val) {
-        int g=updatetree(root,index,val);
+         int diff = val-arr[index];
+        arr[index]=val;
+        update1(index+1,diff);
     }
     
     int sumRange(int left, int right) {
-                return sumtree(root,left,right);
-
+        return sum(right+1)-sum(left);
     }
 };
 
