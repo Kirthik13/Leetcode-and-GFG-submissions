59ddcba0bd6464bc0a946ecdbee613c7b6f362da
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-13 08:18:50 +0530

    Time: 267 ms (70.76%), Space: 21.7 MB (80.71%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,83 +1,37 @@
 class Solution {
 public:
-    vector<int> findSubstring(string s, vector<string>& words) {
-        if(words[0].size()>s.size()) return {};
+    vector<int> findSubstring(string s, vector<string>& v) {
+        int n=v.size();
+        int wordsiz=v[0].size();
+        int win=n*wordsiz;
         
-        int win=words[0].size()*words.size();
-        unordered_map<string,int>m1;
-        for(auto &i:words)
+        unordered_map<string,int>m,temp;
+        
+        vector<int>ans;
+        int strsize=s.size();
+        for(auto &i:v) m[i]++;
+        
+        for(int i=0;i<=strsize-win;i++)
         {
-            m1[i]++;
-        }
-        vector<int>v;
-        // cout<<"win : "<<win<<endl;
-        for(int i=0;i<s.size();i++)
-        {
-            if(i>=win-1)
+            for(int j=i;j<i+win;j+=wordsiz)
             {
-                unordered_map<string, int>temp_map=m1;
-                bool bugFound=false;
-                string sam=s.substr(i-(win-1),win);
-                // cout<<"sam : "<<sam<<endl;
-                int currwin=words[0].size();
-                // cout<<"currwin : "<<currwin<<endl;
-                for(int j=i-(win-1);j<=i;j+=currwin)
-                {
-                    // cout<<i-(win-1)<<endl;
-                     string temp=s.substr(j,currwin);
+                string sam=s.substr(j,wordsiz);
                 
-                if(m1.find(temp)==m1.end() || temp_map[temp]<=0) {
-                    bugFound=true;
+                if(m.find(sam)!=m.end())
+                {
+                    temp[sam]++;
+                }
+                else{
                     break;
-                } else {
-                    temp_map[temp]--;
-                } 
                 }
                 
-                if(!bugFound)
-                {
-                    v.emplace_back(i-(win-1));
-                    temp_map.clear();
-                }
             }
+            
+            if(temp==m) ans.push_back(i);
+            
+            temp.clear();
         }
-        return v;
-        
-//          int wordLen=words.size();
-//         int eachWordLen=words[0].size();
-//         int window=wordLen*eachWordLen;
-//         string temp="";
-        
-//         if(window>s.size()) {
-//             return {};
-//         }
-        
-//         unordered_map<string, int>word_count;
-//         vector<int>result;
-//         for(int i=0; i<wordLen; i++) {
-//             word_count[words[i]]++;
-//         }
-        
-//         for(int i=0; i<s.size()-window+1; i++) {
-//             unordered_map<string, int>temp_map=word_count;
-//             bool bugFound=false;
-            
-//             for(int j=i; j<i+window; j=j+eachWordLen) {
-//                 temp=s.substr(j,eachWordLen);
-//                 cout<<"temp : "<<temp<<endl;
-//                 if(word_count.find(temp)==word_count.end() || temp_map[temp]<=0) {
-//                     bugFound=true;
-//                     break;
-//                 } else {
-//                     temp_map[temp]--;
-//                 } 
-//             }
-            
-//             if(!bugFound) {
-//                 result.push_back(i);
-//                 temp_map.clear();
-//             }
-//         }
-//         return result;
+            return ans;
+
     }
 };
