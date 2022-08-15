ce3d17294c253cb5f50cec712455c599e3311b6c
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-15 17:33:26 +0530

    Time: 19 ms (13.91%), Space: 12.1 MB (13.29%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,70 +1,33 @@
 class Solution {
 public:
-    int fn(vector<int>&v,int idx,int n,int buy, vector<vector<int>>&dp)
+    int fn(vector<int>&v,int idx,int n,int status,  vector<vector<int>>&dp)
     {
         if(idx>=n) return 0;
         
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
+        if(dp[idx][status]!=-1) return dp[idx][status];
         
-        int curr_res{};
+        int curr{};
         
-        
-        
-        if(buy)
+        if(status==0)
         {
-            int bought=fn(v,idx+1,n,false,dp)-v[idx];
-            int notbought=fn(v,idx+1,n,buy,dp);
+            int buy=fn(v,idx+1,n,1,dp)-v[idx];
+            int notbuy=fn(v,idx+1,n,0,dp);
+            curr=max(buy,notbuy);
             
-            curr_res=max(bought,notbought);
-
         }
         else{
-             int sold=fn(v,idx+2,n,true,dp)+v[idx];
-            int notsold=fn(v,idx+1,n,buy,dp);
             
-            curr_res=max(sold,notsold);
-
-            
+            int sell=fn(v,idx+2,n,0,dp)+v[idx];
+            int notsell=fn(v,idx+1,n,1,dp);
+            curr=max(sell,notsell);
         }
-        return dp[idx][buy]=curr_res;
+        
+        return dp[idx][status]=curr;
     }
+
     int maxProfit(vector<int>& v) {
         int n=v.size();
-        
-        // vector<vector<int>>dp(n+2,vector<int>(2,0));
-
-        vector<int>dp(2,0);
-        vector<int>curr(2,0);
-            int temp=dp[1];
-
-        
-        for(int idx=n-1;idx>=0;idx--)
-        {
-            
-
-          
-                
-                    int bought=dp[false]-v[idx];
-                    int notbought=dp[true];
-
-                   curr[1]=max(bought,notbought);
-
-              
-                     int sold=temp+v[idx];
-                    int notsold=dp[false];
-
-                    curr[0]=max(sold,notsold);
-
-
-                
-              
-            
-            temp=dp[true];
-            dp=curr;
-        }
-        
-        
-        return dp[1];
-        // return fn(v,0,n,1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        return fn(v,0,n,0,dp);
     }
 };
