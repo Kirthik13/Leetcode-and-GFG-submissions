148622ad4ac567bac3f4f1a7ee7d717020186129
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-15 17:26:31 +0530

    Time: 93 ms (12.05%), Space: 21.3 MB (5.94%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -25,9 +25,37 @@ public:
         return dp[idx][k][status]=curr;
         
     }
-    int maxProfit(int k, vector<int>& v) {
+    int maxProfit(int k1, vector<int>& v) {
         int n=v.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(k+1,vector<int>(2,-1)));
-        return fn(v,0,n,0,k,dp);
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(k1+1,vector<int>(2,0)));
+        
+        for(int idx=n-1;idx>=0;idx--)
+        {
+            for(int k=1;k<=k1;k++)
+            {
+                for(int status=1;status>=0;status--)
+                {
+                    int curr{};
+        
+                    if(status==0)
+                    {
+                        int buy=dp[idx+1][k][1]-v[idx];
+                        int notbuy=dp[idx+1][k][0];
+                        curr=max(buy,notbuy);
+                    }
+                    else{
+                         int sell=dp[idx+1][k-1][0]+v[idx];
+                        int notsell=dp[idx+1][k][1];
+                        curr=max(sell,notsell);
+
+                    }
+
+                    dp[idx][k][status]=curr;
+                }
+            }
+        }
+        
+        return dp[0][k1][0];
+        // return fn(v,0,n,0,k,dp);
     }
 };
