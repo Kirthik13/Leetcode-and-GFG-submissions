58c276fcec6eb6307e97e394e1e74b9f62ab7d08
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-21 23:11:25 +0530

    Time: 34 ms (56.14%), Space: 17.8 MB (38.61%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int candy(vector<int>& v) {
-        vector<int>tot(v.size(),1);
-        int ans{};
-        if(v.size()<=1)
-		 return v.size();
-        for(int i=1;i<v.size();i++)
+        
+        int n=v.size();
+        vector<int>curr(n,1);
+       
+        for(int i=1;i<n;i++)
         {
             if(v[i-1]<v[i])
             {
-                tot[i]=tot[i-1]+1;
+                curr[i]=curr[i-1]+1;
             }
         }
-        
-        for(int i=v.size()-1;i>0;i--)
+       
+         
+        for(int i=n-1;i>0;i--)
         {
             if(v[i-1]>v[i])
             {
-                tot[i-1]=max(tot[i]+1,tot[i-1]);
+                curr[i-1]=max(curr[i]+1,curr[i-1]);
             }
-             
         }
-        for(auto &i:tot) ans+=i;
-        return ans;
+        int sum=accumulate(begin(curr),end(curr),0);
+        
+        return sum;
         
     }
 };
