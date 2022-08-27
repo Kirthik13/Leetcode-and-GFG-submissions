ad9103274852f179f4c28d17752b61777050d711
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-27 22:08:51 +0530

    Time: 123 ms (5.02%), Space: 90.3 MB (7.84%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,44 +1,37 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& v1, vector<int>& v2) {
+    void fn( priority_queue<int>&max,priority_queue<int,vector<int>,greater<int>>&min,int el)
+    {
+        max.push(el);
+        min.push(max.top());
+        max.pop();
         
-        int m=v1.size(),n=v2.size();
-        if(n<m) return findMedianSortedArrays(v2,v1);
-
-        int l=0,r=m;
-        
-        while(l<=r)
+        if(max.size()<min.size())
         {
-            int cut1=(l+r)/2;
-            int cut2=(m+n+1)/2-cut1;
-           
-            int l1=(cut1-1>=0)?v1[cut1-1]:INT_MIN;
-            int l2=(cut2-1>=0)?v2[cut2-1]:INT_MIN;
-            
-            int r1=(cut1<m)?v1[cut1]:INT_MAX;
-            int r2=(cut2<n)?v2[cut2]:INT_MAX;
-
-         
-            if(l1<=r2 and l2<=r1)
-            {
-                if((m+n)%2==0)
-                {
-                    return (max(l1,l2)+min(r1,r2))/2.0;
-                }
-                return max(l1,l2);
-
-            }
-            else if(l1>r2)
-            {
-                r=cut1-1;
-            }
-            else{
-                l=cut1+1;
-
-            }
-                
+            max.push(min.top());
+            min.pop();
         }
-        return 0.0;
+    }
+    double findMedianSortedArrays(vector<int>& v1, vector<int>& v2) {
+        priority_queue<int>max;
+        priority_queue<int,vector<int>,greater<int>>min;
+        
+        for(auto &i:v1)
+        {
+            fn(max,min,i);    
+        }
+
+        for(auto &i:v2)
+        {
+            fn(max,min,i);    
+        }
+        
+        int m=max.size();
+        int n=min.size();
+        
+        return ((m+n)&1)?(max.top()*1.0):(max.top()+min.top())/2.0;
+        
+        
         
     }
 };
