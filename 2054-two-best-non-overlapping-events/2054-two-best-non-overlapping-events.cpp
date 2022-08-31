06664e109f198d4223b28741f778aa6bd8684643
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-31 13:24:06 +0530

    Time: 1292 ms (28.03%), Space: 224.2 MB (5.92%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
-    int maxTwoEvents(vector<vector<int>>& v) {
-        vector<vector<int>>temp;
-        
-        for(auto &i:v)
+            // map<pair<pair<int,int>,int>,int>dp;
+                map<pair<int,int>,int>dp;
+
+ 
+    int fn(vector<vector<int>>& v,int idx,int n,int c) 
+    {
+        if(c==2) return 0;
+        if(idx>=n)
         {
-            temp.push_back({i[0],1,i[2]});
-            temp.push_back({i[1]+1,0,i[2]});
-            
-        }
-        
-        sort(begin(temp),end(temp));
-        
-        int m=0,ans{};
-        for(auto &i:temp)
-        {
-            if(i[1])
+            if(c==1 or c==2)
             {
-                ans=max(ans,m+i[2]);
+                            return 0;
+                
+
             }
-            else{
-                m=max(m,i[2]);
-            }
+            return INT_MIN;
         }
-        return ans;
+        
+        if(dp.find({idx,c})!=dp.end()) return dp[{idx,c}];
+        
+        
+        vector<int>ans={v[idx][1],INT_MAX,INT_MAX};
+        
+        //Searching the event whose starting time > ending time of previous event selected
+        int nextidx=upper_bound(begin(v),end(v),ans)-begin(v);
+        
+        
+        int call1{},call2{};
+        
+            call1=v[idx][2]+fn(v,nextidx,n,c+1);
+        
+             call2=fn(v,idx+1,n,c);        
+        
+        return dp[{idx,c}]=max(call1,call2);
+        
+    }
+    int maxTwoEvents(vector<vector<int>>& v) {
+        sort(begin(v),end(v));
+        
+       int n=v.size();
+        return fn(v,0,n,0);
     }
 };
