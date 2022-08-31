d75ad0aa94ce7ebf17906e863ac83018f7c5da6e
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-31 16:30:14 +0530

    Time: 5 ms (44.47%), Space: 5.9 MB (39.43%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
+    int dp[31];
+    int fn(int n)
+    {
+        if(n<0) return 0;
+        if(n<=1) return n;
+        
+        if(dp[n]!=-1) return dp[n];
+        
+        return dp[n]=fn(n-1)+fn(n-2);
+    }
     int fib(int n) {
         
-        if(n<=1) return n;
-        int prev=1,nextprev=0;
-        for(int i=2;i<=n;i++)
-        {
-            int curr=prev+nextprev;
-            nextprev=prev;
-            prev=curr;
-            
-            
-        }
-        return prev;
+        memset(dp,-1,sizeof(dp));
+        return fn(n);
         
     }
 };
