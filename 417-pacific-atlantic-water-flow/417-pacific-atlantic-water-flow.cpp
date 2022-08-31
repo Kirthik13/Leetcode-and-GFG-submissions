4cd5568b0500833bfc75ae210c43b79a676b92cc
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-31 09:34:40 +0530

    Time: 90 ms (33.42%), Space: 19 MB (49.23%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -4,56 +4,61 @@ public:
     
     bool isvalid(vector<vector<int>>& v,int r,int c,int m,int n,int val,vector<vector<int>>&vis)
     {
-        // cout<<r<<" "<<c<<endl;
-        // cout<<v[r][c]<<endl;
-        return r<0 or c<0 or c>=n or r>=m or v[r][c]>val or vis[r][c]==1;
+        return r<0 or c<0 or c>=n or r>=m or v[r][c]<val or vis[r][c]==1;
     }
-    bool cancross(vector<vector<int>>& v,int r,int c,int &canalt,int &canpac,int m,int n)
+   
+    void dfs(vector<vector<int>>& v,int r,int c,int m,int n,vector<vector<int>>&vis) 
     {
-        if(r<0) canpac=1;
-        if(c<0) canpac=1;
-        if(r>=m) canalt=1;
-        if(c>=n) canalt=1;
-        
-        if(canalt and canpac) return 1;
-        return 0;
-    }
-    void dfs(vector<vector<int>>& v,int r,int c,int &canalt,int &canpac,int m,int n,vector<vector<int>>&vis) 
-    {
+       
         vis[r][c]=1;
-        if(canpac and canalt) return;
         for(auto &i:dir)
         {
             int newr=i[0]+r;
             int newc=i[1]+c;
             
-            if(canpac and canalt) return;
             
-            if(cancross(v,newr,newc,canalt,canpac,m,n))
-            {
-                return;
-            }
+           
             if(!isvalid(v,newr,newc,m,n,v[r][c],vis))
             {
-                dfs(v,newr,newc,canalt,canpac,m,n,vis);
+                dfs(v,newr,newc,m,n,vis);
             }
             
         }
+        // int m = matrix.length, n = matrix[0].length;
+      // vis[r][c] = true;
+      // for(auto &d: dir){
+      //   int x = r + d[0];
+      //   int y = c + d[1];
+      //   // if(x < 0 || y < 0 || x >= m || y >= n || vis[x][y] || v[r][c] > v[x][y]){
+      //     if(isvalid(v,x,y,m,n,v[r][c],vis)){
+      //     continue;
+      //   }
+      //   dfs(v, x, y,m,n,vis);
+      // }
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& v) {
          vector<vector<int>>totans;
         
         int m=v.size(),n=v[0].size();
-                vector<vector<int>>vis(m,vector<int>(n));
+        vector<vector<int>>atl(m,vector<int>(n));
+        vector<vector<int>>pac(m,vector<int>(n));
+        
+       for(int i = 0; i < m; i++){
+        dfs(v,i,  0,m,n,pac);
+        dfs(v,i, n - 1,m,n,atl);
+      }   
+      
+      for(int i = 0; i < n; i++){
+        dfs(v,0,  i,m,n,pac);
+        dfs(v,m - 1, i,m,n,atl);
+      }     
+        
         
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                int canalt=0,canpac=0;
-                vis.assign(m,vector<int>(n,0));
-                dfs(v,i,j,canalt,canpac,m,n,vis);
-                if(canalt and canpac)
+                if(pac[i][j] and atl[i][j])
                 {
                     totans.push_back({i,j});
                 }
