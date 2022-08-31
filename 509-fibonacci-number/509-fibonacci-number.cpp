99c8cf2d595019eee80ef089867e1372e88ebfa4
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-08-31 16:31:09 +0530

    Time: 11 ms (36.89%), Space: 10.2 MB (8.63%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    int dp[31];
+    int dp[1000000];
+    int getx(int a)
+    {
+        return a*10000;
+    }
     int fn(int n)
     {
         if(n<0) return 0;
         if(n<=1) return n;
         
-        if(dp[n]!=-1) return dp[n];
+        if(dp[getx(n)]!=-1) return dp[getx(n)];
         
-        return dp[n]=fn(n-1)+fn(n-2);
+        return dp[getx(n)]=fn(n-1)+fn(n-2);
     }
     int fib(int n) {
         
