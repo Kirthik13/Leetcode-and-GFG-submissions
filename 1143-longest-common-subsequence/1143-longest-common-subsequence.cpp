4131c44c46cccc6bf8508dd2916c22e5f5e893e5
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-09-01 16:18:10 +0530

    Time: 40 ms (44.56%), Space: 10.9 MB (64.56%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    // int dp[1001][1001];
-    int fn(string &s, string &t,int idx1,int idx2,int m,int n,vector<vector<int>>&dp) 
+    int dp[1001][1001];
+    int fn(string &s, string &t,int idx1,int idx2,int m,int n) 
     {
         if(idx1>=m or idx2>=n) return 0;
         
@@ -9,23 +9,22 @@ public:
         
         if(s[idx1]==t[idx2])
         {
-            return dp[idx1][idx2]=1+fn(s,t,idx1+1,idx2+1,m,n,dp);
+            return dp[idx1][idx2]=1+fn(s,t,idx1+1,idx2+1,m,n);
         }
-        int call1=fn(s,t,idx1+1,idx2,m,n,dp);
-        int call2=fn(s,t,idx1,idx2+1,m,n,dp);
+        int call1=fn(s,t,idx1+1,idx2,m,n);
+        int call2=fn(s,t,idx1,idx2+1,m,n);
         
         return dp[idx1][idx2]=max(call1,call2);
-         
-        
+       
         
     }
     int longestCommonSubsequence(string s, string t) {
         
          // if(s.size()<t.size()) swap(s,t);
         int m=s.size(),n=t.size();
-        // memset(dp,-1,sizeof(dp));
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return fn(s,t,0,0,m,n,dp);
+        memset(dp,-1,sizeof(dp));
+        
+        return fn(s,t,0,0,m,n);
     }
 };
 
