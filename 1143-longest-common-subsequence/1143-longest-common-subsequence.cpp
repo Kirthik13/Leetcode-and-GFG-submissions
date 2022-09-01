46b3ae80784ee4d7f3b8e697959426d5491b6d43
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-09-01 16:25:20 +0530

    Time: 47 ms (32.92%), Space: 11.4 MB (61.20%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -18,34 +18,54 @@ public:
        
         
     }
-    int longestCommonSubsequence(string s, string t) {
+    
+    void printset(string &s, string &t,int idx1,int idx2,int m,int n,vector<char>&res,int po) 
+    {
+        if(idx1>=m or idx2>=n)
+        {
+            for(auto &i:res)
+            {
+                cout<<i;
+            }
+            cout<<endl;
+            return ;
+        }
         
-         // if(s.size()<t.size()) swap(s,t);
+        
+        if(s[idx1]==t[idx2])
+        {
+            // cout<<s[idx1]<<" ";
+            if(res.size()>po)
+            {
+                res[po]=s[idx1];
+            }
+            dp[idx1][idx2]=1+fn(s,t,idx1+1,idx2+1,m,n);
+            printset(s,t,idx1+1,idx2+1,m,n,res,po+1);
+            
+        }
+        else{
+            int call1=fn(s,t,idx1+1,idx2,m,n);
+            int call2=fn(s,t,idx1,idx2+1,m,n);
+
+            if(max(call1,call2)==call1)
+            {
+                printset(s,t,idx1+1,idx2,m,n,res,po);
+            }
+            else{
+                printset(s,t,idx1,idx2+1,m,n,res,po);
+
+            }
+        }
+       
+    }
+    int longestCommonSubsequence(string s, string t) {
         int m=s.size(),n=t.size();
         memset(dp,-1,sizeof(dp));
         
-        return fn(s,t,0,0,m,n);
+        int g=fn(s,t,0,0,m,n);
+        
+        vector<char>res(g);
+        printset(s,t,0,0,m,n,res,0);
+        return g;
     }
 };
-
-// class Solution {
-// public:
-//     int fn(string &s1,string &s2,int n,int m,int i1,int i2,vector<vector<int>>&dp)
-//     {
-//         if(n==i1 or m==i2) return 0;
-//         if(dp[i1][i2]!=-1)  return dp[i1][i2];
-//         if(s1[i1]==s2[i2])
-//         {
-//             return dp[i1][i2]=1+fn(s1,s2,n,m,i1+1,i2+1,dp);
-//         }
-        
-//            return dp[i1][i2]=max(fn(s1,s2,n,m,i1+1,i2,dp),fn(s1,s2,n,m,i1,i2+1,dp));
-        
-//     }
-//     int longestCommonSubsequence(string s1, string s2) {
-//         if(s1.size()<s2.size()) swap(s1,s2);
-//         int n=s1.size(),m=s2.size();
-//         vector<vector<int>>dp(1001,vector<int>(1001,-1));
-//        return fn(s1,s2,n,m,0,0,dp);
-//     }
-// };
