2d48af95aa61c159d4ac5bb3c877eb68f034cc43
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-09-02 19:52:53 +0530

    Time: 132 ms (82.34%), Space: 67.8 MB (11.03%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -6,10 +6,11 @@ public:
         for(int i=0;i<v.size();i++)
         {
             sum+=v[i];
+
+            if(sum<v[i]) sum=v[i];
             
             maxsf=max(maxsf,sum);
             
-            if(sum<0) sum=0;
         }
         return maxsf;
     }
