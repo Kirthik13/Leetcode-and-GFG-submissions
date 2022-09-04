7e59f8cad4e0af74ac67ed870f77f0d94481f4c0
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-09-04 07:23:04 +0530

    Time: 312 ms (50.00%), Space: 128.7 MB (40.00%) - LeetHub

---
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
@@ -0,0 +1,50 @@
+#define ll long long
+class Solution {
+public:
+    int maximumRobots(vector<int>& ct, vector<int>& rt, long long budget) {
+        
+        priority_queue<pair<ll,ll>>pq;
+        
+        ll curcost{};
+        int i{};
+        int ans{};
+        for(int j=0;j<ct.size();j++)
+        {
+            curcost+=rt[j];
+            pq.push({ct[j],j});
+            
+            if(pq.size() and (pq.top().first+(j-i+1)*curcost<=budget))
+            {
+                ans=max(ans,(j-i+1));
+            }
+            else{
+                
+                while(pq.size() and (pq.top().first+(j-i+1)*curcost>budget))
+                {
+                    curcost-=rt[i];
+                
+                    while(pq.size() and pq.top().second<=i) pq.pop();
+                
+                    i++;
+                
+                    
+
+                }
+                
+                if(pq.size() and (pq.top().first+(j-i+1)*curcost<=budget))
+                {
+                    ans=max(ans,(j-i+1));
+                }                
+            }
+            
+        }
+        
+        return ans;
+            
+            
+        
+        
+        
+        
+    }
+};
