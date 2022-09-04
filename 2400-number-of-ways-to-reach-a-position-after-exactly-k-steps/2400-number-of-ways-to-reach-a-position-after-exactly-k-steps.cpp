bfc772bb7b020e911b009945001795464977f2ed
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-09-04 09:34:50 +0530

    Time: 2082 ms (50.00%), Space: 187.4 MB (50.00%) - LeetHub

---
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -0,0 +1,55 @@
+#define ll long long
+#define mod 1000000007
+
+class Solution {
+public:
+    ll int fn(ll int st,ll int en,ll int k, map<pair<ll int,ll int>,ll int>&dp)
+    {
+        if(k==0)
+        {
+            if(st==en) return 1LL;
+            return 0LL;
+        }
+        // if(st==en) return 0;
+        if(k<=0 || st+k < en || st-k > en) return 0;
+        if(dp.find({st,k})!=dp.end()) return dp[{st,k}];
+        
+        ll int call1=fn(st+1,en,k-1,dp)%mod;
+        ll int call2=fn(st-1,en,k-1,dp)%mod;
+        
+        return dp[{st,k}]=(call1+call2)%mod;
+        
+    }
+    int numberOfWays(int st, int en, int k) {
+//         queue<pair<int,int>>q;
+        
+//         q.push({st,0});
+//         int cnt{};
+        
+//         while(!q.empty())
+//         {
+//             int n=q.size();
+//             for(int i=0;i<n;i++)
+//             {
+//                 auto node=q.front();
+//                 q.pop();
+//                 if(node.second==k)
+//                 {
+//                     if(node.first==en) cnt++;
+                    
+                        
+//                 }
+//                 else{
+//                     q.push({node.first-1,node.second+1});
+//                     q.push({node.first+1,node.second+1});
+                    
+//                 }
+//             }
+//         }
+        // return cnt;
+        
+        map<pair<ll int,ll int>,ll int>dp;
+        return fn(st,en,k,dp);
+        
+    }
+};
