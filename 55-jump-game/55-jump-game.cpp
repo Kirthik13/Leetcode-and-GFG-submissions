1a4f633fb53d92252bcf844d055348459cb6e076
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-09-09 11:34:37 +0530

    Time: 165 ms (26.59%), Space: 48.4 MB (44.84%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
-    int dp[10001];
-    bool fn(vector<int>&v,int idx,int n)
-    {
-        if(idx==n-1) return 1;
-        
-        if(dp[idx]!=-1) return dp[idx];
-        
-        for(int i=1;i<=v[idx];i++)
-        {
-            if(fn(v,idx+i,n)) return dp[idx]=1;
-        }
-        
-        return dp[idx]=0;
-        
-    }
     bool canJump(vector<int>& v) {
         int n=v.size();
-        // vector<int>dp(n,-1);
-        memset(dp,-1,sizeof(dp));
-        return fn(v,0,n);
+        int last=n-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(i+v[i]>=last) last=i;
+        }
+        return last<=0;
     }
 };
