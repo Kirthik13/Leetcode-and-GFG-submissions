9b379acca42d7b806513346fec889b26f07d63bb
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-09-09 09:43:37 +0530

    Time: 715 ms (43.70%), Space: 8.6 MB (5.52%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -7,40 +7,26 @@ public:
     }
     bool dfs(vector<vector<char>>& v,string &s,int idx,int sn,int r,int c,int m,int n, vector<vector<int>>&vis)
     {
+                if(v[r][c]!=s[idx]) return 0;
+
         if(idx==sn-1)
         {
-//             cout<<sn<<endl;
-//             cout<<r<<c<<endl;
+
            
-//             cout<<v[r][c]<<endl;
-            if(s[idx]==v[r][c]) return 1;
-            for(auto &i:dir)
-            {
-                int newr=i[0]+r;
-                int newc=i[1]+c;      
-
-                if(!isvalid(v,s,idx,sn,newr,newc,m,n,vis))
-                {
                      return 1;
-                }
-
-            }
-            return 0;
+             
         }
-        if(v[r][c]!=s[idx]) return 0;
+        
         vis[r][c]=1;
         
         for(auto &i:dir)
         {
             int newr=i[0]+r;
             int newc=i[1]+c;      
-                          // cout<<newr<<" "<<newc<<endl;
 
             if(!isvalid(v,s,idx+1,sn,newr,newc,m,n,vis))
             {
-                // vis[newr][newc]=1;
-                // cout<<s[idx]<<endl;
-              // cout<<newr<<" "<<newc<<endl;
+               
                 if(dfs(v,s,idx+1,sn,newr,newc,m,n,vis)) return 1;
             }
             
