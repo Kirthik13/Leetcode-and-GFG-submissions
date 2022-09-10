24c9c2b2403b4b14308eeb44d81e1b9f5ad92d06
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-09-10 07:53:34 +0530

    Time: 44 ms (18.61%), Space: 11.7 MB (66.59%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,61 +1,39 @@
 class Solution {
 public:
-    int fn(vector<int>&v,int idx,int n,int status,int k, vector<vector<vector<int>>>&dp)
+    int dp[1001][101][2];
+    int fn(vector<int>& v,int idx,int n,int k,int canbuy)
     {
-        if(idx>=n) return 0;
-        if(k==0) return 0;
-        
-        if(dp[idx][k][status]!=-1)  return dp[idx][k][status];
-        
-        int curr{};
-        
-        if(status==0)
+
+        if(idx>=n)
         {
-            int buy=fn(v,idx+1,n,1,k,dp)-v[idx];
-            int notbuy=fn(v,idx+1,n,0,k,dp);
-            curr=max(buy,notbuy);
+            // if(k==0) return 0;
+            return 0;
         }
-        else{
-             int sell=fn(v,idx+1,n,0,k-1,dp)+v[idx];
-            int notsell=fn(v,idx+1,n,1,k,dp);
-            curr=max(sell,notsell);
+        
+                if(k==0) return 0;
+
+            
+        if(dp[idx][k][canbuy]!=-1)   return dp[idx][k][canbuy];
+        
+        int call1{},call2{};
+        if(canbuy)
+        {
+            call1=fn(v,idx+1,n,k,0)-v[idx];
+            call2=fn(v,idx+1,n,k,canbuy);
             
         }
-        
-        return dp[idx][k][status]=curr;
-        
-    }
-    int maxProfit(int k1, vector<int>& v) {
-        int n=v.size();
-       vector<vector<int>>dp(k1+1,vector<int>(2,0));
-        
-        for(int idx=n-1;idx>=0;idx--)
-        {
-            for(int k=1;k<=k1;k++)
-            {
-                for(int status=1;status>=0;status--)
-                {
-                    int curr{};
-        
-                    if(status==0)
-                    {
-                        int buy=dp[k][1]-v[idx];
-                        int notbuy=dp[k][0];
-                        curr=max(buy,notbuy);
-                    }
-                    else{
-                         int sell=dp[k-1][0]+v[idx];
-                        int notsell=dp[k][1];
-                        curr=max(sell,notsell);
-
-                    }
-
-                    dp[k][status]=curr;
-                }
-            }
+        else{
+            call1=fn(v,idx+1,n,k-1,1)+v[idx];
+            call2=fn(v,idx+1,n,k,canbuy);
         }
         
-        return dp[k1][0];
-        // return fn(v,0,n,0,k,dp);
+        return dp[idx][k][canbuy]=max(call1,call2);
+        
+    }
+    int maxProfit(int k, vector<int>& v) {
+        int n=v.size();
+        // vector<vector<int>>dp()
+        memset(dp,-1,sizeof(dp));
+        return fn(v,0,n,k,1);
     }
 };
