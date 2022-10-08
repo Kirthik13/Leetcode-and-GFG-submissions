05cedaba88d2c8533ca45d9e64f7af0fd0432999
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-10-08 21:14:16 +0530

    Time: 1567 ms (5.01%), Space: 16.4 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -6,6 +6,7 @@ public:
         int ans=INT_MAX;
         for(int i=0;i<v.size()-2;i++)
         {
+            if(i!=0 and v[i-1]==v[i]) continue;
             int j=i+1;
             int k=v.size()-1;
             while(j<k)
@@ -24,11 +25,16 @@ public:
                
                 if(v[i]+v[j]+v[k]>=target)
                 {
+                    
+                    while(k>j and v[k]==v[k-1]) k--;
                     k--;
+                    
                     
                 }
                 else
                 {
+                    while(j<k and v[j]==v[j+1]) j++;
+                    
                     j++;
                 }
                 
