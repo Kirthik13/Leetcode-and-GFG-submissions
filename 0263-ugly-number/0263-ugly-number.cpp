b6e502bbab10969cd3db0c20811bae8b6b8aeaab
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-11-18 08:12:19 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (86.72%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -15,7 +15,7 @@ public:
         
         while(num % 2 == 0 && num) num/=2;
         while(num%3 ==0 && num) num/=3; 
-        while(num%4 ==0 && num) num/=4;
+        // while(num%4 ==0 && num) num/=4;
         while(num%5 ==0 && num) num/=5;
         return num ==1;
     }
