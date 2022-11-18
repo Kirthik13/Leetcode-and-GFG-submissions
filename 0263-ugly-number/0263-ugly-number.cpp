4629edada76694cc094ed5f41fcc0f61156f6f7b
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-11-18 08:11:37 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (86.72%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+ 
+    bool isUgly(int num) {
+        // for(int i=2;i*i<=n;i++)
+        // {
+        //     if(n%i==0)
+        //     {
+        //         for(int j=i;j<=n;j=j*2)
+        //         {
+        //             v[j]=1;
+        //         }
+        //     }
+        // }
+        
+        while(num % 2 == 0 && num) num/=2;
+        while(num%3 ==0 && num) num/=3; 
+        while(num%4 ==0 && num) num/=4;
+        while(num%5 ==0 && num) num/=5;
+        return num ==1;
+    }
+};
