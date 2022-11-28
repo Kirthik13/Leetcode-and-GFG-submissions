f2d2fcfad732482ef06901ded8aa485627a4d568
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-11-28 18:29:44 +0530

    Time: 1681 ms (19.42%), Space: 179 MB (52.49%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,37 +1,75 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& v) {
-         vector<vector<int>>tot(2);
+         // vector<vector<int>>tot(2);
         
-        map<int,int>m;    
-        set<int>st;
+        // map<int,int>m;    
+        // set<int>st;
         
-        for(auto &i:v)
+//         for(auto &i:v)
+//         {
+            
+//             st.insert(i[0]);
+//             st.insert(i[1]);
+            
+//             m[i[1]]++;
+            
+//         }
+        
+        
+//         for(auto &i:st)
+//         {
+//             if(m[i]==1)
+//             {
+//                 tot[1].push_back(i);
+//             }
+//             else 
+//             {
+//                 if(m[i]==0){
+//                 tot[0].push_back(i);
+//                 }
+//             }            
+            
+//         }
+        // return tot;
+        
+          vector<vector<int>>ans(2);
+        
+       map<int,int>m;
+        for(auto i:v)
         {
-            
-            st.insert(i[0]);
-            st.insert(i[1]);
-            
-            m[i[1]]++;
-            
-        }
-        
-        
-        for(auto &i:st)
-        {
-            if(m[i]==1)
-            {
-                tot[1].push_back(i);
+            if(m[i[0]]>=0){
+            m[i[0]]++;
             }
-            else 
-            {
-                if(m[i]==0){
-                tot[0].push_back(i);
+            // if(m.count(i[1])!=0){
+                if(m[i[1]]<=0){
+            m[i[1]]--;
                 }
-            }            
+            // else if(m[i[1]]>0)
+            else
+            {
+                m[i[1]]=-1;
+            }
+            // }
             
         }
-        return tot;
+//         for(auto i:m)
+//         {
+//             cout<<i.first<<" "<<i.second<<endl;
+//         }
+        
+        for(auto &i:m)
+        {
+            if(i.second>0)
+            {
+                ans[0].push_back(i.first);
+            }
+            else if(i.second==-1){
+                ans[1].push_back(i.first);
+            }
+        }
+        return ans;
+        
         
     }
 };
