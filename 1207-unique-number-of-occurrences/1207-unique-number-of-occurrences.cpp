41fd2c5fdfe0e535e3e0d6cdd10a16ed8e466a9d
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-11-30 08:29:32 +0530

    Time: 4 ms (69.41%), Space: 8.8 MB (6.37%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& v) {
-        map<int,int>m;
+//         map<int,int>m;
+//         for(auto &i:v)
+//         {
+//             m[i]++;
+//         }
+//         unordered_set<int>st;
+//         for(auto &i:m)
+//         {
+//             if(st.find(i.second)!=st.end()) return 0;
+//             st.insert(i.second);
+//         }
+        
+//         return 1;
+        
+        
+        vector<int>v2(2001);
+        
         for(auto &i:v)
         {
-            m[i]++;
+            v2[i+1000]++;
         }
-        unordered_set<int>st;
-        for(auto &i:m)
+        sort(begin(v2),end(v2));
+        for(int i=1;i<=2000;i++)
         {
-            if(st.find(i.second)!=st.end()) return 0;
-            st.insert(i.second);
+            if(v2[i] and v2[i]==v2[i-1]) return 0;
         }
-        
         return 1;
-        
     }
 };
