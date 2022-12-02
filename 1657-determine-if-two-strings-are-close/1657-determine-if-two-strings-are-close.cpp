711ee739b575802b4b93b347fe9cbcfa9499898d
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-12-02 18:09:09 +0530

    Time: 320 ms (25.75%), Space: 20.8 MB (32.93%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     bool closeStrings(string s1, string s2) {
-        if(s1.size()!=s2.size()) return 0;
-        vector<int>m1(26,0),m2(26,0);
-       
+
+        vector<int>m1(26);
+        vector<int>m2(26);
+        set<char>ms1,ms2;
+        
         for(auto &i:s1)
         {
             m1[i-'a']++;
-            // st1.insert(i);
+            ms1.insert(i);
         }
         for(auto &i:s2)
         {
             m2[i-'a']++;
-                // st2.insert(i);
-
+            ms2.insert(i);
+            
+            
         }
         
-        if(equal(begin(m1),end(m1),begin(m2),end(m2),[](auto a,auto b)
-           {
-               return (bool)a==(bool)b;
-           }
-          )==false)
-        {
-            return 0;
-        }
-          sort(begin(m1), end(m1));
-    sort(begin(m2), end(m2));
-    return m1==m2;
         
+        
+//         for(auto &i:m1) ms1.insert(i.second);
+//         for(auto &i:m2) ms2.insert(i.second);
+        
+        sort(begin(m1),end(m1));
+        sort(begin(m2),end(m2));
+        
+        return ms1==ms2 and m1==m2;
     }
 };
