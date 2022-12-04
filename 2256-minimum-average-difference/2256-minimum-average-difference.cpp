f2d11ab1c65453739f82a7ab451db892bc656834
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-12-04 10:25:29 +0530

    Time: 155 ms (77.87%), Space: 78.4 MB (76.81%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -4,37 +4,43 @@ public:
     int minimumAverageDifference(vector<int>& v) {
         int n=v.size();
         
-        vector<ll int>pre(n);
-        vector<ll int>suf(n);
+//         vector<ll int>pre(n);
+//         vector<ll int>suf(n);
         
-        pre[0]=v[0];
-        suf[n-1]=v[n-1];
+//         pre[0]=v[0];
+//         suf[n-1]=v[n-1];
+        ll pre{},suf{};
         
+        for(auto &i:v) suf+=i;
         
-        for(int i=1;i<n;i++)
-        {
-            pre[i]=pre[i-1]+v[i];
-            suf[n-i-1]=suf[n-i]+v[n-i-1];
+//         for(int i=1;i<n;i++)
+//         {
+//             pre[i]=pre[i-1]+v[i];
+//             suf[n-i-1]=suf[n-i]+v[n-i-1];
             
-        }
+//         }
         
-        // for(int i=n-2;i>=0;i--)
-        // {
-        //     suf[i]=suf[i+1]+v[i];
-        // }        
         
-        suf.push_back(0);
+           
+        
+        // suf.push_back(0);
         
         int minval=INT_MAX;
         int residx=INT_MAX;
         
         for(int i=0;i<n;i++)
         {
-            int a=floor(pre[i]/(1.0*i+1));
+            pre+=v[i];
+            suf-=v[i];
+            //int a=floor(pre[i]/(1.0*i+1));
+
+            int a=floor(pre/(1.0*i+1));
             
              int b=0;
             if(i!=n-1){
-                 b=floor(suf[i+1]/(1.0*(n-i-1)));
+                 // b=floor(suf[i+1]/(1.0*(n-i-1)));
+                 b=floor(suf/(1.0*(n-i-1)));
+                
             }
             int ans=abs(a-b);
             
