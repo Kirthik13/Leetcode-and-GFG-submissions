7d194aee231da442b797b45a14b71f404e78b4ad
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-12-21 07:58:32 +0530

    Time: 220 ms (90.04%), Space: 65.1 MB (61.07%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>>& v,int node,vector<int>&vis,int col)
+    {
+        vis[node]=col;
+        for(auto &i:v[node])
+        {
+            if(vis[i]==-1)
+            {
+                if(!dfs(v,i,vis,1-col)) return 0;
+            }
+            else if(vis[node]==vis[i]) return 0;
+            
+        }
+        return 1;
+    }
+    bool possibleBipartition(int n, vector<vector<int>>& g) {
+        vector<vector<int>>v(n+1);
+        for(auto &i:g)
+        {
+            v[i[0]].push_back(i[1]);
+            v[i[1]].push_back(i[0]);            
+        }
+        
+        vector<int>vis(n+1,-1);
+        
+        for(int i=1;i<=n;i++)
+        {
+            if(vis[i]==-1)
+            {
+                if(!dfs(v,i,vis,1)) return 0;
+            }
+        }
+        return 1;
+    }
+};
