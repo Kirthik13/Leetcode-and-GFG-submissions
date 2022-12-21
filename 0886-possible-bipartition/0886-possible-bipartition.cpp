19ac8fb6a74aa1ab9049611568624e4dc038d034
Kirthik Kumar <kirthikkirthik@gmail.com>
2022-12-21 08:02:30 +0530

    Time: 217 ms (91.10%), Space: 64.6 MB (75.45%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,18 +1,51 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& v,int node,vector<int>&vis,int col)
-    {
-        vis[node]=col;
-        for(auto &i:v[node])
-        {
-            if(vis[i]==-1)
-            {
-                if(!dfs(v,i,vis,1-col)) return 0;
-            }
-            else if(vis[node]==vis[i]) return 0;
+//     bool dfs(vector<vector<int>>& v,int node,vector<int>&vis,int col)
+//     {
+//         vis[node]=col;
+//         for(auto &i:v[node])
+//         {
+//             if(vis[i]==-1)
+//             {
+//                 if(!dfs(v,i,vis,1-col)) return 0;
+//             }
+//             else if(vis[node]==vis[i]) return 0;
             
-        }
-        return 1;
+//         }
+//         return 1;
+//     }
+     bool dfs(vector<vector<int>>& v,int nde,vector<int>&vis,int col)
+    {
+        vis[nde]=col;
+//         for(auto &i:v[node])
+//         {
+//             if(vis[i]==-1)
+//             {
+//                 if(!dfs(v,i,vis,1-col)) return 0;
+//             }
+//             else if(vis[node]==vis[i]) return 0;
+            
+//         }
+//         return 1;
+         queue<int>q;
+         q.push(nde);
+         while(!q.empty())
+         {
+             auto node=q.front();
+             q.pop();
+             for(auto &i:v[node])
+//           {
+                if(vis[i]==-1)
+                {
+                    // if(!dfs(v,i,vis,1-col)) return 0;
+                    vis[i]=1-vis[node];
+                    q.push(i);
+                }
+                else if(vis[node]==vis[i]) return 0;
+            
+             }
+         
+    return 1;
     }
     bool possibleBipartition(int n, vector<vector<int>>& g) {
         vector<vector<int>>v(n+1);
