2a038a40481c0b5b71aa16b03c556cc4f19014d8
Kirthik Kumar <kirthikkirthik@gmail.com>
2023-01-03 18:00:44 +0530

    Time: 84 ms (53.83%), Space: 12 MB (95.24%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -4,15 +4,20 @@ public:
         int c{};
         for(int i=0;i<v[0].size();i++)
         {
-            string s,t;
-            for(int j=0;j<v.size();j++)
+            // string s,t;
+            for(int j=0;j<v.size()-1;j++)
             {
-                s.push_back(v[j][i]);
-                t.push_back(v[j][i]);
+                // s.push_back(v[j][i]);
+                // t.push_back(v[j][i]);
+                if(v[j][i]>v[j+1][i])
+                {
+                    c++;
+                    break;
+                }
             }
             
-            sort(begin(t),end(t));
-            if(s!=t) c++;
+            // sort(begin(t),end(t));
+            // if(s!=t) c++;
             
         }
         return c;
