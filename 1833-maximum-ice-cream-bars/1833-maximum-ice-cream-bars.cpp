f4d48922c5ca1e8c4a2560c01149b92d4bf2397b
Kirthik Kumar <kirthikkirthik@gmail.com>
2023-01-06 08:09:54 +0530

    Time: 467 ms (19.86%), Space: 76.5 MB (76.68%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -2,21 +2,36 @@ class Solution {
 public:
     int maxIceCream(vector<int>& v, int k) {
         int n=v.size();
-        priority_queue<int,vector<int>,greater<int>>pq;
-        for(auto &i:v) pq.push(i);
-        while(!pq.empty() and k>0)
+        sort(begin(v),end(v));
+        int c{};
+        // priority_queue<int,vector<int>,greater<int>>pq;
+        // for(auto &i:v) pq.push(i);
+//         while(!pq.empty() and k>0)
+//         {
+//             auto val=pq.top();
+//             if(val<=k)
+//             {
+//                 k-=val;
+//                 pq.pop();
+
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+        // return n-pq.size();
+        for(auto &i:v)
         {
-            auto val=pq.top();
+             auto val=i;
             if(val<=k)
             {
                 k-=val;
-                pq.pop();
-
+                c++;
             }
             else{
                 break;
             }
         }
-        return n-pq.size();
+        return c;
     }
 };
