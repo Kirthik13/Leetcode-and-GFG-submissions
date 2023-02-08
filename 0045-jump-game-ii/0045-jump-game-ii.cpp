e2b4708969bdb8748cfcd16ea364bb7cdff82f90
Kirthik Kumar <kirthikkirthik@gmail.com>
2023-02-08 08:07:00 +0530

    Time: 485 ms (23.29%), Space: 18 MB (12.93%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int fn(vector<int>&v,int idx,int n,vector<int>&dp)
+    {
+        if(idx==n-1) return 0;
+        if(idx>=n) return 0;
+        
+        if(dp[idx]!=-1) return dp[idx];
+        
+        int ans=INT_MAX;
+        for(int i=1;i<=v[idx];i++)
+        {
+            int call1=fn(v,idx+i,n,dp);
+            // if(call1!=INT_MAX) call1++;
+            ans=min(call1,ans);
+        }
+        
+        if(ans!=INT_MAX)
+        {
+
+        
+        return dp[idx]=ans+1;
+        }
+        return dp[idx]=ans;
+        
+    }
+    int jump(vector<int>& v) {
+        
+        int n=v.size();
+        vector<int>dp(n+1,-1);
+        return fn(v,0,n,dp);
+        
+    }
+};
