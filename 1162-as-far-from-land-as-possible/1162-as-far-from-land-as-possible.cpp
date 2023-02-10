81cfe40e853c880542ca664fd6b90b50068949ad
Kirthik Kumar <kirthikkirthik@gmail.com>
2023-02-10 07:30:57 +0530

    Time: 98 ms (38.41%), Space: 25.1 MB (23.62%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool check(int &x,int &y,int &m,int &n,vector<vector<int>>& vis)
+    {
+        return x<0 or y<0 or x>=m or y>=n or vis[x][y]==1;
+    }
+    int maxDistance(vector<vector<int>>& v) {
+        vector<pair<int,int>>dir={{-1,0},{1,0},{0,1},{0,-1}};
+        queue<pair<pair<int,int>,int>>q;
+        int m=v.size(),n=v[0].size();
+        vector<vector<int>>vis(m,vector<int>(n));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(v[i][j]==1) q.push({{i,j},0});
+            }
+        }
+        int ans=-1;
+        while(!q.empty())
+        {
+            auto curr=q.front();
+            q.pop();
+            int x=curr.first.first;
+            int y=curr.first.second;
+            int dis=curr.second;
+            if(v[x][y]==0) ans=max(ans,dis);
+            for(auto &i:dir)
+            {
+                int newx=x+i.first;
+                int newy=y+i.second;
+                
+                if(!check(newx,newy,m,n,vis))
+                {
+                    q.push({{newx,newy},dis+1});
+                    vis[newx][newy]=1;
+                }
+                
+            }
+            
+        }
+        return ans;
+    }
+};
