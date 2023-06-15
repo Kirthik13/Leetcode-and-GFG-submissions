2cce05dbf2dd285123d91c6c637f08438c734d85
Kirthik Kumar <kirthikkirthik@gmail.com>
2023-06-15 07:32:02 +0530

    Time: 196 ms (81.08%), Space: 104.7 MB (95.47%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -11,33 +11,47 @@
  */
 class Solution {
 public:
+    vector<int>sums;
+    void dfs(TreeNode* root,int lvl) 
+    {
+        if(!root) return;
+        sums.resize(max((int)sums.size(),lvl));
+        sums[lvl-1]+=root->val;
+        dfs(root->left,lvl+1);
+        dfs(root->right,lvl+1);
+        
+        
+    
+    }
     int maxLevelSum(TreeNode* root) {
         
-        queue<TreeNode*>q;
+//         queue<TreeNode*>q;
         
-        q.push(root);
-        int lev=INT_MAX,ans=INT_MIN;
-        int levcnt=1;
-        while(!q.empty())
-        {
-            int n=q.size();
-            int sum{};
-            for(int i=0;i<n;i++)
-            {
-                auto node=q.front();
-                sum+=node->val;
-                q.pop();
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
+//         q.push(root);
+//         int lev=INT_MAX,ans=INT_MIN;
+//         int levcnt=1;
+//         while(!q.empty())
+//         {
+//             int n=q.size();
+//             int sum{};
+//             for(int i=0;i<n;i++)
+//             {
+//                 auto node=q.front();
+//                 sum+=node->val;
+//                 q.pop();
+//                 if(node->left) q.push(node->left);
+//                 if(node->right) q.push(node->right);
                 
-            }
-            if(ans<sum)
-            {
-                lev=levcnt;
-                ans=sum;
-            }
-            levcnt++;
-        }
-        return lev;
+//             }
+//             if(ans<sum)
+//             {
+//                 lev=levcnt;
+//                 ans=sum;
+//             }
+//             levcnt++;
+//         }
+//         return lev;
+            dfs(root,1);
+        return distance(begin(sums),max_element(begin(sums),end(sums)))+1;
     }
 };
