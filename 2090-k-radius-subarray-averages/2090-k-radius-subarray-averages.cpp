36b281ae70b0fcca58645928355f478fedaa11b6
Kirthik Kumar <kirthikkirthik@gmail.com>
2023-06-20 10:03:01 +0530

    Time: 276 ms (22.82%), Space: 139 MB (28.37%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& v, int k) {
+        int n=v.size();
+        
+        vector<int>totans(n,-1);
+      
+
+        vector<long long>pre(n+1);
+        
+        pre[0]=v[0];
+        // suf[n-1]=v[n-1];
+        
+        for(int i=0;i<n;i++)
+        {
+            pre[i+1]=v[i]+pre[i];
+        }
+      
+        
+        for(int i=0;i<n;i++)
+        {
+           
+            if(i-k<0 or i+k>n-1) continue;
+           
+            totans[i]= (pre[i+k+1]-pre[i-k])/(k*2+1);
+        }
+        return totans;
+    }
+};
