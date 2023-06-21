d820f0cf3a2f685f5edf480fcfe45727a09ceaf8
Kirthik Kumar <kirthikkirthik@gmail.com>
2023-06-21 08:17:01 +0530

    Time: 92 ms (89.42%), Space: 44.8 MB (22.63%) - LeetHub

---
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -0,0 +1,35 @@
+#define ll long long 
+class Solution {
+public:
+    ll calculateans(vector<pair<ll int,ll int>>& v, vector<int>& cost,int medianele) {
+        ll ans{};
+        for(int i=0;i<v.size();i++)
+        {
+            ans=ans+(abs(v[i].first-medianele)*v[i].second);
+        }
+        return ans;
+    }
+    long long minCost(vector<int>& v2, vector<int>& cost) {
+        int n=v2.size();
+        vector<pair<ll int,ll int>>v;
+        for(int i=0;i<n;i++)
+        {
+            v.push_back({v2[i],cost[i]});
+        }
+        sort(begin(v),end(v));
+        ll totnos{},curr{};
+        for(auto &i:v)
+        {
+            totnos+=i.second;
+        }
+        int i{};
+        while(i<n and curr<(totnos+1)/2)
+        {
+            curr+=v[i].second;
+            i++;
+        }
+        
+        return calculateans(v,cost,v[i-1].first);
+
+    }
+};
