2a3a229c090fff6f16b4d9f399a679cd1c2452c0
Kirthik Kumar <kirthikkirthik@gmail.com>
2023-10-08 07:56:44 +0530

    Time: 54 ms (31.36%), Space: 13.9 MB (19.28%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1458-max-dot-product-of-two-subsequences/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1458-max-dot-product-of-two-subsequences.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int fn(vector<int>& v1, vector<int>& v2,int i,int j,int m,int n,    vector<vector<int>>&dp) 
+    {
+        if(i>=m or j>=n)
+        {
+            return 0;
+        }
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int take1=fn(v1,v2,i+1,j,m,n,dp);
+        int take2=fn(v1,v2,i,j+1,m,n,dp);
+        int take12=fn(v1,v2,i+1,j+1,m,n,dp)+(v1[i]*v2[j]);
+        int leave=fn(v1,v2,i+1,j+1,m,n,dp);
+        
+        return dp[i][j]=max({take1,take2,take12,leave});
+        
+                
+
+        
+    }
+    int maxDotProduct(vector<int>& v1, vector<int>& v2) {
+        int m=v1.size(),n=v2.size();
+        
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        
+        int ans=fn(v1,v2,0,0,m,n,dp);
+        
+        if(ans==0)
+        {
+            int a=*min_element(begin(v1),end(v1));
+            int b=*max_element(begin(v1),end(v1));
+            int c=*min_element(begin(v2),end(v2));
+            int d=*max_element(begin(v2),end(v2));
+            
+            
+            ans=max(a*d,b*c);
+        }
+        return ans;
+    }
+};
