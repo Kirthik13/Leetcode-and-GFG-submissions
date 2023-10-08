d9f7350df9aab90455b0d54924ea66816e926fd2
Kirthik Kumar <kirthikkirthik@gmail.com>
2023-10-08 08:03:41 +0530

    Time: 44 ms (36.76%), Space: 13.9 MB (20.05%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -4,15 +4,16 @@ public:
     {
         if(i>=m or j>=n)
         {
-            return 0;
+            return INT_MIN;
         }
         
         if(dp[i][j]!=-1) return dp[i][j];
         
         int take1=fn(v1,v2,i+1,j,m,n,dp);
         int take2=fn(v1,v2,i,j+1,m,n,dp);
-        int take12=fn(v1,v2,i+1,j+1,m,n,dp)+(v1[i]*v2[j]);
         int leave=fn(v1,v2,i+1,j+1,m,n,dp);
+        
+        int take12=(leave<0?0:leave)+(v1[i]*v2[j]);
         
         return dp[i][j]=max({take1,take2,take12,leave});
         
@@ -27,16 +28,16 @@ public:
         
         int ans=fn(v1,v2,0,0,m,n,dp);
         
-        if(ans==0)
-        {
-            int a=*min_element(begin(v1),end(v1));
-            int b=*max_element(begin(v1),end(v1));
-            int c=*min_element(begin(v2),end(v2));
-            int d=*max_element(begin(v2),end(v2));
+//         if(ans==0)
+//         {
+//             int a=*min_element(begin(v1),end(v1));
+//             int b=*max_element(begin(v1),end(v1));
+//             int c=*min_element(begin(v2),end(v2));
+//             int d=*max_element(begin(v2),end(v2));
             
             
-            ans=max(a*d,b*c);
-        }
+//             ans=max(a*d,b*c);
+//         }
         return ans;
     }
 };
